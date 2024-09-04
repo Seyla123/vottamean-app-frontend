f12921c98c4d4037ead8e6227c6cf4a87dabab3b
Sok Chan Seiha <sokchanseiha@gmail.com>
2024-09-04 18:54:44 +0700

    feature: create-class-period, add input field

--- src/pages/admin/class-period/ClassPeriodCreatePage.jsx
+++ src/pages/admin/class-period/ClassPeriodCreatePage.jsx
@@ -1,16 +1,20 @@
 import Box from "@mui/material/Box";
 import { Typography } from "@mui/material";
-import { DemoContainer } from '@mui/x-date-pickers/internals/demo';
-import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
-import { TimePicker } from '@mui/x-date-pickers/TimePicker';
+import { DemoContainer } from "@mui/x-date-pickers/internals/demo";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { TimePicker } from "@mui/x-date-pickers/TimePicker";
 import { DemoItem } from "@mui/x-date-pickers/internals/demo";
+import Button from "@mui/material/Button";
+import Stack from "@mui/material/Stack";
+import Divider from "@mui/material/Divider";
+import Card from "@mui/material/Card";
 
 function ClassPeriodCreatePage() {
   return (
-    <Box>
+    <Box sx={{mx: 4}}>
       {/* Title */}
-      <Box>
+      <Box sx={{my: 3}}>
         <Typography variant="h4" fontWeight="bold" sx={{ my: 0 }} gutterBottom>
           ADD CLASS PERIOD
         </Typography>
@@ -19,26 +23,54 @@ function ClassPeriodCreatePage() {
         </Typography>
       </Box>
       {/* Form */}
-      <Box>
+      <Card sx={{ boxShadow: 2, Shadow: 1, padding: 3}}>
+        {/* title field */}
+        <Typography gutterBottom variant="subtitle1" fontWeight="bold">
+          Class Period Information
+        </Typography>
+        <Divider sx={{ bgcolor: "black", my: 1.5 }}></Divider>
+        {/* content field */}
         <Box>
-          <Typography gutterBottom variant="body1" fontWeight="bold">
-            Class Period Information
-          </Typography>
-          <Box>
-            <LocalizationProvider dateAdapter={AdapterDayjs}>
-              <DemoItem label="Start time" components={["TimePicker"]}>
-                <TimePicker sx={{width: '100%'}} label="select" />
+          {/* time field */}
+          <LocalizationProvider dateAdapter={AdapterDayjs}>
+            <DemoContainer
+              components={["TimePicker", "TimePicker", "TimePicker"]}
+            >
+              <DemoItem label="Start time">
+                <TimePicker sx={{ width: "100%" }} label="select" />
               </DemoItem>
-              <DemoItem label="Start time" components={["TimePicker"]}>
-                <TimePicker sx={{width: '100%'}} label="select" />
+              <DemoItem label="End time">
+                <TimePicker sx={{ width: "100%" }} label="select" />
               </DemoItem>
-            </LocalizationProvider>
+            </DemoContainer>
+          </LocalizationProvider>
+          {/* button field */}
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "flex-end",
+            }}
+          >
+            <Stack
+              spacing={3}
+              direction="row"
+              sx={{
+                mt: 4,
+                width: "350px",
+              }}
+            >
+              <Button variant="outlined" size="large" sx={{ width: "100%", height: 40 }} color="black">
+                cancel
+              </Button>
+              <Button variant="contained" size="large" sx={{ width: "100%", height: 40}}>
+                add period
+              </Button>
+            </Stack>
           </Box>
         </Box>
-      </Box>
+      </Card>
     </Box>
   );
 }
-
 
 export default ClassPeriodCreatePage;
