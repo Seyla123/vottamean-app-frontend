3a3b7b985484dde1010d2590c94855648d84e5fe
Sveita <sveitast7@gmail.com>
2024-09-05 19:56:18 +0700

    refactor : forgot password page

--- src/pages/auth/PasswordForgotPage.jsx
+++ src/pages/auth/PasswordForgotPage.jsx
@@ -1,8 +1,71 @@
 import React from "react";
 import { Box, Typography, TextField, Button, Link, Card} from "@mui/material";
-import forget from "../../../assets/icon/forget.png";
+import forget from "../../assets/icon/forget.png";
+import GoBackButton from "../../components/common/GoBackButton";
+import { Padding } from "@mui/icons-material";
 function PasswordForgotPage() {
-  const screen = {
+  
+  return (
+    <Box sx={screen}>
+      <Card sx={content}>
+        {/* back button and img  */}
+        <Box>
+          <GoBackButton handleOnClick={() => {console.log('clicked');}} />
+          <Box sx={img}>
+            <img src={forget} style={{ width: "100%" }}></img>
+          </Box>
+        </Box>
+        {/* Reset Password Title  */}
+        <Box sx={resetTitle}>
+          <Typography
+            sx={{ transitionDuration: "0.5s",fontSize: { xs: "24px", md: "36px" }, fontWeight: "bold" }}
+          >
+            Forgot Password ?
+          </Typography>
+          <Typography sx={{ fontSize: { xs: "14px", md: "16px" } }}>
+            We will send you an email to reset your password
+          </Typography>
+        </Box>
+        {/* Text input */}
+        <Box sx={textInputContainer}>
+          <Box sx={textfield}>
+            <Typography>Email</Typography>
+            <TextField
+              id="email"
+              placeholder="email"
+              variant="outlined"
+            />
+          </Box>
+        </Box>
+
+        <Box display={"flex"} flexDirection={"column"} gap={"8px"}>
+          <Button fullWidth  variant="contained" sx={{padding:{xs:1.5,md:2}}}>
+            Continue
+          </Button>
+
+          <Box
+            sx={{
+              display: "flex",
+              gap: "8px",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            <Typography variant="subtitle1">
+              Don't have an account ?{" "}
+            </Typography>
+            <Link src={{}}>Sign up</Link>
+          </Box>
+        </Box>
+      </Card>
+    </Box>
+  );
+}
+
+export default PasswordForgotPage;
+
+
+const screen = {
     width: "100%",
     height: "100vh",
     display : "flex",
@@ -61,68 +124,3 @@ function PasswordForgotPage() {
     display: "flex",
     flexDirection: "column",
   };
-
-  const button = {
-    width: "100%",
-    height: { sx: "42px", md: "56px" },
-    mb: { md: "24px", xs: "12px" },
-  };
-  return (
-    <Box sx={screen}>
-      <Card sx={content}>
-        {/* back button and img  */}
-        <Box>
-          <BackButton />
-          <Box sx={img}>
-            <img src={forget} style={{ width: "100%" }}></img>
-          </Box>
-        </Box>
-        {/* Reset Password Title  */}
-        <Box sx={resetTitle}>
-          <Typography
-            sx={{ fontSize: { xs: "24px", md: "36px" }, fontWeight: "bold" }}
-          >
-            Forgot Password ?
-          </Typography>
-          <Typography sx={{ fontSize: { xs: "14px", md: "16px" } }}>
-            We will send you an email to reset your password
-          </Typography>
-        </Box>
-        {/* Text input */}
-        <Box sx={textInputContainer}>
-          <Box sx={textfield}>
-            <Typography>Email</Typography>
-            <TextField
-              id="email"
-              placeholder="email"
-              variant="outlined"
-              sx={{ width: "100%" }}
-            />
-          </Box>
-        </Box>
-
-        <Box>
-          <Button variant="contained" sx={button}>
-            Continue
-          </Button>
-
-          <Box
-            sx={{
-              display: "flex",
-              gap: "8px",
-              alignItems: "center",
-              justifyContent: "center",
-            }}
-          >
-            <Typography variant="subtitle1">
-              Don't have an account ?{" "}
-            </Typography>
-            <Link src={{}}>Sign up</Link>
-          </Box>
-        </Box>
-      </Card>
-    </Box>
-  );
-}
-
-export default PasswordForgotPage;
