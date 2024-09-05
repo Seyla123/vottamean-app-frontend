e4a59e2fca15a30e06da77494f31de658dda08b8
Seav Seyla <Mrrseyla.758@gmaill.com>
2024-09-05 13:25:33 +0700

    feature : add GoBack button

---
+++ src/components/common/GoBackButton.jsx
@@ -0,0 +1,23 @@
+import { Box, Typography } from "@mui/material";
+import WestIcon from "@mui/icons-material/West";
+function GoBackButton() {
+  return (
+    <Box
+      component={"div"}
+      sx={{
+        display: "flex",
+        gap: 1,
+        alignItems: "center",
+        color: "text.disabled",
+        cursor: "pointer",
+        ":hover": { color: "black" },
+      }}>
+      <WestIcon fontSize="small" sx={{ alignSelf: "center" }} />
+      <Typography variant="body1" fontWeight={600} lineHeight={"24px"}>
+        Back
+      </Typography>
+    </Box>
+  );
+}
+
+export default GoBackButton;
--- src/pages/auth/signup/SignupPage.jsx
+++ src/pages/auth/signup/SignupPage.jsx
@@ -7,11 +7,21 @@ import {
   Stepper,
   StepLabel,
 } from "@mui/material";
-
 import GetStartSignUpForm from "../../../components/auth/GetStartSignUpForm";
 import PersonalInformationForm from "../../../components/auth/PersonalInformationForm";
+import { useState } from "react";
 function SignupPage() {
-
+const [stepActive ,setStepActive] = useState(0);
+const handleBack = () => {
+  setStepActive(stepActive - 1)
+}
+const handleNext = () => {
+  setStepActive(stepActive + 1)
+}
+const stepFrom = {
+  0: <GetStartSignUpForm/>,
+  1: <PersonalInformationForm onClickBack={handleBack}/>
+}
   return (
     <>
       <Container
@@ -19,7 +29,7 @@ function SignupPage() {
         sx={{ display: "flex", flexDirection: "column", gap: 3 }}
       >
         <Box sx={{ width: 1, paddingTop: { xs: 3, md: 4 } }}>
-          <Stepper activeStep={0} alternativeLabel>
+          <Stepper activeStep={stepActive} alternativeLabel>
             {steps.map((label) => (
               <Step key={label}>
                 <StepLabel>{label}</StepLabel>
@@ -29,15 +39,15 @@ function SignupPage() {
         </Box>
         <AuthContainerCard sideCard="left" >
             {/* <GetStartSignUpForm/> */}
-            <PersonalInformationForm/>
+            {stepFrom[stepActive]}
         </AuthContainerCard>
       </Container>
       <Box sx={{ display: "flex", flexDirection: "row", pt: 2 }}>
-        <Button color="inherit"  disabled={false} sx={{ mr: 1 }}>
+        <Button onClick={handleBack} color="inherit"  disabled={false} sx={{ mr: 1 }}>
           Back
         </Button>
         <Box sx={{ flex: "1 1 auto" }} />
-        <Button>Next</Button>
+        <Button onClick={handleNext}>Next</Button>
       </Box>
     </>
   );
