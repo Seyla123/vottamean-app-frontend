85638b7d47f3ce7defe22458128a4a0bc3aa1484
Sok Chan Seiha <sokchanseiha@gmail.com>
2024-09-06 15:21:48 +0700

    feature: detail-class-period, relocation styling properties

--- src/pages/admin/class-period/ClassPeriodDetailPage.jsx
+++ src/pages/admin/class-period/ClassPeriodDetailPage.jsx
@@ -6,27 +6,6 @@ import ModeEditIcon from "@mui/icons-material/ModeEdit";
 
 function ClassPeriodDetailPage() {
 
-  const info = {
-    "Class Period ID": 123,
-    "Start Time": "8:00 AM",
-    "End Time": "8:30 AM",
-    Period: "1h 30mn",
-  };
-
-  const content = { lg: "32px", xs: "24px" };
-  const section = { my: 0, fontSize: { lg: 32, xs: 20 } };
-  const title = { fontSize: { lg: 16, xs: 14 } };
-  const infoArea = {
-    boxShadow: 1,
-    px: { lg: "32px", xs: "24px" },
-    pt: { lg: "24px", xs: "16px" },
-    pb: "32px" };
-  const divider = {
-    bgcolor: "black",
-    my: "16px",
-    mt: { lg: "16px", xs: "8px" },
-  };
-
   return (
     <Box>
       <Box sx={{ my: content }}>
@@ -39,13 +18,12 @@ function ClassPeriodDetailPage() {
       </Box>
 
       <Card sx={infoArea}>
-        <Box>
+        <Box sx={{display: "flex"}}>
           <Typography sx={{ fontSize: 18 }} fontWeight="bold">
             Class Period Information
           </Typography>
           <ModeEditIcon/>
           <DeleteForeverIcon/>
-
         </Box>
         <Divider sx={divider} />
         <Box>
@@ -70,3 +48,24 @@ function ClassPeriodDetailPage() {
 }
 
 export default ClassPeriodDetailPage;
+
+const info = {
+  "Class Period ID": 123,
+  "Start Time": "8:00 AM",
+  "End Time": "8:30 AM",
+  Period: "1h 30mn",
+};
+
+const content = { lg: "32px", xs: "24px" };
+const section = { my: 0, fontSize: { lg: 32, xs: 20 } };
+const title = { fontSize: { lg: 16, xs: 14 } };
+const infoArea = {
+  boxShadow: 1,
+  px: { lg: "32px", xs: "24px" },
+  pt: { lg: "24px", xs: "16px" },
+  pb: "32px" };
+const divider = {
+  bgcolor: "black",
+  my: "16px",
+  mt: { lg: "16px", xs: "8px" },
+};
