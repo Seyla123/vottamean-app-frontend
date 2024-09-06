d3d2c659627b8795460b5d5941997b8791c2701b
Sok Chan Seiha <sokchanseiha@gmail.com>
2024-09-06 14:59:06 +0700

    feature: detail-class-period, refactor design style

--- src/pages/admin/class-period/ClassPeriodDetailPage.jsx
+++ src/pages/admin/class-period/ClassPeriodDetailPage.jsx
@@ -5,16 +5,26 @@ import List from "@mui/material/List";
 // import {ModeEditIcon, DeleteForeverIcon} from "@mui/icons-material"
 
 function ClassPeriodDetailPage() {
-  const content = { lg: "32px", xs: "24px" };
-  const section = { my: 0, fontSize: { lg: 32, xs: 20 } };
-  const title = { fontSize: { lg: 16, xs: 14 } };
-  const divider = { bgcolor: "black", my: "16px", mt: {lg: "16px", xs:"8px"}};
 
   const info = {
     "Class Period ID": 123,
     "Start Time": "8:00 AM",
     "End Time": "8:30 AM",
-    Period: "1h 30mn"
+    Period: "1h 30mn",
+  };
+
+  const content = { lg: "32px", xs: "24px" };
+  const section = { my: 0, fontSize: { lg: 32, xs: 20 } };
+  const title = { fontSize: { lg: 16, xs: 14 } };
+  const infoArea = {
+    boxShadow: 1,
+    px: { lg: "32px", xs: "24px" },
+    pt: { lg: "24px", xs: "16px" },
+    pb: "32px" };
+  const divider = {
+    bgcolor: "black",
+    my: "16px",
+    mt: { lg: "16px", xs: "8px" },
   };
 
   return (
@@ -28,27 +38,25 @@ function ClassPeriodDetailPage() {
         </Typography>
       </Box>
 
-      <Card
-        sx={{
-          boxShadow: 1,
-          px: { lg: "32px", xs: "24px" },
-          pt: { lg: "24px", xs: "16px" },
-          pb: "32px"
-        }}
-      >
+      <Card sx={infoArea}>
         <Typography sx={{ fontSize: 18 }} fontWeight="bold">
           Class Period Information
         </Typography>
         <Divider sx={divider} />
         <Box>
-          <List sx={{display: "flex", flexDirection: "column", gap: "10px"}}>
-
-              {Object.entries(info).map(([subject, value], index) => (
-                <Box key={index} sx={{ display: "flex", gap: 1}}>
-                  <Typography sx={{fontSize: {lg: "16px", xs: "14px"}}} fontWeight="medium" >{subject} :</Typography>
-                  <Typography sx={{fontSize: {lg: "16px", xs: "14px"}}}>{value}</Typography>
-                </Box>
-              ))}
+          <List sx={{ display: "flex", flexDirection: "column", gap: "10px" }}>
+            {Object.entries(info).map(([subject, value], index) => (
+              <Box key={index} sx={{ display: "flex", gap: 1 }}>
+                <Typography
+                  sx={{ fontSize: { lg: "16px", xs: "14px" } }}
+                  fontWeight="medium">
+                  {subject} :
+                </Typography>
+                <Typography sx={{ fontSize: { lg: "16px", xs: "14px" } }}>
+                  {value}
+                </Typography>
+              </Box>
+            ))}
           </List>
         </Box>
       </Card>
