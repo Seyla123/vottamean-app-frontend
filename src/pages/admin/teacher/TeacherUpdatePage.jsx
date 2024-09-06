edcd785d028dc0a6606a738dc06c21c3a2c12e59
Nem Puthiraksa <raksasparkle@gmail.com>
2024-09-06 11:00:27 +0700

    feature : add header to update teacher page

---
+++ src/pages/admin/teacher/TeacherUpdatePage.jsx
@@ -0,0 +1,16 @@
+import { Box } from '@mui/material'
+import React from 'react'
+import Header from '../../../components/teacher/Header'
+import formInfo from '../../../components/teacher/formInfo'
+function TeacherUpdatePage() {
+  return (
+    <Box>
+        {/* Header */}
+      <Header header="UPDATE TEACHER" subheader=" Please Update Teacher Information" />
+      {/* Form edit */}
+    
+    </Box>
+  )
+}
+
+export default TeacherUpdatePage
