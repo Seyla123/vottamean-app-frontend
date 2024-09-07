6ee767e67d644af90c3c3b73ebaa6f5425c8e9db
sopheaktra <sopheak1013@gmail.com>
2024-09-07 16:42:46 +0700

    feature:  Done on Changing Tab in Detail page Student

--- src/pages/admin/student/StudentDetailPage.jsx
+++ src/pages/admin/student/StudentDetailPage.jsx
@@ -1,26 +1,24 @@
 import {
   Box,
-  Button,
   Typography,
   useTheme,
   useMediaQuery,
-  Hidden,
   Divider,
   Card,
+  Tabs,
+  Tab,
 } from "@mui/material";
 import EditIcon from "@mui/icons-material/Edit";
 import DeleteIcon from "@mui/icons-material/Delete";
 import React from "react";
-import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 const StudentDetailPage = () => {
   const theme = useTheme();
   const navigate = useNavigate();
   const mobile = theme.breakpoints;
-  const [selectedPostId, setSelectedPostId] = useState(null);
 
-  const handleUpdateStudent = () => {
+  const handleUpdateStudent = (selectedPostId) => {
     navigate(`/student/update/${selectedPostId}`);
   };
 
@@ -59,14 +57,10 @@ const StudentDetailPage = () => {
       height: "100%",
       objectFit: "cover",
     },
-    alignItems: {
-      xs: "center",
-      sm: "flex-start",
-    },
   };
 
   // State to track the active tab
-  const [activeTab, setActiveTab] = useState("student");
+  const [activeTab, setActiveTab] = React.useState(0);
 
   const containerStyles = {
     width: "100%",
@@ -75,11 +69,6 @@ const StudentDetailPage = () => {
   };
 
   const isMobile = useMediaQuery("(max-width:600px)");
-
-  const selectType = [
-    { value: "Class A", label: "Class A" },
-    { value: "Class B", label: "Class B" },
-  ];
 
   return (
     <Box sx={containerStyles}>
@@ -103,57 +92,20 @@ const StudentDetailPage = () => {
           There are student's information
         </Typography>
       </Box>
-      <Box sx={{ display: "flex", alignItems: "center", py: "24px" }}>
-        <Box
-          onClick={() => setActiveTab("student")}
-          sx={{
-            cursor: "pointer",
-            width: "206px",
-            borderBottom:
-              activeTab === "student" ? "2px solid #1976d2" : "none",
-          }}
-        >
-          <Button
-            sx={{
-              color: activeTab === "student" ? "#2196F3" : "rgba(0, 0, 0, 0.6)",
-              fontFamily: "Roboto",
-              fontSize: "14px",
-              fontWeight: "medium",
-            }}
-          >
-            STUDENT INFORMATION
-          </Button>
-        </Box>
-        <Box
-          onClick={() => setActiveTab("guardian")}
-          sx={{
-            cursor: "pointer",
-            width: "206px",
-            borderBottom:
-              activeTab === "guardian" ? "2px solid #1976d2" : "none",
-          }}
-        >
-          <Button
-            sx={{
-              color:
-                activeTab === "guardian" ? "#2196F3" : "rgba(0, 0, 0, 0.6)",
-              fontFamily: "Roboto",
-              fontSize: "14px",
-              fontWeight: "medium",
-            }}
-          >
-            GUARDIAN CONTACT
-          </Button>
-        </Box>
-      </Box>
+
+      <Tabs
+        value={activeTab}
+        onChange={(event, newValue) => setActiveTab(newValue)}
+        textColor="primary"
+        indicatorColor="primary"
+      >
+        <Tab label="STUDENT INFORMATION" />
+        <Tab label="GUARDIAN CONTACT" />
+      </Tabs>
+
       {/* Conditionally render Student or Guardian information */}
-      {activeTab === "student" && (
-        <Card
-          sx={{
-            width: "100%",
-            py: "32px",
-          }}
-        >
+      {activeTab === 0 && (
+        <Card sx={{ width: "100%", px: "16px",py:"32px" }}>
           <Box
             sx={{
               display: "flex",
@@ -167,7 +119,7 @@ const StudentDetailPage = () => {
             <Box>
               <EditIcon
                 sx={{ mr: 1, color: "blue" }}
-                onClick={handleUpdateStudent}
+                onClick={() => handleUpdateStudent("ANB1000")}
               />
               <DeleteIcon sx={{ color: "red" }} />
             </Box>
@@ -175,21 +127,22 @@ const StudentDetailPage = () => {
           <Divider
             sx={{ my: { xs: "12px", sm: "16px" }, borderBottomWidth: 3 }}
           />
+          {/* Student details go here */}
           <Box
             sx={{
               display: "flex",
               flexDirection: {
-                xs: "column",
-                sm: "row",
+              xs: "column",
+              sm: "row",
               },
               width: "100%",
               alignItems: "center",
               gap: {
-                xs: 3,
-                sm: 5,
+              xs: 3,
+              sm: 5,
               },
               mt: 2,
-            }}
+              }}
           >
             <Box sx={profile}>
               <img
@@ -198,17 +151,16 @@ const StudentDetailPage = () => {
                 style={{ width: "100%", height: "100%", objectFit: "cover" }}
               />
             </Box>
-            <Box
-              sx={{
-                display: "flex",
-                flexDirection: "column",
-                gap: {
-                  xs: "10px",
-                  sm: "12px",
-                },
-                width: "100%",
-              }}
-            >
+            <Box             sx={{
+              display: "flex",
+              flexDirection: "column",
+              gap: {
+                xs: "8px",
+                sm: "16px",
+              },
+              width: "100%",
+            }}>
+              {/* Example Student Information */}
               <Typography sx={textStyles}>
                 Student ID: <Typography sx={textInput}>ANB1000</Typography>
               </Typography>
@@ -246,13 +198,8 @@ const StudentDetailPage = () => {
         </Card>
       )}
 
-      {activeTab === "guardian" && (
-        <Card
-          sx={{
-            width: "100%",
-            py: "32px",
-          }}
-        >
+      {activeTab === 1 && (
+        <Card sx={{ width: "100%", py: "32px", px:'16px' }}>
           <Box
             sx={{
               display: "flex",
@@ -266,55 +213,40 @@ const StudentDetailPage = () => {
             <Box>
               <EditIcon
                 sx={{ mr: 1, color: "blue" }}
-                onClick={handleUpdateStudent}
+                onClick={() => handleUpdateStudent("ANB1000")}
               />
             </Box>
           </Box>
           <Divider
             sx={{ my: { xs: "12px", sm: "16px" }, borderBottomWidth: 3 }}
           />
+          {/* Guardian details go here */}
           <Box
             sx={{
               display: "flex",
-              flexDirection: {
-                xs: "column",
-                sm: "row",
+              flexDirection: "column",
+              gap: {
+                xs: "10px",
+                sm: "12px",
               },
               width: "100%",
-              alignItems: "center",
-              gap: {
-                xs: 3,
-                sm: 5,
-              },
-              mt: 2,
             }}
           >
-            <Box
-              sx={{
-                display: "flex",
-                flexDirection: "column",
-                gap: {
-                  xs: "10px",
-                  sm: "12px",
-                },
-                width: "100%",
-              }}
-            >
-              <Typography sx={textStyles}>
-                Guardain Name:{" "}
-                <Typography sx={textInput}>Potato Fried</Typography>
-              </Typography>
-              <Typography sx={textStyles}>
-                Relationship: <Typography sx={textInput}>Father</Typography>
-              </Typography>
-              <Typography sx={textStyles}>
-                Email:{" "}
-                <Typography sx={textInput}>exampleusergmail.com</Typography>
-              </Typography>
-              <Typography sx={textStyles}>
-                Phone Number: <Typography sx={textInput}>01234567</Typography>
-              </Typography>
-            </Box>
+            {/* Example Guardian Information */}
+            <Typography sx={textStyles}>
+              Guardian Name:{" "}
+              <Typography sx={textInput}>Potato Fried</Typography>
+            </Typography>
+            <Typography sx={textStyles}>
+              Relationship: <Typography sx={textInput}>Father</Typography>
+            </Typography>
+            <Typography sx={textStyles}>
+              Email:{" "}
+              <Typography sx={textInput}>exampleusergmail.com</Typography>
+            </Typography>
+            <Typography sx={textStyles}>
+              Phone Number: <Typography sx={textInput}>01234567</Typography>
+            </Typography>
           </Box>
         </Card>
       )}
