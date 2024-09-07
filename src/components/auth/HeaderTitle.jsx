b68662d3257aee6b8855522ffef318eb76339198
Seav <Mrrseyla.758@gmail.com>
2024-09-07 17:58:33 +0700

    Refactor : In Headtitle Auth

--- src/components/auth/HeaderTitle.jsx
+++ src/components/auth/HeaderTitle.jsx
@@ -1,16 +1,24 @@
 import { Box, Typography } from '@mui/material'
 
-function HeaderTitle({title , subTitle, center, children}) {
-    const container = { display: "flex", flexDirection: "column", gap: 1, textAlign: center ? "center" : "left" }
-    return (
-        <Box sx={container}>
-            {children}
-            <Typography variant="h4" fontWeight={"bold"} padding={0}>
-                {title}
-            </Typography>
-            <Typography variant="subtitle1">{subTitle}</Typography>
-        </Box>
-    )
-}
+const HeaderTitle = ({ title, subTitle, center, children }) => {
 
-export default HeaderTitle
+  return (
+    <Box sx={containerStyles}>
+    {/* Render any additional content passed as children */}
+      {children}
+      <Typography variant="h4" fontWeight={"bold"} padding={0}>
+        {title}
+      </Typography>
+      <Typography variant="subtitle1">{subTitle}</Typography>
+    </Box>
+  );
+};
+
+export default HeaderTitle;
+
+const containerStyles = {
+    display: "flex",
+    flexDirection: "column",
+    gap: 1,
+    textAlign: center ? "center" : "left",
+  };
