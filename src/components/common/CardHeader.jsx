8cedac436db3dbf99668490e1241d8d2795da1da
Seav <Mrrseyla.758@gmail.com>
2024-09-07 16:59:07 +0700

    feature : add comment to CardHeader

--- src/components/common/CardHeader.jsx
+++ src/components/common/CardHeader.jsx
@@ -1,12 +1,26 @@
-import {Stack , Typography, Divider} from "@mui/material"
+/**
+ * CardHeader component
+ * 
+ * This component is used to render a header for a card, including a title and a divider.
+ * 
+ * Usage:
+ * 
+ * import CardHeader from './CardHeader';
+ * 
+ * <CardHeader title="Your Card Title" />
+ * 
+ * @param {string} title - The title to be displayed in the card header.
+ * @returns JSX.Element
+ */
 
-function CardHeader({title}) {
-  return (
-    <Stack component={'div'} direction="column" gap={1}>
-    <Typography variant="formTitle" >School Information</Typography>
-    <Divider sx={{  borderWidth: 1 , borderColor: "text.secondary"}} />
-    </Stack>
-  )
+import { Stack, Typography, Divider } from "@mui/material"
+function CardHeader({ title }) {
+    return (
+        <Stack component={'div'} direction="column" gap={1}>
+            <Typography variant="formTitle" >{title}</Typography>
+            <Divider sx={{ borderWidth: 1, borderColor: "text.secondary" }} />
+        </Stack>
+    )
 }
 
 export default CardHeader;
