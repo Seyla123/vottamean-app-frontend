3ccac417b8cf825882adde7032223ffce9267ec9
Seav <Mrrseyla.758@gmail.com>
2024-09-07 17:56:37 +0700

    Refactor : in GetStartFrom Component

--- src/components/auth/GetStartSignUpForm.jsx
+++ src/components/auth/GetStartSignUpForm.jsx
@@ -1,46 +1,36 @@
-import { Box, Typography, TextField, Button, Checkbox } from "@mui/material";
+import { Box, Typography, TextField, Checkbox } from "@mui/material";
 import HeaderTitle from "./HeaderTitle";
-function GetStartSignUp({ onClickNext, children}) {
+
+const GetStartSignUp = ({ children }) => {
+
   return (
-    <Box
-      sx={{
-        gap: 3,
-        justifyContent: "center",
-        display: "flex",
-        flexDirection: "column",
-      }}
-    >
+    <Box sx={containerStyles}>
       {/* header title */}
-      <HeaderTitle
-        title={"Get started"}
-        subTitle={"Create an account"}
-      />
+      <HeaderTitle title={"Get started"} subTitle={"Create an account"} />
 
       {/* form container */}
-      <Box
-        sx={{ display: "flex", flexDirection: "column", gap: { xs: 3, md: 4 } }}
-      >
+      <Box sx={formContainerStyles}>
         {/* email input container */}
-        <Box display={"flex"} flexDirection={"column"} gap={0.5}>
+        <Box sx={inputContainerStyles}>
           <Typography variant="body1">Email</Typography>
           <TextField placeholder="email" />
         </Box>
 
         {/* Password input container */}
-        <Box display={"flex"} flexDirection={"column"} gap={0.5}>
+        <Box sx={inputContainerStyles}>
           <Typography variant="body1">Password</Typography>
           <TextField placeholder="password" />
         </Box>
 
         {/* Confirm password input container */}
-        <Box display={"flex"} flexDirection={"column"} gap={0.5}>
+        <Box sx={inputContainerStyles}>
           <Typography variant="body1">Confirm Password </Typography>
           <TextField placeholder="confirm password" />
         </Box>
       </Box>
 
       {/* check box */}
-      <Box sx={{ display: "flex", alignItems: "center" }}>
+      <Box sx={checkboxContainerStyles}>
         <Checkbox
           color="default"
           inputProps={{ "aria-label": "primary checkbox" }}
@@ -55,14 +45,7 @@ function GetStartSignUp({ onClickNext, children}) {
       </Box>
 
       {/* container button continue and already ahve an account login link */}
-      <Box
-        sx={{
-          display: "flex",
-          flexDirection: "column",
-          gap: 1,
-          textAlign: "center",
-        }}
-      >
+      <Box sx={buttonContainerStyles}>
         {children}
         <Typography variant="body1">
           Already have an account?
@@ -74,6 +57,36 @@ function GetStartSignUp({ onClickNext, children}) {
       </Box>
     </Box>
   );
-}
+};
 
 export default GetStartSignUp;
+
+const containerStyles = {
+  justifyContent: "center",
+  display: "flex",
+  flexDirection: "column",
+};
+
+const formContainerStyles = {
+  display: "flex",
+  flexDirection: "column",
+  gap: { xs: 3, md: 4 },
+};
+
+const inputContainerStyles = {
+  display: "flex",
+  flexDirection: "column",
+  gap: 0.5,
+};
+
+const checkboxContainerStyles = {
+  display: "flex",
+  alignItems: "center",
+};
+
+const buttonContainerStyles = {
+  display: "flex",
+  flexDirection: "column",
+  gap: 1,
+  textAlign: "center",
+};
