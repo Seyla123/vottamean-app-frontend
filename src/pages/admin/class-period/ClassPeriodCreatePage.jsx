be8d92c87a0e6ff50e935ad23b8c6b5858158582
Sok Chan Seiha <sokchanseiha@gmail.com>
2024-09-08 12:47:11 +0700

    feature: refactor-component-class-period, refactor on create page

--- src/pages/admin/class-period/ClassPeriodCreatePage.jsx
+++ src/pages/admin/class-period/ClassPeriodCreatePage.jsx
@@ -9,119 +9,188 @@ import FormComponent from "../../../components/common/FormComponent";
 import ButtonContainer from "../../../components/common/ButtonContainer";
 
 function ClassPeriodCreatePage() {
+  
   const [startTime, setStartTime] = useState(null);
   const [endTime, setEndTime] = useState(null);
   const [error, setError] = useState(false);
 
   // style
-  const containerButton = {
-    maxWidth: {lg: "364px", sm: " 350px"},
-    mt: "24px",
-    width: "100%",
-    display: "flex",
-    gap: { lg: "24px", xs: "16px" },
-  };
+  // const containerButton = {
+  //   maxWidth: { lg: "364px", sm: " 350px" },
+  //   mt: "24px",
+  //   width: "100%",
+  //   display: "flex",
+  //   gap: { lg: "24px", xs: "16px" },
+  // };
   const containerInput = {
     display: "flex",
     flexDirection: "column",
     gap: { xs: "16px", lg: "24px" },
   };
-  const form = { boxShadow: 1, p: {lg: "24px", xs: "16px"}, display: "flex", flexDirection: "column", gap: "24px"};
-  const button = { width: "100%", height: { lg: "42px", sm: "38px" } };
-  const section = { my: 0, fontSize: { lg: 32, xs: 20 } };
-  const title = { fontSize: { lg: 16, xs: 14 } };
-  const divider = { bgcolor: "black" };
+  // const form = {
+  //   boxShadow: 1,
+  //   p: { lg: "24px", xs: "16px" },
+  //   display: "flex",
+  //   flexDirection: "column",
+  //   gap: "24px",
+  // };
+  // const button = { width: "100%", height: { lg: "42px", sm: "38px" } };
+  // const section = { my: 0, fontSize: { lg: 32, xs: 20 } };
+  // const title = { fontSize: { lg: 16, xs: 14 } };
+  // const divider = { bgcolor: "black" };
   const timeInput = { width: "100%", mt: "4px" };
-  const content = { lg: "32px", xs: "24px" };
+  // const content = { lg: "32px", xs: "24px" };
 
-  const handleSubmit = () => {
+  const onClickNext = () => {
     if (!startTime || !endTime) {
       setError(true);
     } else {
       setError(false);
     }
   };
+  const onClickBack = () => {
+    console.log('back');
+  }
 
   return (
-    <div></div>
+    <>
+      <FormComponent title={"Update Class Period"} subTitle={"Please Fill Class Period Information"}>
+        <CardComponent title={"Class Period Information"}>
+        <LocalizationProvider dateAdapter={AdapterDayjs}>
+          <DemoContainer
+            components={["TimePicker", "TimePicker", "TimePicker"]}>
+            <Box sx={containerInput}>
+              <Box>
+                <Typography color={!startTime && error ? "red" : "inherit"}>
+                  Start time
+                </Typography>
+                <TimePicker
+                  sx={timeInput}
+                  value={startTime}
+                  onChange={(newValue) => setStartTime(newValue)}
+                  slotProps={{
+                    textField: {
+                      placeholder: "select",
+                      helperText:
+                        !startTime && error ? "Start time is required" : "",
+                      error: !startTime && error,
+                    },
+                  }}
+                />
+              </Box>
+              <Box>
+                <Typography color={!endTime && error ? "red" : "inherit"}>
+                  End time
+                </Typography>
+                <TimePicker
+                  sx={timeInput}
+                  value={endTime}
+                  onChange={(newValue) => setEndTime(newValue)}
+                  slotProps={{
+                    textField: {
+                      placeholder: "select",
+                      helperText:
+                        !endTime && error ? "End time is required" : "",
+                      error: !endTime && error,
+                      margin: "none",
+                      FormHelperTextProps: {},
+                    },
+                  }}
+                />
+              </Box>
+            </Box>
+          </DemoContainer>
+        </LocalizationProvider>
+          {/* Button Container  */}
+          <ButtonContainer
+            leftBtn={onClickBack}
+            rightBtn={onClickNext}
+            leftBtnTitle={"Cancel"}
+            rightBtnTitle={"Update"}
+          />
+        </CardComponent>
+      </FormComponent>
+    </>
   );
 }
 
 export default ClassPeriodCreatePage;
 
-const save = <Box>
-      {/* Title */}
-      <Box sx={{ my: content }}>
-        <Typography fontWeight="bold" sx={section}>
-          ADD CLASS PERIOD
-        </Typography>
-        <Typography color="textDisabled" sx={title}>
-          Please Fill Class Period Information
-        </Typography>
-      </Box>
-      <Card sx={form}>
-        <Typography sx={{ fontSize: 18 }} fontWeight="bold">
-          Class Period Information
-        </Typography>
-        <Divider sx={divider} />
-        <Box>
-          <LocalizationProvider dateAdapter={AdapterDayjs}>
-            <DemoContainer
-              components={["TimePicker", "TimePicker", "TimePicker"]}
-            >
-              <Box sx={containerInput}>
-                <Box>
-                  <Typography color={!startTime && error ? "red" : "inherit"}>
-                    Start time
-                  </Typography>
-                  <TimePicker
-                    sx={timeInput}
-                    value={startTime}
-                    onChange={(newValue) => setStartTime(newValue)}
-                    slotProps={{
-                      textField: {
-                        placeholder: "select",
-                        helperText:
-                          !startTime && error ? "Start time is required" : "",
-                        error: !startTime && error,
-                      },
-                    }}
-                  />
-                </Box>
-                <Box>
-                  <Typography color={!endTime && error ? "red" : "inherit"}>
-                    End time
-                  </Typography>
-                  <TimePicker
-                    sx={timeInput}
-                    value={endTime}
-                    onChange={(newValue) => setEndTime(newValue)}
-                    slotProps={{
-                      textField: {
-                        placeholder: "select",
-                        helperText:
-                          !endTime && error ? "End time is required" : "",
-                        error: !endTime && error,
-                        margin: "none",
-                        FormHelperTextProps: {},
-                      },
-                    }}
-                  />
-                </Box>
-              </Box>
-            </DemoContainer>
-          </LocalizationProvider>
+// const save = (
+//   <Box>
+//     {/* Title */}
+//     <Box sx={{ my: content }}>
+//       <Typography fontWeight="bold" sx={section}>
+//         ADD CLASS PERIOD
+//       </Typography>
+//       <Typography color="textDisabled" sx={title}>
+//         Please Fill Class Period Information
+//       </Typography>
+//     </Box>
+//     <Card sx={form}>
+//       <Typography sx={{ fontSize: 18 }} fontWeight="bold">
+//         Class Period Information
+//       </Typography>
+//       <Divider sx={divider} />
+//       <Box>
+        // <LocalizationProvider dateAdapter={AdapterDayjs}>
+        //   <DemoContainer
+        //     components={["TimePicker", "TimePicker", "TimePicker"]}
+        //   >
+        //     <Box sx={containerInput}>
+        //       <Box>
+        //         <Typography color={!startTime && error ? "red" : "inherit"}>
+        //           Start time
+        //         </Typography>
+        //         <TimePicker
+        //           sx={timeInput}
+        //           value={startTime}
+        //           onChange={(newValue) => setStartTime(newValue)}
+        //           slotProps={{
+        //             textField: {
+        //               placeholder: "select",
+        //               helperText:
+        //                 !startTime && error ? "Start time is required" : "",
+        //               error: !startTime && error,
+        //             },
+        //           }}
+        //         />
+        //       </Box>
+        //       <Box>
+        //         <Typography color={!endTime && error ? "red" : "inherit"}>
+        //           End time
+        //         </Typography>
+        //         <TimePicker
+        //           sx={timeInput}
+        //           value={endTime}
+        //           onChange={(newValue) => setEndTime(newValue)}
+        //           slotProps={{
+        //             textField: {
+        //               placeholder: "select",
+        //               helperText:
+        //                 !endTime && error ? "End time is required" : "",
+        //               error: !endTime && error,
+        //               margin: "none",
+        //               FormHelperTextProps: {},
+        //             },
+        //           }}
+        //         />
+        //       </Box>
+        //     </Box>
+        //   </DemoContainer>
+        // </LocalizationProvider>
 
-          <Box sx={{ display: "flex", justifyContent: "flex-end" }}>
-            <Stack direction="row" sx={containerButton}>
-              <Button sx={button} variant="outlined" color="black">
-                cancel
-              </Button>
-              <Button sx={button} variant="contained" onClick={handleSubmit}>
-                add period
-              </Button>
-            </Stack>
-          </Box>
-        </Box>
-      </Card>
-    </Box>
+//         <Box sx={{ display: "flex", justifyContent: "flex-end" }}>
+//           <Stack direction="row" sx={containerButton}>
+//             <Button sx={button} variant="outlined" color="black">
+//               cancel
+//             </Button>
+//             <Button sx={button} variant="contained" onClick={handleSubmit}>
+//               add period
+//             </Button>
+//           </Stack>
+//         </Box>
+//       </Box>
+//     </Card>
+//   </Box>
+// );
