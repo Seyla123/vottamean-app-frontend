770a755485e1cec4a153222ecc1768d6e83d617d
Nem Puthiraksa <raksasparkle@gmail.com>
2024-09-08 17:59:51 +0700

    feature : add Teacher Detail Page

--- src/pages/admin/teacher/TeacherDetailPage.jsx
+++ src/pages/admin/teacher/TeacherDetailPage.jsx
@@ -9,11 +9,30 @@ const clickDetele = ()=>{
   console.log('delete');
   
 }
+
+const teacher = {
+  "Full Name ": "John Doe",
+  "Email": "johndoe@example.com",
+  "Phone Number": "0923456789",
+  "Address": "123 Street, ABC City, ABC Country",
+  "Date of Birth": "1990-01-01",
+  "Gender": "Male",
+  "Degree": "Bachelor of Science in Computer Engineering",
+  "University": "University of Technology",
+  "Graduation Date": "2010-01-01",
+  "Marital Status": "Single",
+  "Religion": "Buddhist",
+  "Nationality": "Myanmar",
+}
 function TeacherDetailPage() {
   return (
 <FormComponent title={'Teacher Detail'} subTitle={'These are Teacher’s information'}>
-  <CardComponent title={"Teacher Information"} handleEdit={clickEdit} handleDelete={clickDetele}>
-    <CardInformation/>
+  <CardComponent 
+    title={"Teacher Information"} 
+    imgUrl='ddddd' 
+    handleEdit={clickEdit} 
+    handleDelete={clickDetele}>
+    <CardInformation  data={teacher} />
   </CardComponent>
 </FormComponent>
   )
