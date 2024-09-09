5b5f15db7c7835a1eca813aae093d33edd7c398c
NarakCODE <channarakluy@gmail.com>
2024-09-09 10:31:39 +0700

    Feature : Change table shadow

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -1,34 +1,40 @@
-import { createTheme } from '@mui/material/styles';
-import { responsiveFontSizes } from '@mui/material/styles';
+import { createTheme } from "@mui/material/styles";
+import { responsiveFontSizes } from "@mui/material/styles";
 
 // Define the theme
 let theme = createTheme({
-  palette: {
-    primary: {
-      main: '#2196F3',
+    palette: {
+        primary: {
+            main: "#2196F3",
+        },
+        secondary: {
+            main: "#797979",
+        },
+        text: {
+            disabled: "#9E9E9E",
+            secondary: "#797979",
+        },
     },
-    secondary: {
-      main: '#797979',
+    typography: {
+        // Define default font family
+        fontFamily: "Roboto, sans-serif",
+        // Define other typography settings as needed
+        h4: {
+            fontSize: "32px",
+        },
+        // Define for Form title
+        formTitle: {
+            fontSize: "18px",
+            fontFamily: "Roboto, sans-serif",
+            fontWeight: 500,
+        },
     },
-    text: {
-      disabled: '#9E9E9E',
-      secondary: '#797979',
-    },
-  },
-  typography: {
-    // Define default font family
-    fontFamily: 'Roboto, sans-serif',
-    // Define other typography settings as needed
-    h4:{
-      fontSize: '32px',
-    },
-    // Define for Form title
-    formTitle:{
-      fontSize: '18px',
-      fontFamily: 'Roboto, sans-serif',
-      fontWeight: 500,
-    },
-  }
+    shadows: [
+        "none", // elevation 0
+        "0px 5px 10px rgba(0,0,0,0.05)", // elevation 1
+        // Add other shadows as needed, or keep them as "none"
+        ...Array(23).fill("none"), // Fill remaining elevations with "none"
+    ],
 });
 
 // Apply responsive font sizes
