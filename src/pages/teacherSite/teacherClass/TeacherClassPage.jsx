01e7a911df774508a6de51f5abb55832f6a60a2c
Nem Puthiraksa <raksasparkle@gmail.com>
2024-09-09 15:56:49 +0700

    feature: teacher-class-page, add card component and colors

--- src/pages/teacherSite/teacherClass/TeacherClassPage.jsx
+++ src/pages/teacherSite/teacherClass/TeacherClassPage.jsx
@@ -1,89 +1,48 @@
-import React from 'react';
-import FormComponent from '../../../components/common/FormComponent';
-import { Card, Typography, CardContent, Avatar, Box } from '@mui/material';
-import classIcon from '../../../assets/icon/class.png';
-import { cardContainer } from '../../../styles/global';
-import { useTheme } from '@emotion/react';
-import WatchLaterIcon from '@mui/icons-material/WatchLater';
+import React from "react";
+import FormComponent from "../../../components/common/FormComponent";
+import CardDetail from "../../../components/teacherSite/CardDetail"; // import your card component
+import { Grid2 } from "@mui/material";
+import classIcon from "../../../assets/icon/list-class.jpg";
+
+// data
+const classesData = [
+  { id: 1, className: "E.109", day: "Monday", subject: "Math", students: 52, time: "7:00 - 9:00" },
+  { id: 2, className: "E.110", day: "Tuesday", subject: "Science", students: 45, time: "9:00 - 11:00" },
+  { id: 3, className: "E.111", day: "Wednesday", subject: "History", students: 30, time: "10:00 - 12:00" },
+  { id: 4, className: "E.112", day: "Thursday", subject: "Geography", students: 40, time: "11:00 - 1:00" },
+  { id: 5, className: "E.113", day: "Friday", subject: "Biology", students: 25, time: "1:00 - 3:00" },
+  { id: 6, className: "E.109", day: "Monday", subject: "Math", students: 52, time: "7:00 - 9:00" },
+  { id: 7, className: "E.110", day: "Tuesday", subject: "Science", students: 45, time: "9:00 - 11:00" },
+
+];
+
+//colors for card
+const colors = ["#D1E9F6", "#FFF4D2","#FFDBA4", "#E3F2C1","#F1D3CE" ,"#D2E0FB", ];
+
 function TeacherClassPage() {
-  const theme = useTheme();
-  const mobile = theme.breakpoints;
-
-  const subjectSize = {
-    [mobile.down("sm")]: {
-      fontSize: "24px",
-    },
-    [mobile.up("sm")]: {
-      fontSize: "32px",
-    },
-  };
-
-  const daySize = {
-    [mobile.down("sm")]: {
-      fontSize: "16px",
-    },
-    [mobile.up("sm")]: {
-      fontSize: "20px",
-    },
-  };
-
-  const classSize = {
-    [mobile.down("sm")]: {
-      fontSize: "16px",
-    },
-    [mobile.up("sm")]: {
-      fontSize: "18px",
-    },
-  };
-
   return (
     <>
-      {/* Header */}
-      <FormComponent title={'Class Today'} subTitle={'These are 10 classes'}>
-        {/* Card */}
-        <CardContent 
-          sx={{ 
-            ...cardContainer, 
-            borderRadius: '16px', 
-            height: {
-              xs: '200px',
-              sm: '250px'
-            },
-            display: 'flex',
-            flexDirection: 'column',
-            justifyContent: 'space-between', // Center the content vertically
-          }}
-        >
-          <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
-            {/* Class Name and Teacher Name */}
-            <Box sx={{ display: 'flex', alignItems: 'center', gap: '8px' }}>
-              <Avatar src={classIcon} alt='class list picture' />
-              <Typography sx={classSize} fontWeight="medium">
-                E.109
-              </Typography>
-            </Box>
-            {/* Day */}
-            <Typography sx={daySize} fontWeight="medium">
-              Monday
-            </Typography>
-          </Box>
-          {/* Subject */}
-          <Typography sx={subjectSize} fontWeight="bold" >
-            Math
-          </Typography>
-          {/* Total Students and Time */}
-          <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
-            <Typography sx={daySize}>
-              52 Students
-            </Typography>
-            <Box sx={{backgroundColor: '#fadfc1', padding: '4px 8px', borderRadius: '16px',display: 'flex', alignItems: 'center', gap: '8px'}}>
-              <WatchLaterIcon/>
-              <Typography sx={daySize}>
-                  7:00 - 9:00
-              </Typography>
-            </Box>
-          </Box>
-        </CardContent>
+      <FormComponent title="Class Today" subTitle="These are 7 classes">
+        <Grid2 container spacing={2}>
+          {classesData.map((classData, index) => {
+            // Random color
+            const cardColor = colors[index % colors.length];
+            // card
+            return (
+              <Grid2 item  size={{ xs: 12, md: 6 }} key={classData.id}>
+                <CardDetail
+                  className={classData.className}
+                  day={classData.day}
+                  subject={classData.subject}
+                  students={classData.students}
+                  time={classData.time}
+                  classIcon={classIcon}
+                  randomColor={cardColor} // Pass the determined color
+                />
+              </Grid2>
+            );
+          })}
+        </Grid2>
       </FormComponent>
     </>
   );
--- src/styles/theme.js
+++ src/styles/theme.js
@@ -3,32 +3,32 @@ import { responsiveFontSizes } from "@mui/material/styles";
 
 // Define the theme
 let theme = createTheme({
-    palette: {
-        primary: {
-            main: "#2196F3",
-        },
-        secondary: {
-            main: "#797979",
-        },
-        text: {
-            disabled: "#9E9E9E",
-            secondary: "#797979",
-        },
+  palette: {
+    primary: {
+      main: '#2196F3',
     },
-    typography: {
-        // Define default font family
-        fontFamily: "Roboto, sans-serif",
-        // Define other typography settings as needed
-        h4: {
-            fontSize: "32px",
-        },
-        // Define for Form title
-        formTitle: {
-            fontSize: "18px",
-            fontFamily: "Roboto, sans-serif",
-            fontWeight: 500,
-        },
+    secondary: {
+      main: '#797979',
     },
+    text: {
+      disabled: '#EEF7FE',
+      secondary: '#797979',
+    },
+  },
+  typography: {
+    // Define default font family
+    fontFamily: 'Roboto, sans-serif',
+    // Define other typography settings as needed
+    h4:{
+      fontSize: '32px',
+    },
+    // Define for Form title
+    formTitle:{
+      fontSize: '18px',
+      fontFamily: 'Roboto, sans-serif',
+      fontWeight: 500,
+    },
+  }
 });
 
 // Apply responsive font sizes
