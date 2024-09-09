799b22f02f36913cdb7a19a3de2937c16aac20c6
NarakCODE <channarakluy@gmail.com>
2024-09-09 10:11:11 +0700

    Feature : Data table

--- src/routes/routesConfig.jsx
+++ src/routes/routesConfig.jsx
@@ -1,312 +1,318 @@
-import React from 'react';
-import { lazy } from 'react';
+import React from "react";
+import { lazy } from "react";
+import TestPage from "../pages/TestPage";
 
 // Lazy-loaded Pages
-const LoginPage = lazy(() => import('../pages/auth/LoginPage'));
-const SignupPage = lazy(() => import('../pages/auth/SignupPage'));
+const LoginPage = lazy(() => import("../pages/auth/LoginPage"));
+const SignupPage = lazy(() => import("../pages/auth/SignupPage"));
 const PasswordForgotPage = lazy(
-    () => import('../pages/auth/PasswordForgotPage')
+    () => import("../pages/auth/PasswordForgotPage")
 );
-const PasswordResetPage = lazy(() => import('../pages/auth/PasswordResetPage'));
+const PasswordResetPage = lazy(() => import("../pages/auth/PasswordResetPage"));
 const PasswordChangePage = lazy(
-    () => import('../pages/auth/PasswordChangePage')
+    () => import("../pages/auth/PasswordChangePage")
 );
-const AccountVerifyPage = lazy(() => import('../pages/auth/AccountVerifyPage'));
+const AccountVerifyPage = lazy(() => import("../pages/auth/AccountVerifyPage"));
 const AccountSuccessPage = lazy(
-    () => import('../pages/auth/AccountSuccessPage')
+    () => import("../pages/auth/AccountSuccessPage")
 );
 
-const NotFoundPage = lazy(() => import('../pages/NotFoundPage'));
+const NotFoundPage = lazy(() => import("../pages/NotFoundPage"));
 
 const DashboardPage = lazy(
-    () => import('../pages/admin/dashboard/DashboardPage')
+    () => import("../pages/admin/dashboard/DashboardPage")
 );
 
 const SessionListPage = lazy(
-    () => import('../pages/admin/session/SessionListPage')
+    () => import("../pages/admin/session/SessionListPage")
 );
 const SessionDetailPage = lazy(
-    () => import('../pages/admin/session/SessionDetailPage')
+    () => import("../pages/admin/session/SessionDetailPage")
 );
 const SessionCreatePage = lazy(
-    () => import('../pages/admin/session/SessionCreatePage')
+    () => import("../pages/admin/session/SessionCreatePage")
 );
 const SessionUpdatePage = lazy(
-    () => import('../pages/admin/session/SessionUpdatePage')
+    () => import("../pages/admin/session/SessionUpdatePage")
 );
 
 const StudentListPage = lazy(
-    () => import('../pages/admin/student/StudentListPage')
+    () => import("../pages/admin/student/StudentListPage")
 );
 const StudentDetailPage = lazy(
-    () => import('../pages/admin/student/StudentDetailPage')
+    () => import("../pages/admin/student/StudentDetailPage")
 );
 const StudentCreatePage = lazy(
-    () => import('../pages/admin/student/StudentCreatePage')
+    () => import("../pages/admin/student/StudentCreatePage")
 );
 const StudentUpdatePage = lazy(
-    () => import('../pages/admin/student/StudentUpdatePage')
+    () => import("../pages/admin/student/StudentUpdatePage")
 );
 
 const SubjectListPage = lazy(
-    () => import('../pages/admin/subject/SubjectListPage')
+    () => import("../pages/admin/subject/SubjectListPage")
 );
 const SubjectDetailPage = lazy(
-    () => import('../pages/admin/subject/SubjectDetailPage')
+    () => import("../pages/admin/subject/SubjectDetailPage")
 );
 const SubjectCreatePage = lazy(
-    () => import('../pages/admin/subject/SubjectCreatePage')
+    () => import("../pages/admin/subject/SubjectCreatePage")
 );
 const SubjectUpdatePage = lazy(
-    () => import('../pages/admin/subject/SubjectUpdatePage')
+    () => import("../pages/admin/subject/SubjectUpdatePage")
 );
 
 const ClassPeriodListPage = lazy(
-    () => import('../pages/admin/class-period/ClassPeriodListPage')
+    () => import("../pages/admin/class-period/ClassPeriodListPage")
 );
 const ClassPeriodDetailPage = lazy(
-    () => import('../pages/admin/class-period/ClassPeriodDetailPage')
+    () => import("../pages/admin/class-period/ClassPeriodDetailPage")
 );
 const ClassPeriodCreatePage = lazy(
-    () => import('../pages/admin/class-period/ClassPeriodCreatePage')
+    () => import("../pages/admin/class-period/ClassPeriodCreatePage")
 );
 const ClassPeriodUpdatePage = lazy(
-    () => import('../pages/admin/class-period/ClassPeriodUpdatePage')
+    () => import("../pages/admin/class-period/ClassPeriodUpdatePage")
 );
 
 const TeacherListPage = lazy(
-    () => import('../pages/admin/teacher/TeacherListPage')
+    () => import("../pages/admin/teacher/TeacherListPage")
 );
 const TeacherDetailPage = lazy(
-    () => import('../pages/admin/teacher/TeacherDetailPage')
+    () => import("../pages/admin/teacher/TeacherDetailPage")
 );
 const TeacherCreatePage = lazy(
-    () => import('../pages/admin/teacher/TeacherCreatePage')
+    () => import("../pages/admin/teacher/TeacherCreatePage")
 );
 const TeacherUpdatePage = lazy(
-    () => import('../pages/admin/teacher/TeacherUpdatePage')
+    () => import("../pages/admin/teacher/TeacherUpdatePage")
 );
 
-const ClassListPage = lazy(() => import('../pages/admin/class/ClassListPage'));
+const ClassListPage = lazy(() => import("../pages/admin/class/ClassListPage"));
 const ClassDetailPage = lazy(
-    () => import('../pages/admin/class/ClassDetailPage')
+    () => import("../pages/admin/class/ClassDetailPage")
 );
 const ClassCreatePage = lazy(
-    () => import('../pages/admin/class/ClassCreatePage')
+    () => import("../pages/admin/class/ClassCreatePage")
 );
 const ClassUpdatePage = lazy(
-    () => import('../pages/admin/class/ClassUpdatePage')
+    () => import("../pages/admin/class/ClassUpdatePage")
 );
 
 const SchoolUpdatePage = lazy(
-    () => import('../pages/admin/school/SchoolUpdatePage')
+    () => import("../pages/admin/school/SchoolUpdatePage")
 );
 
 const AccountProfilePage = lazy(
-    () => import('../pages/admin/setting/account/AccountProfilePage')
+    () => import("../pages/admin/setting/account/AccountProfilePage")
 );
 
 const AttendanceReportPage = lazy(
-    () => import('../pages/admin/report/attendance/AttendanceReportPage')
+    () => import("../pages/admin/report/attendance/AttendanceReportPage")
 );
 
 // Routes configuration
 const routesConfig = [
     {
-        path: '/login',
+        path: "/test",
+        element: <TestPage />,
+        showSidebar: false,
+    },
+    {
+        path: "/login",
         element: <LoginPage />,
         showSidebar: false,
     },
     {
-        path: '/signup',
+        path: "/signup",
         element: <SignupPage />,
         showSidebar: false,
     },
     {
-        path: '/forgot-password',
+        path: "/forgot-password",
         element: <PasswordForgotPage />,
         showSidebar: false,
     },
     {
-        path: '/reset-password',
+        path: "/reset-password",
         element: <PasswordResetPage />,
         showSidebar: false,
     },
     {
-        path: '/change-password',
+        path: "/change-password",
         element: <PasswordChangePage />,
         showSidebar: false,
     },
     {
-        path: '/verify-account',
+        path: "/verify-account",
         element: <AccountVerifyPage />,
         showSidebar: false,
     },
     {
-        path: '/success',
+        path: "/success",
         element: <AccountSuccessPage />,
         showSidebar: false,
     },
     {
-        path: '/dashboard',
+        path: "/dashboard",
         showSidebar: true,
         children: [
             {
-                path: '',
+                path: "",
                 element: <DashboardPage />,
                 children: [],
             },
             {
-                path: 'sessions',
+                path: "sessions",
                 children: [
                     {
-                        path: '',
+                        path: "",
                         element: <SessionListPage />,
                     },
                     {
-                        path: ':id',
+                        path: ":id",
                         element: <SessionDetailPage />,
                     },
                     {
-                        path: 'create',
+                        path: "create",
                         element: <SessionCreatePage />,
                     },
                     {
-                        path: 'update/:id',
+                        path: "update/:id",
                         element: <SessionUpdatePage />,
                     },
                 ],
             },
             {
-                path: 'students',
+                path: "students",
                 children: [
                     {
-                        path: '',
+                        path: "",
                         element: <StudentListPage />,
                     },
                     {
-                        path: ':id',
+                        path: ":id",
                         element: <StudentDetailPage />,
                     },
                     {
-                        path: 'create',
+                        path: "create",
                         element: <StudentCreatePage />,
                     },
                     {
-                        path: 'update/:id',
+                        path: "update/:id",
                         element: <StudentUpdatePage />,
                     },
                 ],
             },
             {
-                path: 'subjects',
+                path: "subjects",
                 children: [
                     {
-                        path: '',
+                        path: "",
                         element: <SubjectListPage />,
                     },
                     {
-                        path: ':id',
+                        path: ":id",
                         element: <SubjectDetailPage />,
                     },
                     {
-                        path: 'create',
+                        path: "create",
                         element: <SubjectCreatePage />,
                     },
                     {
-                        path: 'update/:id',
+                        path: "update/:id",
                         element: <SubjectUpdatePage />,
                     },
                 ],
             },
             {
-                path: 'class-periods',
+                path: "class-periods",
                 children: [
                     {
-                        path: '',
+                        path: "",
                         element: <ClassPeriodListPage />,
                     },
                     {
-                        path: ':id',
+                        path: ":id",
                         element: <ClassPeriodDetailPage />,
                     },
                     {
-                        path: 'create',
+                        path: "create",
                         element: <ClassPeriodCreatePage />,
                     },
                     {
-                        path: 'update/:id',
+                        path: "update/:id",
                         element: <ClassPeriodUpdatePage />,
                     },
                 ],
             },
             {
-                path: 'teachers',
+                path: "teachers",
                 children: [
                     {
-                        path: '',
+                        path: "",
                         element: <TeacherListPage />,
                     },
                     {
-                        path: ':id',
+                        path: ":id",
                         element: <TeacherDetailPage />,
                     },
                     {
-                        path: 'create',
+                        path: "create",
                         element: <TeacherCreatePage />,
                     },
                     {
-                        path: 'update/:id',
+                        path: "update/:id",
                         element: <TeacherUpdatePage />,
                     },
                 ],
             },
             {
-                path: 'classes',
+                path: "classes",
                 element: <ClassListPage />,
                 children: [
                     {
-                        path: '',
+                        path: "",
                         element: <ClassListPage />,
                     },
                     {
-                        path: ':id',
+                        path: ":id",
                         element: <ClassDetailPage />,
                     },
                     {
-                        path: 'create',
+                        path: "create",
                         element: <ClassCreatePage />,
                     },
                     {
-                        path: 'update/:id',
+                        path: "update/:id",
                         element: <ClassUpdatePage />,
                     },
                 ],
             },
             {
-                path: 'school',
+                path: "school",
                 element: <SchoolUpdatePage />,
                 children: [
                     {
-                        path: 'update/:id',
+                        path: "update/:id",
                         element: <SchoolUpdatePage />,
                     },
                 ],
             },
             {
-                path: 'settings',
+                path: "settings",
                 children: [
                     {
-                        path: 'account',
+                        path: "account",
                         element: <AccountProfilePage />,
                     },
                 ],
             },
             {
-                path: 'reports',
+                path: "reports",
                 children: [
                     {
-                        path: 'attendance',
+                        path: "attendance",
                         element: <AttendanceReportPage />,
                     },
                 ],
@@ -314,7 +320,7 @@ const routesConfig = [
         ],
     },
     {
-        path: '*',
+        path: "*",
         element: <NotFoundPage />,
         showSidebar: false,
     },
