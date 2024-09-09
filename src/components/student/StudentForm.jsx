764f59e954f48770d42073194a829de1e48d3ee9
Seav <Mrrseyla.758@gmail.com>
2024-09-09 16:02:27 +0700

    Refactor : Student form

---
+++ src/components/student/StudentForm.jsx
@@ -0,0 +1,113 @@
+import { useState } from "react"
+import { LocalizationProvider, DesktopDatePicker } from "@mui/x-date-pickers";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import TextFieldComponent from "../common/TextFieldComponent";
+import SelectField from "../common/SelectField";
+import { Box, Typography } from "@mui/material";
+function StudentFrom() {
+    const [newStudent, setNewStudent] = useState({
+        firstName: "",
+        lastName: "",
+        className: "",
+        gender: "",
+        dob: null,
+        phoneNumber: "",
+        email: "",
+        address: "",
+      });
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+      setNewStudent((prev) => ({ ...prev, [name]: value }));
+  };
+  return (
+    <>
+    {/* first name */}
+      <TextFieldComponent
+        label="First Name"
+        name="firstName"
+        value={newStudent.firstName}
+        onChange={handleInputChange}
+        placeholder={"first name"}
+      />
+      {/* last name */}
+      <TextFieldComponent
+        label="Last Name"
+        name="lastName"
+        value={newStudent.lastName}
+        onChange={handleInputChange}
+        placeholder={"last name"}
+      />
+      {/* class */}
+      <SelectField
+        label="Class"
+        name="className"
+        value={newStudent.className}
+        onChange={handleInputChange}
+        options={classOptions}
+        placeholder={"select class"}
+      />
+      {/* gender */}
+      <SelectField
+        label="Gender"
+        name="gender"
+        value={newStudent.gender}
+        onChange={handleInputChange}
+        options={genderOptions}
+        placeholder={"select gender"}
+      />
+      {/* dob */}
+      <Box display="flex" flexDirection="column" gap="4px">
+        <Typography sx={{ fontSize: "16px" }}>Date of birth</Typography>
+        <LocalizationProvider dateAdapter={AdapterDayjs}>
+          <DesktopDatePicker
+
+            inputFormat="MM/DD/YYYY"
+            value={newStudent.dob}
+            onChange={(date) =>
+              setNewStudent((prev) => ({ ...prev, dob: date }))
+            }
+            renderInput={(params) => <TextFieldComponent {...params} />}
+          />
+        </LocalizationProvider>
+      </Box>
+      {/* phone number */}
+      <TextFieldComponent
+        label="Phone Number"
+        name="phoneNumber"
+        value={newStudent.phoneNumber}
+        onChange={handleInputChange}
+        placeholder={"phone number"}
+      />
+      {/* email */}
+      <TextFieldComponent
+        label="Email"
+        name="email"
+        value={newStudent.email}
+        onChange={handleInputChange}
+        placeholder={"email"}
+      />
+      {/* address */}
+      <TextFieldComponent
+        label="Address"
+        name="address"
+        value={newStudent.address}
+        onChange={handleInputChange}
+        placeholder={"address"}
+      />
+    </>
+  )
+}
+
+export default StudentFrom;
+const genderOptions = [
+    { value: "male", label: "Male" },
+    { value: "female", label: "Female" },
+    { value: "other", label: "Other" },
+  ];
+  
+  const classOptions = [
+    { value: "Class A", label: "Class A" },
+    { value: "Class B", label: "Class B" },
+  ];
+  
--- src/pages/admin/student/StudentCreatePage.jsx
+++ src/pages/admin/student/StudentCreatePage.jsx
@@ -1,17 +1,17 @@
-import React, { useState } from "react";
-import {OutlinedInput,Button,Box,Typography,Divider,useMediaQuery,Card,Select,MenuItem, Tab,Tabs} from "@mui/material";
-import { LocalizationProvider, DesktopDatePicker } from "@mui/x-date-pickers";
-import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
-import dayjs from "dayjs";
+import { useState } from "react";
+import {
+  Box,
+  Tab,
+  Tabs
+} from "@mui/material";
 import { useNavigate } from "react-router-dom";
 import FormComponent from "../../../components/common/FormComponent";
 import CardComponent from "../../../components/common/CardComponent";
 import ButtonContainer from "../../../components/common/ButtonContainer";
-
+import StudentFrom from "../../../components/student/StudentForm";
+import GardianForm from "../../../components/student/GardianForm";
 const StudentCreatePage = () => {
   const navigate = useNavigate();
-  const [newStudent, setNewStudent] = useState({firstName: "",lastName: "",className: "",gender: "",dob: dayjs(),phoneNumber: "",email: "",address: "",});
-  const [newGuardian, setNewGuardian] = useState({firstName: "",lastName: "",relationship: "",email: "",phoneNumber: "",});
   const [activeTab, setActiveTab] = useState(0);
 
   const handleCancel = () => {
@@ -26,299 +26,71 @@ const StudentCreatePage = () => {
     if (activeTab === 0) {
       setActiveTab(1);
     } else {
-      navigate("/dashboard/students"); 
+      navigate("/dashboard/students");
     }
   };
 
-  const handleInputChange = (e) => {
-    const { name, value } = e.target;
-    if (activeTab === 0) {
-      setNewStudent((prev) => ({ ...prev, [name]: value }));
-    } else {
-      setNewGuardian((prev) => ({ ...prev, [name]: value }));
-    }
+  const handleTabChange = (event, newValue) => {
+    setActiveTab(newValue);
   };
+
 
 
   return (
-    <>
-
-      <FormComponent
-        title={"Add Student"}
-        subTitle={"Please fill Students Indformation"}
-      >
-         <Tabs
-          value={activeTab}
-          onChange={(event, newValue) => setActiveTab(newValue)}
-          textColor="primary"
-          indicatorColor="primary"
+    <FormComponent title="Add Student" subTitle="Please Fill Student information">
+      {/* Tab  */}
+          <Tabs
+            value={activeTab}
+            onChange={handleTabChange}
+            aria-label="tabs example"
+            textColor="primary"
+            indicatorColor="primary"
+          >
+            <Tab label="Student Information" />
+            <Tab label="Guardian Information" />
+          </Tabs>
+      <CardComponent title="Student Information">
+        <Box
+          component="form"
+          sx={containerStyle}
         >
-          <Tab label="STUDENT INFORMATION" />
-          <Tab label="GUARDIAN INFORMATION" />
-        </Tabs>
-        
-        {activeTab === 0  && (
-      //  <Card>
-         
-        <CardComponent title={"Student Information"}>        
-        <Box sx={{display:'flex', justifyContent:'center'}}>
-            <Box sx={profile}>
-              <img
-                src="https://via.placeholder.com/100"
-                alt="Profile"
-                style={{ width: "100%", height: "100%", objectFit: "cover" }}
-              />
-            </Box>
-          </Box> 
-          <Box
-            sx={{display: "flex",flexDirection: "row",justifyContent: "space-between", gap: "12px", }}>
-            <Box sx={{ flex: 1 }}>
-              <Typography sx={{ fontSize: "16px" }}>First Name</Typography>
-              <OutlinedInput
-                name="firstName"
-                placeholder="First name"
-                value={newStudent.firstName}
-                onChange={handleInputChange}
-                fullWidth
-              
-              />
-            </Box>
-            <Box sx={{ flex: 1 }}>
-              <Typography sx={{ fontSize: "16px" }}>Last Name</Typography>
-              <OutlinedInput
-                name="lastName"
-                placeholder="Last name"
-                value={newStudent.lastName}
-                onChange={handleInputChange}
-                fullWidth
-              
-              />
-            </Box>
-          </Box>
-          <Box sx={{ width: "100%" ,py: { xs: "8px", sm: "12px" } }}>
-            <Typography sx={{ fontSize: "16px" }}>Class</Typography>
-            <Select
-              name="gender"
-              value={newStudent.gender}
-              onChange={handleInputChange}
-              fullWidth
-              displayEmpty
-            
-              renderValue={(selected) => {
-                if (!selected) {
-                    return <Box component="p" variant="body2" sx={{ color: '#B5B5B5' }}>class</Box>;
-                }
-                return selected;
-            }}
-            >
-              {selectType.map((option) => (
-                <MenuItem key={option.value} value={option.value}>
-                  {option.label}
-                </MenuItem>
-              ))}
-            </Select>
-          </Box>
-          <Box
-            sx={{
-              display: "flex",
-              flexDirection: "row",
-              gap: "12px",
-              alignItems: "center",
-            }}
-          >
-            <Box sx={{ width: "100%", flex: 1 }}>
-              <Typography sx={{ fontSize: "16px" }}>Gender</Typography>
-              <Select
-                name="gender"
-                value={newStudent.gender}
-                onChange={handleInputChange}
-                fullWidth
-                displayEmpty
-                renderValue={(selected) => {
-                  if (!selected) {
-                      return <Box component="p" variant="body2" sx={{ color: '#B5B5B5' }}>gender</Box>;
-                  }
-                  return selected;
-              }}
-              >
-                {genderOptions.map((option) => (
-                  <MenuItem key={option.value} value={option.value}>
-                    {option.label}
-                  </MenuItem>
-                ))}
-              </Select>
-            </Box>
-            {/* Form Date of birth*/}
-            <Box sx={{ width: "100%", flex: 1 }}>
-              <Typography>Date of Birth</Typography>
-              <LocalizationProvider dateAdapter={AdapterDayjs}>
-                <Box sx={{ width: "100%" }}>
-                  <DesktopDatePicker
-                    defaultValue={dayjs("2022-04-17T15:30")}
-                    sx={{
-                      width: "100%",
-                      "@media (max-width:600px)": { width: "100%" },
-                    }}
-                    renderInput={(params) => (
-                      <TextField {...params} fullWidth />
-                    )}
-                  />
-                </Box>
-              </LocalizationProvider>
-            </Box>
-          </Box>
-          <Box sx={{ width: "100%",py: { xs: "8px", sm: "12px" }  }}>
-            <Typography sx={{ fontSize: "16px" }}>Phone Number</Typography>
-            <OutlinedInput
-              name="phoneNumber"
-              placeholder="Phone number"
-              value={newStudent.phoneNumber}
-              onChange={handleInputChange}
-              fullWidth
-
-            />
-          </Box>
-          <Box sx={{ width: "100%", py: { xs: "8px", sm: "12px" } }}>
-            <Typography sx={{ fontSize: "16px" }}>Email</Typography>
-            <OutlinedInput
-              name="email"
-              placeholder="Email"
-              value={newStudent.email}
-              onChange={handleInputChange}
-              fullWidth
-            
-            />
-          </Box>
-          <Box sx={{ width: "100%", py: { xs: "8px", sm: "12px" } }}>
-            <Typography sx={{ fontSize: "16px" }}>Address</Typography>
-            <OutlinedInput
-              name="address"
-              placeholder="Address"
-              value={newStudent.address}
-              onChange={handleInputChange}
-              fullWidth
-            
-              multiline
-              rows={5}
-            />
-          </Box>
-          {/* Button cancel and Add*/}
-          <ButtonContainer
-            leftBtn={handleCancel}
-            rightBtn={handleNext}
-            leftBtnTitle={"Cancel"}
-            rightBtnTitle={"Add Student"}
-          />
-
-        </CardComponent>
-        //  </Card>
-      )}
-
-      {/* Gurdian INformation */}
-      {activeTab === 1 && (
-        <CardComponent title={"Guardian Information"}>
-          <Box
-            sx={{
-              display: "flex",
-              flexDirection: "row",
-              justifyContent: "space-between",
-              gap: "12px",
-            }}
-          >
-            <Box sx={{ flex: 1 }}>
-              <Typography sx={{ fontSize: "16px" }}>First Name</Typography>
-              <OutlinedInput
-                name="firstName"
-                placeholder="First name"
-                value={newGuardian.firstName}
-                onChange={handleInputChange}
-                fullWidth
-              
-              />
-            </Box>
-            <Box sx={{ flex: 1 }}>
-              <Typography sx={{ fontSize: "16px" }}>Last Name</Typography>
-              <OutlinedInput
-                name="lastName"
-                placeholder="Last name"
-                value={newGuardian.lastName}
-                onChange={handleInputChange}
-                fullWidth
-              
-              />
-            </Box>
-          </Box>
-          <Box sx={{ width: "100%", py: { xs: "8px", sm: "12px" } }}>
-            <Typography sx={{ fontSize: "16px" }}>
-              Guardian's Relationship
-            </Typography>
-            <OutlinedInput
-              name="relationship"
-              placeholder="Relationship"
-              value={newGuardian.relationship}
-              onChange={handleInputChange}
-              fullWidth
-            
-            />
-          </Box>
-          <Box sx={{ width: "100%", py: { xs: "8px", sm: "12px" } }}>
-            <Typography sx={{ fontSize: "16px" }}>Email</Typography>
-            <OutlinedInput
-              name="email"
-              placeholder="Email"
-              value={newGuardian.email}
-              onChange={handleInputChange}
-              fullWidth
-            
-            />
-          </Box>
-          <Box sx={{ width: "100%", py: { xs: "8px", sm: "12px" } }}>
-            <Typography sx={{ fontSize: "16px" }}>Phone Number</Typography>
-            <OutlinedInput
-              name="phoneNumber"
-              placeholder="Phone number"
-              value={newGuardian.phoneNumber}
-              onChange={handleInputChange}
-              fullWidth
-            
-            />
-          </Box>
-          {/* Button cancel and Add*/}
-          <ButtonContainer
-            leftBtn={handleCancel}
-            rightBtn={handleNext}
-            leftBtnTitle={"Cancel"}
-            rightBtnTitle={"Add Guardian"}
-          />
-        </CardComponent>
-      )}
-      </FormComponent>
-       </>
+          {/* stundet tab */}
+          {activeTab === 0 && (
+           <StudentFrom/>
+          )}
+          {/* guardian tab */}
+          {activeTab === 1 && (
+            <GardianForm/>
+          )}
+        </Box>
+        {/* buttons container */}
+        <ButtonContainer
+          leftBtn={handleCancel}
+          rightBtn={handleNext}
+          leftBtnTitle="Cancel"
+          rightBtnTitle={activeTab === 0 ? "Next" : "Add Student"}
+        />
+      </CardComponent>
+    </FormComponent>
   );
 };
 
 export default StudentCreatePage;
-
+const containerStyle = {
+  display: "grid",
+  gap: { xs: "12px", md: "24px" },
+  gridTemplateColumns: {
+    xs: "repeat(1, 1fr)",
+    md: "repeat(2, 1fr)",
+  },
+}
 const genderOptions = [
   { value: "male", label: "Male" },
   { value: "female", label: "Female" },
   { value: "other", label: "Other" },
 ];
-const selectType = [
+
+const classOptions = [
   { value: "Class A", label: "Class A" },
   { value: "Class B", label: "Class B" },
 ];
-
-const profile = {
-  width: 100,
-  height: 100,
-  borderRadius: "50%",
-  overflow: "hidden",
-  display: "flex",
-  justifyContent: "center",
-  "& img": {
-    width: "100%",
-    height: "100%",
-    objectFit: "cover",
-  },
-};
