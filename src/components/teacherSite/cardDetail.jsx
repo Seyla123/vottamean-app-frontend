0d82431f4641c0f8bdc01b20dbe3b681ac3e6ec8
Nem Puthiraksa <raksasparkle@gmail.com>
2024-09-09 20:37:48 +0700

    feature : teacher-class-page, add icons and more design for card

--- src/components/teacherSite/cardDetail.jsx
+++ src/components/teacherSite/cardDetail.jsx
@@ -1,42 +1,89 @@
-import React from 'react';
-import { CardContent, Avatar, Box, Typography } from '@mui/material';
-import AccessTimeIcon from '@mui/icons-material/AccessTime';
-import { cardContainer } from '../../styles/global';
-import theme from '../../styles/theme';
-const CardDetail = ({ className, day, subject, students, time, classIcon, randomColor }) => {
+import React from "react";
+import { CardContent, Box, Typography, Stack, Chip, Avatar } from "@mui/material";
+import AccessTimeIcon from "@mui/icons-material/AccessTime";
+import { cardContainer } from "../../styles/global";
+import { styled } from "@mui/material/styles";
+import PermIdentityIcon from '@mui/icons-material/PermIdentity';
+import TodayIcon from '@mui/icons-material/Today';
+const IconContainer = styled(Box)({
+  width: '60px',
+  height: '60px',
+  backgroundColor: 'white',
+  padding: '10px',
+  borderRadius: '50%',
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+});
 
+const CardDetail = ({
+  className,
+  day,
+  subject,
+  students,
+  time,
+  classIcon,
+  randomColor,
+}) => {
   return (
-    <CardContent sx={{...cardStyle, bgcolor: randomColor,}}>
-      <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
-        <Box sx={{ display: 'flex', alignItems: 'center', gap: '8px' }}>
+    <CardContent sx={{ ...cardStyle, bgcolor: randomColor }}>
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+        }}
+      >
+        <Box sx={{ display: "flex", alignItems: "center", gap: "8px" }}>
           {/* class icon */}
-          <Avatar src={classIcon} alt='class list picture' sx={{ width: '40px', height: '40px' }} />
+          <IconContainer sx={{ display: "flex", alignItems: "center"}}>
+            <Avatar
+              variant="square"
+              src={classIcon}
+              alt="class list picture"
+              sx={{ width: 40, height: 40 }}
+            />
+          </IconContainer>
+          
           {/* class */}
           <Typography sx={classSize} fontWeight="medium">
             {className}
           </Typography>
         </Box>
         {/* day */}
-        <Typography sx={daySize}>
-          {day}
-        </Typography>
+        <Box sx={{ display: "flex", alignItems: "center", gap: "8px" }}>
+          <Typography sx={daySize}>{day}</Typography>
+          <TodayIcon />
+        </Box>
       </Box>
       {/* subject */}
-      <Typography sx={subjectSize}>
-        {subject}
-      </Typography>
-      <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+      <Typography sx={subjectSize}>{subject}</Typography>
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+        }}
+      >
         {/* total students */}
-        <Typography sx={timeSize}>
-          {students} <span style={{ color: theme.palette.secondary.main }}>Students</span>
-        </Typography>
+        <Stack sx={timeSize}>
+          <Chip  
+          label={students}
+          color="black"
+          icon={<PermIdentityIcon/>}>
+          <span>Students</span>
+          </Chip>
+        </Stack>
         {/* time */}
-        <Box sx={boxStyle}>
-          <AccessTimeIcon sx={{ padding: '2px', borderRadius: '50%' }} />
-          <Typography sx={timeSize}>
-            {time}
-          </Typography>
-        </Box>
+        <Stack direction="row" spacing={1}>
+          <Chip
+            label={time}
+            sx={{ bgcolor: "#7bba3b", color: "white" }}
+            size=""
+            variant="contained"
+            icon={<AccessTimeIcon color="inherit"/>} 
+          />
+        </Stack>
       </Box>
     </CardContent>
   );
@@ -44,41 +91,32 @@ const CardDetail = ({ className, day, subject, students, time, classIcon, random
 
 export default CardDetail;
 
-const boxStyle = { 
-  backgroundColor: '#387F39', 
-  color: 'white',
-  padding: '3px 8px', 
-  borderRadius: '16px',
-  display: 'flex',
-  alignItems: 'center',
-  gap: '8px' }
+const cardStyle = {
+  ...cardContainer,
+  borderRadius: "16px",
+  height: { xs: "200px", sm: "250px" },
+  display: "flex",
+  flexDirection: "column",
+};
 
-  // styles for mobile
-  const subjectSize = {
-    fontSize: { xs: "24px", sm: "32px" },
-    fontWeight: "medium",
-    letterSpacing: 0.5,
-  };
+// styles for mobile
+const subjectSize = {
+  fontSize: { xs: "24px", sm: "32px" },
+  fontWeight: "medium",
+  letterSpacing: 0.5,
+};
 
-  const daySize = {
-    fontSize: { xs: "16px", sm: "18px" },
-    fontWeight: "medium"
-  };
+const daySize = {
+  fontSize: { xs: "16px", sm: "18px" },
+  fontWeight: "medium",
+};
 
-  const timeSize = {
-    fontSize: { xs: "14px", sm: "16px" },
-    fontWeight: "medium"
-  };
+const timeSize = {
+  fontSize: { xs: "14px", sm: "16px" },
+  fontWeight: "medium",
+};
 
-  const classSize = {
-    fontSize: { xs: "18px", sm: "20px" },
-    fontWeight: "medium"
-  };
-
-  const cardStyle = {
-    ...cardContainer,
-    borderRadius: '16px',
-    height: { xs: '200px', sm: '250px' }, 
-    display: 'flex', flexDirection: 'column',
-    justifyContent: 'space-between'
-  }
+const classSize = {
+  fontSize: { xs: "20px", sm: "22px" },
+  fontWeight: "medium",
+};
--- src/pages/teacherSite/teacherClass/TeacherClassPage.jsx
+++ src/pages/teacherSite/teacherClass/TeacherClassPage.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import FormComponent from "../../../components/common/FormComponent";
 import CardDetail from "../../../components/teacherSite/CardDetail"; // import your card component
 import { Grid2 } from "@mui/material";
-import classIcon from "../../../assets/icon/list-class.jpg";
+import teacher from "../../../assets/icon/teacher.png";
 
 // data
 const classesData = [
@@ -12,17 +12,17 @@ const classesData = [
   { id: 4, className: "E.112", day: "Thursday", subject: "Geography", students: 40, time: "11:00 - 1:00" },
   { id: 5, className: "E.113", day: "Friday", subject: "Biology", students: 25, time: "1:00 - 3:00" },
   { id: 6, className: "E.109", day: "Monday", subject: "Math", students: 52, time: "7:00 - 9:00" },
-  { id: 7, className: "E.110", day: "Tuesday", subject: "Science", students: 45, time: "9:00 - 11:00" },
+  { id: 7, className: "E.122", day: "Tuesday", subject: "Science", students: 45, time: "9:00 - 11:00" },
 
 ];
 
 //colors for card
-const colors = ["#D1E9F6", "#FFF4D2","#FFDBA4", "#E3F2C1","#F1D3CE" ,"#D2E0FB", ];
+const colors = ["#e7f7ff", "#fffaeb","#ffebcb", "#f1fcd9","#ffece9" ,"#e7eaff", ];
 
 function TeacherClassPage() {
   return (
     <>
-      <FormComponent title="Class Today" subTitle="These are 7 classes">
+      <FormComponent title="Class Schedule" subTitle="These are 7 classes">
         <Grid2 container spacing={2}>
           {classesData.map((classData, index) => {
             // Random color
@@ -36,7 +36,7 @@ function TeacherClassPage() {
                   subject={classData.subject}
                   students={classData.students}
                   time={classData.time}
-                  classIcon={classIcon}
+                  classIcon={teacher}
                   randomColor={cardColor} // Pass the determined color
                 />
               </Grid2>
@@ -45,7 +45,7 @@ function TeacherClassPage() {
         </Grid2>
       </FormComponent>
     </>
-  );
+  ); 
 }
 
 export default TeacherClassPage;
