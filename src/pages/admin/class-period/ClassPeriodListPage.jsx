92e0b57f97e73e6e5713fbd08b7f70c54d0e2388
NarakCODE <channarakluy@gmail.com>
2024-09-09 16:27:48 +0700

    Feature : Reusable CRUD DataTable

--- src/pages/admin/class-period/ClassPeriodListPage.jsx
+++ src/pages/admin/class-period/ClassPeriodListPage.jsx
@@ -1,7 +1,9 @@
-import { useMediaQuery, Stack, Box, Button } from '@mui/material';
+import React from 'react';
+import { Button } from '@mui/material';
 import FormComponent from '../../../components/common/FormComponent';
 import { useNavigate } from 'react-router-dom';
 import DataTable from '../../../components/common/DataTable';
+
 
 function ClassPeriodListPage() {
     const navigate = useNavigate();
@@ -12,146 +14,136 @@ function ClassPeriodListPage() {
         { id: 'period', label: 'Period' },
     ];
 
+    const rows = [
+        {
+            id: 1,
+            start: '8:00 AM',
+            end: '5:00 PM',
+            period: '8h',
+        },
+        {
+            id: 2,
+            start: '8:00 AM',
+            end: '5:00 PM',
+            period: '8h',
+        },
+        {
+            id: 3,
+            start: '8:00 AM',
+            end: '5:00 PM',
+            period: '8h',
+        },
+        {
+            id: 4,
+            start: '8:00 AM',
+            end: '5:00 PM',
+            period: '8h',
+        },
+        {
+            id: 5,
+            start: '8:00 AM',
+            end: '5:00 PM',
+            period: '8h',
+        },
+        {
+            id: 6,
+            start: '8:00 AM',
+            end: '5:00 PM',
+            period: '8h',
+        },
+        {
+            id: 7,
+            start: '8:00 AM',
+            end: '5:00 PM',
+            period: '8h',
+        },
+        {
+            id: 8,
+            start: '8:00 AM',
+            end: '5:00 PM',
+            period: '8h',
+        },
+        {
+            id: 9,
+            start: '8:00 AM',
+            end: '5:00 PM',
+            period: '8h',
+        },
+        {
+            id: 10,
+            start: '8:00 AM',
+            end: '5:00 PM',
+            period: '8h',
+        },
+        {
+            id: 11,
+            start: '8:00 AM',
+            end: '5:00 PM',
+            period: '8h',
+        },
+        {
+            id: 12,
+            start: '8:00 AM',
+            end: '5:00 PM',
+            period: '8h',
+        },
+        {
+            id: 13,
+            start: '8:00 AM',
+            end: '5:00 PM',
+            period: '8h',
+        },
+        {
+            id: 14,
+            start: '8:00 AM',
+            end: '5:00 PM',
+            period: '8h',
+        },
+    ];
+
     const handleCreate = () => {
         navigate(`/dashboard/class-periods/create`);
     };
 
     const handleEdit = row => {
-        navigate(`/dashboard/class-periods/update${row.id}1`);
+        navigate(`/dashboard/class-periods/update/${row.id}`);
     };
 
-    const handleDelete = row => {};
+    const handleDelete = row => {
+        console.log('Delete row:', row);
+    };
 
-    const handleSelectedDelete = () => {};
+    const handleSelectedDelete = () => {
+        console.log('Delete all');
+    };
 
-    const hideColumns = ['id'];
+    const hideColumns = ['start'];
 
     return (
-        <Box>
-            <FormComponent
-                title={'Class Period List'}
-                subTitle={'There are 24 Class Periods'}
+        <FormComponent
+            title={'Class Period List'}
+            subTitle={'There are 24 Class Periods'}
+        >
+            <Button
+                variant='contained'
+                sx={{ width: '170px', mb: 2, alignSelf: 'flex-end' }}
+                onClick={handleCreate}
             >
-                <Stack
-                    sx={{
-                        width: 1,
-                        display: 'flex',
-                        flexDirection: 'row',
-                        justifyContent: 'flex-end',
-                    }}
-                >
-                    <Button
-                        variant='contained'
-                        sx={{ width: '170px' }}
-                        onClick={handleCreate}
-                    >
-                        ADD SESSION
-                    </Button>
-                </Stack>
+                ADD CLASS PERIOD
+            </Button>
 
-                {/* List Session */}
-                <DataTable
-                    rows={rows}
-                    columns={columns}
-                    onSelectedDelete={handleSelectedDelete}
-                    hiddenColumns={hideColumns}
-                    onEdit={handleEdit}
-                    onDelete={handleDelete}
-                    emptyTitle={'No Session'}
-                    emptySubTitle={'No Session Available'}
-                />
-            </FormComponent>
-        </Box>
+            <DataTable
+                rows={rows}
+                columns={columns}
+                onEdit={handleEdit}
+                onDelete={handleDelete}
+                onSelectedDelete={handleSelectedDelete}
+                hideColumns={hideColumns}
+                emptyTitle={'No Session'}
+                emptySubTitle={'No Session Available'}
+            />
+        </FormComponent>
     );
 }
 
 export default ClassPeriodListPage;
-
-const rows = [
-    {
-        id: 1,
-        start: '8:00 AM',
-        end: '5:00 PM',
-        period: '8h',
-    },
-    {
-        id: 2,
-        start: '8:00 AM',
-        end: '5:00 PM',
-        period: '8h',
-    },
-    {
-        id: 3,
-        start: '8:00 AM',
-        end: '5:00 PM',
-        period: '8h',
-    },
-    {
-        id: 4,
-        start: '8:00 AM',
-        end: '5:00 PM',
-        period: '8h',
-    },
-    {
-        id: 5,
-        start: '8:00 AM',
-        end: '5:00 PM',
-        period: '8h',
-    },
-    {
-        id: 6,
-        start: '8:00 AM',
-        end: '5:00 PM',
-        period: '8h',
-    },
-    {
-        id: 7,
-        start: '8:00 AM',
-        end: '5:00 PM',
-        period: '8h',
-    },
-    {
-        id: 8,
-        start: '8:00 AM',
-        end: '5:00 PM',
-        period: '8h',
-    },
-    {
-        id: 9,
-        start: '8:00 AM',
-        end: '5:00 PM',
-        period: '8h',
-    },
-    {
-        id: 10,
-        start: '8:00 AM',
-        end: '5:00 PM',
-        period: '8h',
-    },
-    {
-        id: 11,
-        start: '8:00 AM',
-        end: '5:00 PM',
-        period: '8h',
-    },
-    {
-        id: 12,
-        start: '8:00 AM',
-        end: '5:00 PM',
-        period: '8h',
-    },
-    {
-        id: 13,
-        start: '8:00 AM',
-        end: '5:00 PM',
-        period: '8h',
-    },
-    {
-        id: 14,
-        start: '8:00 AM',
-        end: '5:00 PM',
-        period: '8h',
-    },
-];
-
-const paginationModel = { page: 0, pageSize: 10 };
