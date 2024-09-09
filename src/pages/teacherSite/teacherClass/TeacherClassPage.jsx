db40d610dc6410abb2dc4e7e8164f86ebc60dc69
Nem Puthiraksa <raksasparkle@gmail.com>
2024-09-09 13:10:44 +0700

    feature : teacher-class-page, add card design

--- src/pages/teacherSite/teacherClass/TeacherClassPage.jsx
+++ src/pages/teacherSite/teacherClass/TeacherClassPage.jsx
@@ -1,9 +1,92 @@
-import React from 'react'
-
+import React from 'react';
+import FormComponent from '../../../components/common/FormComponent';
+import { Card, Typography, CardContent, Avatar, Box } from '@mui/material';
+import classIcon from '../../../assets/icon/class.png';
+import { cardContainer } from '../../../styles/global';
+import { useTheme } from '@emotion/react';
+import WatchLaterIcon from '@mui/icons-material/WatchLater';
 function TeacherClassPage() {
+  const theme = useTheme();
+  const mobile = theme.breakpoints;
+
+  const subjectSize = {
+    [mobile.down("sm")]: {
+      fontSize: "24px",
+    },
+    [mobile.up("sm")]: {
+      fontSize: "32px",
+    },
+  };
+
+  const daySize = {
+    [mobile.down("sm")]: {
+      fontSize: "16px",
+    },
+    [mobile.up("sm")]: {
+      fontSize: "20px",
+    },
+  };
+
+  const classSize = {
+    [mobile.down("sm")]: {
+      fontSize: "16px",
+    },
+    [mobile.up("sm")]: {
+      fontSize: "18px",
+    },
+  };
+
   return (
-    <div>TeacherClassPage</div>
-  )
+    <>
+      {/* Header */}
+      <FormComponent title={'Class Today'} subTitle={'These are 10 classes'}>
+        {/* Card */}
+        <CardContent 
+          sx={{ 
+            ...cardContainer, 
+            borderRadius: '16px', 
+            height: {
+              xs: '200px',
+              sm: '250px'
+            },
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'space-between', // Center the content vertically
+          }}
+        >
+          <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+            {/* Class Name and Teacher Name */}
+            <Box sx={{ display: 'flex', alignItems: 'center', gap: '8px' }}>
+              <Avatar src={classIcon} alt='class list picture' />
+              <Typography sx={classSize} fontWeight="medium">
+                E.109
+              </Typography>
+            </Box>
+            {/* Day */}
+            <Typography sx={daySize} fontWeight="medium">
+              Monday
+            </Typography>
+          </Box>
+          {/* Subject */}
+          <Typography sx={subjectSize} fontWeight="bold" >
+            Math
+          </Typography>
+          {/* Total Students and Time */}
+          <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+            <Typography sx={daySize}>
+              52 Students
+            </Typography>
+            <Box sx={{backgroundColor: '#fadfc1', padding: '4px 8px', borderRadius: '16px',display: 'flex', alignItems: 'center', gap: '8px'}}>
+              <WatchLaterIcon/>
+              <Typography sx={daySize}>
+                  7:00 - 9:00
+              </Typography>
+            </Box>
+          </Box>
+        </CardContent>
+      </FormComponent>
+    </>
+  );
 }
 
-export default TeacherClassPage
+export default TeacherClassPage;
