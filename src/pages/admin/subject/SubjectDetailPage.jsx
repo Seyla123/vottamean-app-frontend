596de4fc52766bd3b93f04ee9e5651b517e007c2
Seav <Mrrseyla.758@gmail.com>
2024-09-09 10:59:28 +0700

    feature : add subject detail form

--- src/pages/admin/subject/SubjectDetailPage.jsx
+++ src/pages/admin/subject/SubjectDetailPage.jsx
@@ -1,8 +1,30 @@
-import React from 'react'
+import FormComponent from "../../../components/common/FormComponent"
+import CardComponent from "../../../components/common/CardComponent"
+import CardInformation from "../../../components/common/CardInformation"
 
+const clickEdit = () => {
+  console.log('edit')
+}
+const clickDetele = ()=>{
+  console.log('delete');
+  
+}
+
+const subject = {
+  "Subject ID" : "00101",
+  "Subject Name": "Potato Fried",
+  "Description" : "This is a subject for learning how to cook potato fried."
+}
 function SubjectDetailPage() {
   return (
-    <div>SubjectDetailPage</div>
+<FormComponent title={'Subject Detail'} subTitle={'These are Subject’s information'}>
+  <CardComponent 
+    title={"Subject Information"} 
+    handleEdit={clickEdit} 
+    handleDelete={clickDetele}>
+    <CardInformation  data={subject} />
+  </CardComponent>
+</FormComponent>
   )
 }
 
