2e3cc43947e6ecd46014f865a8f0608ba83f4044
NarakCODE <channarakluy@gmail.com>
2024-09-11 16:41:18 +0700

    Fix : Change the navigation file & clear warning console

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,141 +1,35 @@
-import React from "react";
-import { useNavigate, useLocation, Outlet } from "react-router-dom";
-import { AppProvider } from "@toolpad/core/AppProvider";
-import Logo from "../../assets/images/Logo.png";
-import theme from "../../styles/theme";
+import React from 'react';
+import { useNavigate, useLocation, Outlet } from 'react-router-dom';
+import { AppProvider } from '@toolpad/core/AppProvider';
+import Logo from '../../assets/images/Logo.png';
+import { extendTheme } from '@mui/material';
 
-import {
-    HouseIcon as HomeIcon,
-    CalendarIcon as CalendarMonthIcon,
-    BookIcon as ClassIcon,
-    LibraryIcon as SubjectIcon,
-    GraduationCapIcon as StudentIcon,
-    BriefcaseBusinessIcon as TeacherIcon,
-    SquareChartGanttIcon as ClassPeriodIcon,
-    ClipboardMinusIcon as ReportGmailerrorredIcon,
-    ScrollTextIcon as ChecklistIcon,
-    SettingsIcon,
-    LogOutIcon,
-    UserRoundIcon as Person,
-} from "lucide-react";
-import { Box } from "@mui/system";
-import { DashboardLayout } from "@toolpad/core/DashboardLayout";
-import { Container } from "@mui/material";
+import { Box } from '@mui/system';
+import { DashboardLayout } from '@toolpad/core/DashboardLayout';
+import { Container } from '@mui/material';
+import { teacherSiteNavigation, navigation } from '../../data/navigation';
 
-const navigation = [
-    { kind: "header", title: "Menu" },
-    {
-        segment: "dashboard",
-        title: "Dashboard",
-        icon: <HomeIcon />,
-    },
-    {
-        segment: "dashboard/sessions",
-        title: "Session",
-        icon: <CalendarMonthIcon />,
-    },
-    {
-        segment: "dashboard/classes",
-        title: "Class",
-        icon: <ClassIcon />,
-    },
-    {
-        segment: "dashboard/subjects",
-        title: "Subject",
-        icon: <SubjectIcon />,
-    },
-    {
-        segment: "dashboard/students",
-        title: "Student",
-        icon: <StudentIcon />,
-    },
-    {
-        segment: "dashboard/teachers",
-        title: "Teacher",
-        icon: <TeacherIcon />,
-    },
-    {
-        segment: "dashboard/class-periods",
-        title: "Class Period",
-        icon: <ClassPeriodIcon />,
-    },
-    {
-        segment: "dashboard/reports",
-        title: "Report",
-        icon: <ReportGmailerrorredIcon />,
-        children: [
-            {
-                segment: "attendance",
-                title: "Attendance",
-                icon: <ChecklistIcon />,
-            },
-        ],
-    },
-
-    { kind: "divider" },
-
-    { kind: "header", title: "General" },
-
-    {
-        segment: "dashboard/settings",
-        title: "Settings",
-        icon: <SettingsIcon />,
-        children: [
-            {
-                segment: "account",
-                title: "Account",
-                icon: <Person />,
-            },
-        ],
-    },
-];
-
-const teacherSiteNavigation = [
-    { kind: "header", title: "Menu" },
-    {
-        segment: "dashboard",
-        title: "Dashboard",
-        icon: <HomeIcon />,
-    },
-    {
-        segment: "dashboard/reports",
-        title: "Report",
-        icon: <ReportGmailerrorredIcon />,
-        children: [
-            {
-                segment: "attendance",
-                title: "Attendance",
-                icon: <ChecklistIcon />,
-            },
-        ],
-    },
-
-    { kind: "divider" },
-
-    { kind: "header", title: "General" },
-
-    {
-        segment: "dashboard/settings",
-        title: "Settings",
-        icon: <SettingsIcon />,
-        children: [
-            {
-                segment: "account",
-                title: "Account",
-                icon: <Person />,
-            },
-        ],
-    },
-];
 const Layout = ({ teacherSite, adminSite }) => {
     const navigate = useNavigate();
     const location = useLocation();
 
     const [session, setSession] = React.useState({
         user: {
-            name: "Selena",
-            email: "selena@gmail.com",
-            image: "https://images.unsplash.com/photo-1534528741775-53994a69daeb?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+            name: 'Selena',
+            email: 'selena@gmail.com',
+            image: 'https://images.unsplash.com/photo-1534528741775-53994a69daeb?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        },
+    });
+
+    const theme = extendTheme({
+        breakpoints: {
+            values: {
+                xs: 0,
+                sm: 600,
+                md: 1200,
+                lg: 1200,
+                xl: 1536,
+            },
         },
     });
 
@@ -144,9 +38,9 @@ const Layout = ({ teacherSite, adminSite }) => {
             signIn: () => {
                 setSession({
                     user: {
-                        name: "Selena",
-                        email: "selena@gmail.com",
-                        image: "https://images.unsplash.com/photo-1534528741775-53994a69daeb?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+                        name: 'Selena',
+                        email: 'selena@gmail.com',
+                        image: 'https://images.unsplash.com/photo-1534528741775-53994a69daeb?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
                     },
                 });
             },
@@ -160,14 +54,24 @@ const Layout = ({ teacherSite, adminSite }) => {
         return {
             pathname: location.pathname,
             searchParams: new URLSearchParams(location.search),
-            navigate: (path) => navigate(path),
+            navigate: path => navigate(path),
         };
     }, [location, navigate]);
     return (
         <AppProvider
             branding={{
-                title: "",
-                logo: <img src={Logo} alt="WaveTrack" />,
+                title: '',
+                logo: (
+                    <img
+                        src={Logo}
+                        alt='WaveTrack'
+                        style={{
+                            width: '150px',
+                            height: '100%',
+                            objectFit: 'contain',
+                        }}
+                    />
+                ),
             }}
             navigation={teacherSite ? teacherSiteNavigation : navigation}
             router={router}
@@ -180,7 +84,7 @@ const Layout = ({ teacherSite, adminSite }) => {
                 <Box paddingTop={2}>
                     {/* Render other components or nothing based on your requirements */}
                     <DashboardLayout>
-                        <Container maxWidth="xl">
+                        <Container maxWidth='xl'>
                             <Outlet />
                         </Container>
                     </DashboardLayout>
