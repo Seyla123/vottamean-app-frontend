e17dab1fbf3ba298604c0b80a31888a0bd25baea
NarakCODE <channarakluy@gmail.com>
2024-09-11 11:34:09 +0700

    Feature : Change the profile image

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -90,107 +90,105 @@ const navigation = [
 ];
 
 const teacherSiteNavigation = [
-  { kind: "header", title: "Menu" },
-  {
-    segment: "dashboard",
-    title: "Dashboard",
-    icon: <HomeIcon />,
-  },
-  {
-    segment: "dashboard/reports",
-    title: "Report",
-    icon: <ReportGmailerrorredIcon />,
-    children: [
-      {
-        segment: "attendance",
-        title: "Attendance",
-        icon: <ChecklistIcon />,
-      },
-    ],
-  },
+    { kind: "header", title: "Menu" },
+    {
+        segment: "dashboard",
+        title: "Dashboard",
+        icon: <HomeIcon />,
+    },
+    {
+        segment: "dashboard/reports",
+        title: "Report",
+        icon: <ReportGmailerrorredIcon />,
+        children: [
+            {
+                segment: "attendance",
+                title: "Attendance",
+                icon: <ChecklistIcon />,
+            },
+        ],
+    },
 
-  { kind: "divider" },
+    { kind: "divider" },
 
-  { kind: "header", title: "General" },
+    { kind: "header", title: "General" },
 
-  {
-    segment: "dashboard/settings",
-    title: "Settings",
-    icon: <SettingsIcon />,
-    children: [
-      {
-        segment: "account",
-        title: "Account",
-        icon: <Person />,
-      },
-    ],
-  },
+    {
+        segment: "dashboard/settings",
+        title: "Settings",
+        icon: <SettingsIcon />,
+        children: [
+            {
+                segment: "account",
+                title: "Account",
+                icon: <Person />,
+            },
+        ],
+    },
 ];
 const Layout = ({ teacherSite, adminSite }) => {
-  const navigate = useNavigate();
-  const location = useLocation();
+    const navigate = useNavigate();
+    const location = useLocation();
 
-  const [session, setSession] = React.useState({
-    user: {
-      name: "Doggo",
-      email: "doggo@gmail.com",
-      image:
-        "https://i.pinimg.com/236x/10/18/97/10189726fde11a8182c4ff075bfe094b.jpg",
-    },
-  });
+    const [session, setSession] = React.useState({
+        user: {
+            name: "Selena",
+            email: "selena@gmail.com",
+            image: "https://images.unsplash.com/photo-1534528741775-53994a69daeb?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        },
+    });
 
-  const authentication = React.useMemo(() => {
-    return {
-      signIn: () => {
-        setSession({
-          user: {
-            name: "Doggo",
-            email: "doggo@gmail.com",
-            image:
-              "https://i.pinimg.com/236x/10/18/97/10189726fde11a8182c4ff075bfe094b.jpg",
-          },
-        });
-      },
-      signOut: () => {
-        setSession(null);
-      },
-    };
-  }, []);
+    const authentication = React.useMemo(() => {
+        return {
+            signIn: () => {
+                setSession({
+                    user: {
+                        name: "Selena",
+                        email: "selena@gmail.com",
+                        image: "https://images.unsplash.com/photo-1534528741775-53994a69daeb?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+                    },
+                });
+            },
+            signOut: () => {
+                setSession(null);
+            },
+        };
+    }, []);
 
-  const router = React.useMemo(() => {
-    return {
-      pathname: location.pathname,
-      searchParams: new URLSearchParams(location.search),
-      navigate: (path) => navigate(path),
-    };
-  }, [location, navigate]);
-  return (
-    <AppProvider
-      branding={{
-        title: "",
-        logo: <img src={Logo} alt="WaveTrack" />,
-      }}
-      navigation={teacherSite ? teacherSiteNavigation : navigation}
-      router={router}
-      theme={theme}
-      session={session}
-      authentication={authentication}
-    >
-    {/* Render the component to show different sidebar layout in teacher and admin site */}
-      {teacherSite || adminSite ? (
-        <Box paddingTop={2}>
-          {/* Render other components or nothing based on your requirements */}
-          <DashboardLayout>
-            <Container maxWidth="xl">
-              <Outlet />
-            </Container>
-          </DashboardLayout>
-        </Box>
-      ) : (
-        <Outlet />
-      )}
-    </AppProvider>
-  );
+    const router = React.useMemo(() => {
+        return {
+            pathname: location.pathname,
+            searchParams: new URLSearchParams(location.search),
+            navigate: (path) => navigate(path),
+        };
+    }, [location, navigate]);
+    return (
+        <AppProvider
+            branding={{
+                title: "",
+                logo: <img src={Logo} alt="WaveTrack" />,
+            }}
+            navigation={teacherSite ? teacherSiteNavigation : navigation}
+            router={router}
+            theme={theme}
+            session={session}
+            authentication={authentication}
+        >
+            {/* Render the component to show different sidebar layout in teacher and admin site */}
+            {teacherSite || adminSite ? (
+                <Box paddingTop={2}>
+                    {/* Render other components or nothing based on your requirements */}
+                    <DashboardLayout>
+                        <Container maxWidth="xl">
+                            <Outlet />
+                        </Container>
+                    </DashboardLayout>
+                </Box>
+            ) : (
+                <Outlet />
+            )}
+        </AppProvider>
+    );
 };
 
 export default Layout;
