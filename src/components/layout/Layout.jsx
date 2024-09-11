20178e7fc40a8316ebf3752c1879e001d30c0fc5
NarakCODE <channarakluy@gmail.com>
2024-09-11 11:14:19 +0700

    Feature : Resize the logo

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -4,16 +4,16 @@ import { AppProvider } from "@toolpad/core/AppProvider";
 import Logo from "../../assets/images/Logo.png";
 import theme from "../../styles/theme";
 import {
-  Home as HomeIcon,
-  CalendarMonth as CalendarMonthIcon,
-  Class as ClassIcon,
-  Subject as SubjectIcon,
-  School as StudentIcon,
-  CoPresent as TeacherIcon,
-  LibraryBooks as ClassPeriodIcon,
-  Settings as SettingsIcon,
-  Logout as LogoutIcon,
-  Person,
+    Home as HomeIcon,
+    CalendarMonth as CalendarMonthIcon,
+    Class as ClassIcon,
+    Subject as SubjectIcon,
+    School as StudentIcon,
+    CoPresent as TeacherIcon,
+    LibraryBooks as ClassPeriodIcon,
+    Settings as SettingsIcon,
+    Logout as LogoutIcon,
+    Person,
 } from "@mui/icons-material";
 import ReportGmailerrorredIcon from "@mui/icons-material/ReportGmailerrorred";
 import ChecklistIcon from "@mui/icons-material/Checklist";
@@ -22,71 +22,71 @@ import { DashboardLayout } from "@toolpad/core/DashboardLayout";
 import { Container } from "@mui/material";
 
 const navigation = [
-  { kind: "header", title: "Menu" },
-  {
-    segment: "dashboard",
-    title: "Dashboard",
-    icon: <HomeIcon />,
-  },
-  {
-    segment: "dashboard/sessions",
-    title: "Session",
-    icon: <CalendarMonthIcon />,
-  },
-  {
-    segment: "dashboard/classes",
-    title: "Class",
-    icon: <ClassIcon />,
-  },
-  {
-    segment: "dashboard/subjects",
-    title: "Subject",
-    icon: <SubjectIcon />,
-  },
-  {
-    segment: "dashboard/students",
-    title: "Student",
-    icon: <StudentIcon />,
-  },
-  {
-    segment: "dashboard/teachers",
-    title: "Teacher",
-    icon: <TeacherIcon />,
-  },
-  {
-    segment: "dashboard/class-periods",
-    title: "Class Period",
-    icon: <ClassPeriodIcon />,
-  },
-  {
-    segment: "dashboard/reports",
-    title: "Report",
-    icon: <ReportGmailerrorredIcon />,
-    children: [
-      {
-        segment: "attendance",
-        title: "Attendance",
-        icon: <ChecklistIcon />,
-      },
-    ],
-  },
+    { kind: "header", title: "Menu" },
+    {
+        segment: "dashboard",
+        title: "Dashboard",
+        icon: <HomeIcon />,
+    },
+    {
+        segment: "dashboard/sessions",
+        title: "Session",
+        icon: <CalendarMonthIcon />,
+    },
+    {
+        segment: "dashboard/classes",
+        title: "Class",
+        icon: <ClassIcon />,
+    },
+    {
+        segment: "dashboard/subjects",
+        title: "Subject",
+        icon: <SubjectIcon />,
+    },
+    {
+        segment: "dashboard/students",
+        title: "Student",
+        icon: <StudentIcon />,
+    },
+    {
+        segment: "dashboard/teachers",
+        title: "Teacher",
+        icon: <TeacherIcon />,
+    },
+    {
+        segment: "dashboard/class-periods",
+        title: "Class Period",
+        icon: <ClassPeriodIcon />,
+    },
+    {
+        segment: "dashboard/reports",
+        title: "Report",
+        icon: <ReportGmailerrorredIcon />,
+        children: [
+            {
+                segment: "attendance",
+                title: "Attendance",
+                icon: <ChecklistIcon />,
+            },
+        ],
+    },
 
-  { kind: "divider" },
+    { kind: "divider" },
 
-  { kind: "header", title: "General" },
+    { kind: "header", title: "General" },
 
-  {
-    segment: "dashboard/settings",
-    title: "Settings",
-    icon: <SettingsIcon />,
-    children: [
-      {
-        segment: "account",
-        title: "Account",
-        icon: <Person />,
-      },
-    ],
-  },
+    {
+        segment: "dashboard/settings",
+        title: "Settings",
+        icon: <SettingsIcon />,
+        children: [
+            {
+                segment: "account",
+                title: "Account",
+                icon: <Person />,
+            },
+        ],
+    },
 ];
 
 const teacherSiteNavigation = [
