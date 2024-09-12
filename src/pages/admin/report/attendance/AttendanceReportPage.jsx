fadd12c4ca9d80c8e927b6a44f2a521f86c213e2
Nem Puthiraksa <raksasparkle@gmail.com>
2024-09-12 10:29:23 +0700

    feature : report-attendance-page, add design and grid layout to icon card

--- src/pages/admin/report/attendance/AttendanceReportPage.jsx
+++ src/pages/admin/report/attendance/AttendanceReportPage.jsx
@@ -1,36 +1,39 @@
-import React, { useState } from "react";
+import React, { useMemo, useState } from "react";
 import FormComponent from "../../../../components/common/FormComponent";
-import FilterComponent from "../../../../components/common/FilterComponent";
-import { Button, Stack, Box, IconButton } from "@mui/material";
+import {
+  Grid,
+  Typography,
+  Box,
+  Avatar,
+  Stack,
+  TableContainer,
+  Button,
+} from "@mui/material";
+import { DownloadIcon } from "lucide-react";
 import AttendanceTable from "../../../../components/teacherSite/AttendanceTable";
-import {DownloadIcon} from 'lucide-react'
-const columns = [
-  {
-    id: "id",
-    label: "ID",
-  },
-  {
-    id: "name",
-    label: "Name",
-  },
-  {
-    id: "time",
-    label: "Time",
-  },
-  {
-    id: "subject",
-    label: "Subject",
-  },
-  {
-    id: "class",
-    label: "Class",
-  },
-  {
-    id: "address",
-    label: "Address",
-  },
-];
+import CalendarImage from "../../../../assets/icon/calendar.png";
+import FilterComponent from "../../../../components/common/FilterComponent";
+import { shadow } from "../../../../styles/global";
+import {
+  CheckCheckIcon,
+  AlarmClockIcon,
+  CircleX,
+  PencilIcon,
+} from "lucide-react";
+import theme from "../../../../styles/theme";
 
+
+// --- data --------------------------------
+// column styles
+const columns = [
+  { id: "id", label: "ID" },
+  { id: "name", label: "Name" },
+  { id: "time", label: "Time" },
+  { id: "subject", label: "Subject" },
+  { id: "class", label: "Class" },
+  { id: "address", label: "Address" },
+];
+// data
 const data = [
   {
     id: 31,
@@ -41,10 +44,9 @@ const data = [
     address: "Takeo",
     status: "Present",
   },
-
   {
     id: 21,
-    name: "Teddy ",
+    name: "Teddy",
     time: "7:00 - 11:00",
     subject: "Web Development",
     class: "Class 2",
@@ -53,120 +55,222 @@ const data = [
   },
 ];
 
-function AttendanceReportPage() {
-  // States for each filter
+const AttendanceReportPage = () => {
+  const [rows, setRows] = useState(data);
   const [subjectValue, setSubjectValue] = useState("");
   const [classValue, setClassValue] = useState("");
   const [filterValue, setFilterValue] = useState("");
 
-  // Handle change based on filter type
-  const handleChange = (event, newValue, type) => {
-    if (type === "subject") {
-      setSubjectValue(newValue);
-    } else if (type === "class") {
-      setClassValue(newValue);
-    } else if (type === "filter") {
-      setFilterValue(newValue);
-    }
-  };
-
-  // Data
+  // --- Filters --------------------------------
+  // subjects
   const subjects = [
     { value: "Math", label: "Math" },
     { value: "Science", label: "Science" },
     { value: "English", label: "English" },
   ];
+  // handle subjects
+  const handleSubjectChange = (event) => {
+    setSubjectValue(event.target.value);
+  };
+  // classes
   const classes = [
     { value: "Class A", label: "Class A" },
     { value: "Class B", label: "Class B" },
     { value: "Class C", label: "Class C" },
   ];
+  //handle class
+  const handleClassChange = (event) => {
+    setClassValue(event.target.value);
+  };
+  // filter
   const filter = [
     { value: "All", label: "All" },
-    { value: "Present", label: "Present" },
-    { value: "Absent", label: "Absent" },
+    { value: "Today", label: "Today" },
+  ];
+  // handle filter
+  const handleFilterChange = (event) => {
+    setFilterValue(event.target.value);
+  };
+
+  // Status updates
+  const statusDetails = [
+    { label: "Present", color: "#28a745", icon: CheckCheckIcon },
+    { label: "Late", color: "#fd7e14", icon: AlarmClockIcon },
+    { label: "Permission", color: "#007bff", icon: PencilIcon },
+    { label: "Absent", color: "#dc3545", icon: CircleX },
   ];
 
-  const [rows, setRows] = useState(data);
-  const status = ["Present", "Absent", "Late", "Permission"];
-  const hideColumns = ['time', 'subject','address'];
+  // --- Table status change functions --------------------------------
   const handleStatusChange = (updatedRow, newStatus) => {
-    setRows(prevRows =>
-        prevRows.map(row =>
-            row.id === updatedRow.id ? { ...row, status: newStatus } : row
-        )
+    setRows((prevRows) =>
+      prevRows.map((row) =>
+        row.id === updatedRow.id ? { ...row, status: newStatus } : row
+      )
     );
-    console.log(`Changed ${updatedRow.name} to ${newStatus}`);
-};
+  };
+
+  // --- Count status --------------------------------
+  const statusCounts = {};
+  rows.forEach((row) => {
+    if (!statusCounts[row.status]) {
+      statusCounts[row.status] = 1;
+    } else {
+      statusCounts[row.status]++;
+    }
+  });
+
+  // handle export 
+  const handleExport = () => {
+    console.log('click')
+  }
+
   return (
-    <>
-      <FormComponent
-        title={"Report Attendance"}
-        subTitle={`These are ${rows.length} records`}
-      >
+    <FormComponent title={"Attendance Report"} subTitle={"Report"}>
+      {/* Cards Grid */}
+      <Box sx={cardGrid}>
+        <Box sx={boxGrid}>
+          <Typography variant="h4" fontWeight={"bold"}>
+            Today's report
+          </Typography>
+          <Avatar
+            variant="square"
+            src={CalendarImage}
+            alt="Calendar Icon"
+            sx={avatar}
+          />
+        </Box>
+        <Box sx={{ width: "100%", ...shadow }}>
+          <Grid column={14} container sx={gridContainer}>
+            {statusDetails.map(({ icon: Icon, label, color }, index) => (
+              <Grid key={index} item xs={6} sm={3} sx={gridIcon}>
+                <Avatar sx={{ bgcolor: color, width: 40, height: 40, mb: 1 }}>
+                  <Icon color="white" size={20} />
+                </Avatar>
+                <Typography
+                  variant="body2"
+                  sx={{ color: theme.palette.secondary.main }}
+                >
+                  {label}
+                </Typography>
+                <Typography variant="h5">
+                  {statusCounts[label]?.toString().padStart(2, "0") || "00"}
+                </Typography>
+              </Grid>
+            ))}
+          </Grid>
+        </Box>
+      </Box>
+
+      {/* Filter */}
+      <Box sx={boxStyle}>
         <Box
           sx={{
             display: "flex",
-            flexDirection: { xs: "column-reverse", sm: "row" },
-            justifyContent: "space-between",
-            
+            flexDirection: "row",
+            gap: 2,
+            alignSelf: "start",
           }}
         >
-          <Stack
-            sx={{
-              display: "flex",
-              flexDirection: "row",
-              gap: 1,
-            }}
-          >
-            {/* subject */}
-            <FilterComponent
-              value={subjectValue}
-              data={subjects}
-              onChange={(e, newValue) => handleChange(e, newValue, "subject")}
-              placeholder={"By Subject"}
-              customStyles={filterStyle}
-            />
-            {/* classes */}
-            <FilterComponent
-              value={classValue}
-              data={classes}
-              onChange={(e, newValue) => handleChange(e, newValue, "class")}
-              placeholder={"By Class"}
-              customStyles={filterStyle}
-            />
-            {/* filter */}
-            <FilterComponent
-              value={filterValue}
-              data={filter}
-              onChange={(e, newValue) => handleChange(e, newValue, "filter")}
-              placeholder={"Filter"}
-              customStyles={filterStyle}
-            />
-          </Stack>
-          <Stack >
-            <Button sx={{ width:'fit-content' , alignSelf:'flex-end'}} variant="contained" color="primary" startIcon={<DownloadIcon size={20}/>}>
-              EXPORT
-            </Button>
-          </Stack>
+          <FilterComponent
+            value={subjectValue}
+            data={subjects}
+            onChange={handleSubjectChange}
+            placeholder={"By Subject"}
+          />
+          <FilterComponent
+            value={classValue}
+            data={classes}
+            onChange={handleClassChange}
+            placeholder={"By Class"}
+          />
         </Box>
-        <AttendanceTable
+        <Box alignSelf={"end"}>
+          <FilterComponent
+            value={filterValue}
+            data={filter}
+            onChange={handleFilterChange}
+            placeholder={"Filter"}
+          />
+           
+        </Box>
+      </Box>
 
-            rows={rows}
-            columns={columns}
-            status={status}
-            hideColumns={hideColumns}
-            onStatusChange={handleStatusChange}
-        />
-      </FormComponent>
-    </>
+      {/* Table */}
+      <AttendanceTable
+        rows={rows}
+        columns={columns}
+        status={statusDetails.map((s) => s.label)}
+        hideColumns={["time", "subject", "class", "address"]}
+        onStatusChange={handleStatusChange}
+      />
+
+      <Button
+        variant="contained"
+        endIcon={<DownloadIcon size={16} />}
+        onClick={handleExport}
+        sx={{ alignSelf: "flex-end" }}
+      >
+        Export List
+      </Button>
+    </FormComponent>
   );
-}
+};
 
 export default AttendanceReportPage;
-const filterStyle = {
-  width: {
-    xs: "126px",
-    sm: "154px",
-  },
+
+
+const boxStyle = {
+  display: "flex",
+  flexDirection: "row",
+  justifyContent: "space-between",
+  alignItems: "center",
+  marginTop: 4,
+  width: "100%",
+  gap: 2,
 };
+const cardGrid = {
+  display: "flex",
+  flexDirection: { md: "row", xs: "column" },
+  gap: 1,
+  justifyContent: "space-between",
+  alignItems: "center",
+  width: "100%",
+}
+const gridContainer = {
+  display: "flex",
+  flexDirection: "row",
+  alignItems: "center",
+  justifyContent: "center",
+  p: 1,
+  width: "100%",
+  justifyContent: "center",
+}
+const gridIcon = {
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  justifyContent: "center",
+  textAlign: "center",
+  width: "100%",
+  p: 1,
+  overflow: "hidden",
+}
+const avatar ={
+  width: {
+    xs: "80px",
+    sm: "100px",
+  },
+  height: {
+    xs: "80px",
+    sm: "100px",
+  },
+  objectFit: "cover",
+}
+const boxGrid = {
+  display: "flex",
+  justifyContent: "space-between",
+  alignItems: "center",
+  width: "100%",
+  p: 2,
+  ...shadow,
+}
