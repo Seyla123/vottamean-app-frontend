089a0da516f489d8083e6c95c94e6def99b67a24
Seav <Mrrseyla.758@gmail.com>
2024-09-19 15:52:08 +0700

    fix : fix filter component

--- src/components/common/FilterComponent.jsx
+++ src/components/common/FilterComponent.jsx
@@ -1,25 +1,32 @@
-import React from 'react'
-import { Select,Box, MenuItem } from '@mui/material';
+import React from 'react';
+import { Select, MenuItem, Box } from '@mui/material';
 import { longText } from '../../styles/global';
-function FilterComponent({data,placeholder,value,onChange,customStyles}) {
+
+const FilterComponent = ({ data, placeholder, value, onChange, customStyles }) => {
+  const selectedLabel = data.find(item => item.value === value)?.label || placeholder;
+
   return (
     <Select
-    value={value}
-    onChange={onChange}
-    displayEmpty
-    sx={{...longText,...customStyles}}
-    renderValue={selected => {
-        if (selected.length === 0) {
-            return <Box component="p" variant="body2" sx={{ color: '#B5B5B5' }}>{placeholder}</Box>;
-        }
-        return selected;
-    }}
->
-    {data.map((item) => (
-        <MenuItem key={item.value} value={item.value}>{item.label}</MenuItem>
-    ))}
-</Select>
-  )
-}
+      value={value}
+      onChange={onChange}
+      displayEmpty
+      sx={{ ...longText, ...customStyles }}
+      renderValue={(selected) => (
+        <Box component="p" variant="body2" sx={{ color: value ? 'inherit' : '#B5B5B5' }}>
+          {selectedLabel}
+        </Box>
+      )}
+    >
+      <MenuItem value="" disabled>
+        {placeholder}
+      </MenuItem>
+      {data.map((item) => (
+        <MenuItem key={item.value} value={item.value}>
+          {item.label}
+        </MenuItem>
+      ))}
+    </Select>
+  );
+};
 
 export default FilterComponent;
