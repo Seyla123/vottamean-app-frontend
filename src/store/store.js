023c549aff515532ab6fc8ae19bfceb222ce2f31
Seav <Mrrseyla.758@gmail.com>
2024-09-20 18:01:59 +0700

    file : add attendance reducer

--- src/store/store.js
+++ src/store/store.js
@@ -3,15 +3,19 @@ import { setupListeners } from '@reduxjs/toolkit/query';
 import { authApi } from '../services/authApi';
 import formReducer from './slices/formSlice';
 import authReducer from './slices/authSlice';
+import attendanceReducer from './slices/attendanceSlice';
+import { attendanceApi } from '../services/attendanceApi';
 
 const store = configureStore({
   reducer: {
     form: formReducer,
     auth: authReducer,
+    attendance : attendanceReducer,
+    [attendanceApi.reducerPath]: attendanceApi.reducer,
     [authApi.reducerPath]: authApi.reducer,
   },
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(authApi.middleware),
+    getDefaultMiddleware().concat(authApi.middleware, attendanceApi.middleware),
 });
 
 setupListeners(store.dispatch);
