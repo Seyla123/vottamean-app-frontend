9085297049d0fa4cabeba8ef20df186a992ccf53
Sveita <sveitast7@gmail.com>
2024-09-23 11:46:24 +0700

    feature : format data from api to the needed form

--- src/pages/admin/session/SessionDetailPage.jsx
+++ src/pages/admin/session/SessionDetailPage.jsx
@@ -3,31 +3,42 @@ import CardComponent from "../../../components/common/CardComponent"
 import CardInformation from "../../../components/common/CardInformation"
 import { useGetSessionByIdQuery } from "../../../services/sessionApi"
 import  LoadingCircle  from "../../../components/loading/LoadingCircle"
-import { useNavigate } from "react-router-dom"
+import { useNavigate,useParams } from "react-router-dom"
+import { calculatePeriod } from "../../../utils/formatData"
 
 
 function SessionDetailPage() {
-  const {data , error, isLoading} = useGetSessionByIdQuery(); 
+  // get the id from the route params
+  const {id} = useParams();
+  const {data , error, isLoading} = useGetSessionByIdQuery(id); 
+  // const [remove , result] = useDeleteSessionMutation();
 
   const navigate = useNavigate();
   
   const clickEdit = () => {
-    navigate(`/admin/sessions/update/${data.data.session_id}`);
+    navigate(`/admin/sessions/update/${id}`);
   }
 
   const clickDetele = ()=>{
-    console.log('delete');
-    
+    // remove(id)
+  }
+
+  if(isLoading){
+    return <LoadingCircle/>
+  }
+
+  if(error){
+    return <div>Error loading session: {error.message}</div>
   }
   
-  const session = {
-    "Teach By" : "Seav Seyla",
-    "Time": "7:00 - 8:00",
-    "Period":"1h",
-    "Subject":"Math",
-    "Class":"A05",
-    "Day of week":"Monday",
-  }
+  const session = data?.data ? {
+    "Teach By": `${data.data.Teacher.Info.first_name} ${data.data.Teacher.Info.last_name}`,
+    "Time": `${data.data.Period.start_time} - ${data.data.Period.end_time}`,
+    "Period": calculatePeriod(data.data.Period.start_time, data.data.Period.end_time),
+    "Subject": data.data.Subject.name,
+    "Class": data.data.Class.class_name,
+    "Day of week": data.data.DayOfWeek.day,
+  } : {};
 
   return (
 <FormComponent title={'Session Detail'} subTitle={"These are Session's information"}>
