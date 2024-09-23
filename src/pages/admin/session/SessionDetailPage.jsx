aa57961e7cf3a3c6ccf7ad26b492bff4047d04b2
Sveita <sveitast7@gmail.com>
2024-09-23 10:41:46 +0700

    feature : import useNavigate and navigate edit button to update session  page

--- src/pages/admin/session/SessionDetailPage.jsx
+++ src/pages/admin/session/SessionDetailPage.jsx
@@ -1,24 +1,34 @@
 import FormComponent from "../../../components/common/FormComponent"
 import CardComponent from "../../../components/common/CardComponent"
 import CardInformation from "../../../components/common/CardInformation"
+import { useGetSessionByIdQuery } from "../../../services/sessionApi"
+import  LoadingCircle  from "../../../components/loading/LoadingCircle"
+import { useNavigate } from "react-router-dom"
 
-const clickEdit = () => {
-  console.log('edit')
-}
-const clickDetele = ()=>{
-  console.log('delete');
-  
-}
 
-const session = {
-  "Teach By" : "Seav Seyla",
-  "Time": "7:00 - 8:00",
-  "Period":"1h",
-  "Subject":"Math",
-  "Class":"A05",
-  "Day of week":"Monday",
-}
 function SessionDetailPage() {
+  const {data , error, isLoading} = useGetSessionByIdQuery(); 
+
+  const navigate = useNavigate();
+  
+  const clickEdit = () => {
+    navigate(`/admin/sessions/update/${data.data.session_id}`);
+  }
+
+  const clickDetele = ()=>{
+    console.log('delete');
+    
+  }
+  
+  const session = {
+    "Teach By" : "Seav Seyla",
+    "Time": "7:00 - 8:00",
+    "Period":"1h",
+    "Subject":"Math",
+    "Class":"A05",
+    "Day of week":"Monday",
+  }
+
   return (
 <FormComponent title={'Session Detail'} subTitle={"These are Session's information"}>
   <CardComponent 
