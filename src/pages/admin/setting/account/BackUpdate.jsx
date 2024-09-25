36c2bd5bb4b1edf890a0782e8a8a2d3cd076ce4c
Seang Makaramarady <seangdy085@gmail.com>
2024-09-25 08:49:27 +0700

    test : testing update user

--- src/pages/admin/setting/account/BackUpdate.jsx
+++ src/pages/admin/setting/account/BackUpdate.jsx
@@ -12,7 +12,6 @@ import {
   Box,
   Select,
   MenuItem,
-  Button,
 } from '@mui/material';
 import { LocalizationProvider } from '@mui/x-date-pickers';
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
@@ -85,11 +84,6 @@ function UserUpdatePage() {
     }
   }, [user, dispatch, reset]);
 
-  // Onclick back
-  const onClickBack = () => {
-    window.history.back();
-  };
-
   // Handle input change and dispatch to Redux
   const handleInputChange = (e) => {
     const { name, value } = e.target;
@@ -144,127 +138,128 @@ function UserUpdatePage() {
   }
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <Stack component={'div'} alignSelf={'center'}>
-        <Avatar
-          sx={imgStyle}
-          alt="user profile"
-          src={userData.photo || Profile}
-        />
-        <input
-          accept="image/*"
-          type="file"
-          onChange={handleImageUpload}
-          style={{ marginTop: '10px' }}
-        />
-      </Stack>
+    <FormComponent title="Update User" subTitle="Update user information">
+      <CardComponent title="User Information">
+        <Stack component={'div'} alignSelf={'center'}>
+          <Avatar
+            sx={imgStyle}
+            alt="user profile"
+            src={userData.photo || Profile}
+          />
+          <input
+            accept="image/*"
+            type="file"
+            onChange={handleImageUpload}
+            style={{ marginTop: '10px' }}
+          />
+        </Stack>
 
-      <Stack direction="row" gap={2}>
-        <Box flexGrow={1}>
-          <Typography variant="body1">First Name</Typography>
+        <Stack direction="row" gap={2}>
+          <Box flexGrow={1}>
+            <Typography variant="body1">First Name</Typography>
+            <TextFieldComponent
+              name="first_name"
+              {...register('first_name')}
+              onChange={handleInputChange}
+              placeholder="First Name"
+              error={Boolean(errors.first_name)}
+              helperText={errors.first_name?.message}
+            />
+          </Box>
+
+          <Box flexGrow={1}>
+            <Typography variant="body1">Last Name</Typography>
+            <TextFieldComponent
+              name="last_name"
+              {...register('last_name')}
+              onChange={handleInputChange}
+              placeholder="Last Name"
+              error={Boolean(errors.last_name)}
+              helperText={errors.last_name?.message}
+            />
+          </Box>
+        </Stack>
+
+        <Box>
+          <Typography variant="body1">Gender</Typography>
+          <Select
+            fullWidth
+            value={gender}
+            onChange={(e) => {
+              setGender(e.target.value);
+              dispatch(updateFormData({ gender: e.target.value }));
+            }}
+            displayEmpty
+            renderValue={(selected) => {
+              if (!selected) {
+                return <Box sx={{ color: '#B5B5B5' }}>Select gender</Box>;
+              }
+              return selected;
+            }}
+          >
+            <MenuItem value="Male">Male</MenuItem>
+            <MenuItem value="Female">Female</MenuItem>
+            <MenuItem value="Other">Other</MenuItem>
+          </Select>
+        </Box>
+
+        <Box>
+          <Typography variant="body1">Date of Birth</Typography>
+          <LocalizationProvider dateAdapter={AdapterDayjs}>
+            <Controller
+              name="dob"
+              control={control}
+              render={({ field }) => (
+                <DatePicker
+                  {...field}
+                  value={dob}
+                  onChange={handleDateChange}
+                  slotProps={{
+                    textField: {
+                      fullWidth: true,
+                      placeholder: 'Date of Birth',
+                      error: Boolean(errors.dob),
+                      helperText: errors.dob?.message,
+                    },
+                  }}
+                />
+              )}
+            />
+          </LocalizationProvider>
+        </Box>
+
+        <Box>
+          <Typography variant="body1">Phone Number</Typography>
           <TextFieldComponent
-            name="first_name"
-            {...register('first_name')}
+            name="phone_number"
+            {...register('phone_number')}
             onChange={handleInputChange}
-            placeholder="First Name"
-            error={Boolean(errors.first_name)}
-            helperText={errors.first_name?.message}
+            placeholder="Number"
+            error={Boolean(errors.phone_number)}
+            helperText={errors.phone_number?.message}
+            defaultValue={userData.userProfile.phone_number}
           />
         </Box>
 
-        <Box flexGrow={1}>
-          <Typography variant="body1">Last Name</Typography>
+        <Box>
+          <Typography variant="body1">Address</Typography>
           <TextFieldComponent
-            name="last_name"
-            {...register('last_name')}
+            name="address"
+            {...register('address')}
             onChange={handleInputChange}
-            placeholder="Last Name"
-            error={Boolean(errors.last_name)}
-            helperText={errors.last_name?.message}
+            placeholder="Address"
+            error={Boolean(errors.address)}
+            helperText={errors.address?.message}
           />
         </Box>
-      </Stack>
 
-      <Box>
-        <Typography variant="body1">Gender</Typography>
-        <Select
-          fullWidth
-          value={gender}
-          onChange={(e) => {
-            setGender(e.target.value);
-            dispatch(updateFormData({ gender: e.target.value }));
-          }}
-          displayEmpty
-          renderValue={(selected) => {
-            if (!selected) {
-              return <Box sx={{ color: '#B5B5B5' }}>Select gender</Box>;
-            }
-            return selected;
-          }}
-        >
-          <MenuItem value="Male">Male</MenuItem>
-          <MenuItem value="Female">Female</MenuItem>
-          <MenuItem value="Other">Other</MenuItem>
-        </Select>
-      </Box>
-
-      <Box>
-        <Typography variant="body1">Date of Birth</Typography>
-        <LocalizationProvider dateAdapter={AdapterDayjs}>
-          <Controller
-            name="dob"
-            control={control}
-            render={({ field }) => (
-              <DatePicker
-                {...field}
-                value={dob}
-                onChange={handleDateChange}
-                slotProps={{
-                  textField: {
-                    fullWidth: true,
-                    placeholder: 'Date of Birth',
-                    error: Boolean(errors.dob),
-                    helperText: errors.dob?.message,
-                  },
-                }}
-              />
-            )}
-          />
-        </LocalizationProvider>
-      </Box>
-
-      <Box>
-        <Typography variant="body1">Phone Number</Typography>
-        <TextFieldComponent
-          name="phone_number"
-          {...register('phone_number')}
-          onChange={handleInputChange}
-          placeholder="Number"
-          error={Boolean(errors.phone_number)}
-          helperText={errors.phone_number?.message}
-          defaultValue={userData.userProfile.phone_number}
+        <ButtonContainer
+          leftBtnTitle="Cancel"
+          rightBtnTitle={isUpdating ? 'Updating...' : 'Update'}
+          rightBtn={handleSubmit(onSubmit)}
         />
-      </Box>
-
-      <Box>
-        <Typography variant="body1">Address</Typography>
-        <TextFieldComponent
-          name="address"
-          {...register('address')}
-          onChange={handleInputChange}
-          placeholder="Address"
-          error={Boolean(errors.address)}
-          helperText={errors.address?.message}
-        />
-      </Box>
-      <Button onClick={onClickBack} variant="outlined">
-        Cancel
-      </Button>
-
-      <Button type="submit" variant="contained">
-        Update
-      </Button>
-    </form>
+      </CardComponent>
+    </FormComponent>
   );
 }
 
