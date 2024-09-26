c17d746400d6bc84aab8d51db1eb3beeb10599ce
Sok Chan Seiha <sokchanseiha@gmail.com>
2024-09-26 11:40:12 +0700

    feature: implement create function in subject

--- src/pages/admin/subject/SubjectCreatePage.jsx
+++ src/pages/admin/subject/SubjectCreatePage.jsx
@@ -1,35 +1,77 @@
-import { useState } from 'react';
-import CardComponent from '../../../components/common/CardComponent';
+import { useEffect } from 'react';
+import { yupResolver } from '@hookform/resolvers/yup';
 import { Typography, Stack, TextField } from '@mui/material';
+import { useForm, Controller } from 'react-hook-form';
 import { fieldContainer } from '../../../styles/authStyle';
+import CardComponent from '../../../components/common/CardComponent';
 import FormComponent from '../../../components/common/FormComponent';
 import ButtonContainer from '../../../components/common/ButtonContainer';
-import { useNavigate } from 'react-router-dom';
 import { useCreateSubjectMutation } from '../../../services/subjectApi';
+import { SubjectValidator } from '../../../validators/validationSchemas';
+import { setSnackbar } from '../../../store/slices/uiSlice';
+import { useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 
 function SubjectCreatePage() {
-  const [subjectName, setSubjectName] = useState(null);
-  const [description, setDescription] = useState(null);
   const navigate = useNavigate();
+  const dispatch = useDispatch();
 
-  const [createSubject, { error: apiError }] = useCreateSubjectMutation();
+  const {
+    control,
+    handleSubmit,
+    getValues,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(SubjectValidator),
+    defaultValues: {
+      name: '',
+      description: '',
+    },
+  });
 
-  const onSubmit = async (data) => {
-    // Additional validation for time picker fields
-    if (!subjectName || !description) {
-      return; // Handle required time validation
+  const [
+    createSubject,
+    {
+      isLoading: isCreating,
+      isError: isUpdateError,
+      isSuccess: isCreateSuccess,
+      error: updateError,
+    },
+  ] = useCreateSubjectMutation();
+
+  useEffect(() => {
+    if (isCreating) {
+      dispatch(
+        setSnackbar({ open: true, message: 'Creating...', severity: 'info' }),
+      );
+    } else if (isUpdateError) {
+      dispatch(
+        setSnackbar({
+          open: true,
+          message: updateError.data.message,
+          severity: 'error',
+        }),
+      );
+    } else if (isCreateSuccess) {
+      dispatch(
+        setSnackbar({
+          open: true,
+          message: 'Subject created successfully',
+          severity: 'success',
+        }),
+      );
+      navigate('/admin/subjects');
     }
+  }, [dispatch, isUpdateError, isCreating, isCreateSuccess]);
 
+  const onSubmit = async () => {
     try {
-      await createSubject({
-        name,
-        description,
-        ...data,
-      }).unwrap();
-
-      navigate(`/admin/subjects`);
-    } catch (error) {
-      console.log('Failed to create subject:', error);
+      const { name, description } = getValues();
+      const formData = { name, description };
+      await createSubject(formData).unwrap();
+      console.log(formData);
+    } catch (err) {
+      console.log('error message :', err);
     }
   };
 
@@ -37,39 +79,46 @@ function SubjectCreatePage() {
     <>
       <FormComponent
         title={'Add Subject'}
-        subTitle={'Please Fill subject information'}
+        subTitle={'Please Fill Subject information'}
       >
         <CardComponent title={'Subject Information'}>
           {/* subject name input container */}
           <Stack sx={fieldContainer}>
-            <Typography variant="body1">Subject&apos;s Name</Typography>
-            <TextField
-              value={subjectName}
-              placeholder="subject's name"
-              onChange={(newValue) => {
-                setSubjectName(newValue);
-              }}
+            <Typography variant="body1">Subject's Name</Typography>
+            <Controller
+              name="name"
+              control={control}
+              render={({ field }) => (
+                <TextField
+                  placeholder="subject's name"
+                  {...field}
+                  error={!!errors.name}
+                  helperText={errors.name ? errors.name.message : ''}
+                />
+              )}
             />
           </Stack>
           {/* description input container */}
           <Stack sx={fieldContainer}>
             <Typography variant="body1">Description</Typography>
-            <TextField
-              placeholder="description"
-              value={description}
-              multiline
-              minRows={5}
-            //   placeholder="description"
-              onChange={(newValue) => {
-                setDescription(newValue);
-              }}
+            <Controller
+              name="description"
+              control={control}
+              render={({ field }) => (
+                <TextField
+                  multiline
+                  minRows={5}
+                  placeholder="description"
+                  {...field}
+                />
+              )}
             />
           </Stack>
           {/* Button Container  */}
           <ButtonContainer
-            rightBtn={onSubmit}
+            rightBtn={handleSubmit(onSubmit)}
             leftBtnTitle={'Cancel'}
-            rightBtnTitle={'Add subject'}
+            rightBtnTitle={'Add Subject'}
           />
         </CardComponent>
       </FormComponent>
