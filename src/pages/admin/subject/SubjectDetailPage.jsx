75273d91e2ce8de6fb3cdde8637ceebc5fe30a3e
Sok Chan Seiha <sokchanseiha@gmail.com>
2024-09-27 08:52:34 +0700

    feature: implement delete functionality in subject view detail

--- src/pages/admin/subject/SubjectDetailPage.jsx
+++ src/pages/admin/subject/SubjectDetailPage.jsx
@@ -2,18 +2,14 @@ import React, { useState, useEffect } from 'react';
 import FormComponent from '../../../components/common/FormComponent';
 import CardComponent from '../../../components/common/CardComponent';
 import CardInformation from '../../../components/common/CardInformation';
-import { useGetSubjectByIdQuery } from '../../../services/subjectApi';
+import { useGetSubjectByIdQuery, useDeleteSubjectMutation } from '../../../services/subjectApi';
 import { subjectDetail } from '../../../utils/formatData';
+import DeleteConfirmationModal from '../../../components/common/DeleteConfirmationModal';
+import { setModal, setSnackbar } from '../../../store/slices/uiSlice';
 import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate, useParams } from 'react-router-dom';
 import CircularIndeterminate from '../../../components/loading/LoadingCircle';
 
-const clickEdit = () => {
-  console.log('edit');
-};
-const clickDetele = () => {
-  console.log('delete');
-};
 
 function SubjectDetailPage() {
   const { id } = useParams();
@@ -21,14 +17,34 @@ function SubjectDetailPage() {
   const navigate = useNavigate();
 
   const [details, setDetails] = useState([]);
+  const [subjectToDelete, setSubjectToDelete] = useState(null);
+  const { modal } = useSelector((state) => state.ui);
   
   const { data, error, isLoading, isSuccess } = useGetSubjectByIdQuery(id);
+  const [
+    deleteSubject, { isLoading: isDeleting, isSuccess: isDeleteSuccess, isError: isDeleteError }
+  ] = useDeleteSubjectMutation();
 
   useEffect(() => {
     if (data && isSuccess) {
       setDetails(subjectDetail(data));
     }
-  }, [data, isSuccess]);
+  }, [data, isSuccess, isDeleteSuccess]);
+
+  useEffect(() => {
+    if (isDeleting) {
+      dispatch(
+        setSnackbar({ open: true, message: 'Deleting...', severity: 'info' }),
+      );
+    } else if (isDeleteError) {
+      dispatch(setSnackbar({ open: true, message: error.data.message, severity: 'error' }));
+    } else if (isDeleteSuccess) {
+      dispatch(
+        setSnackbar({ open: true, message: 'Deleted successfully', severity: 'success' }),
+      );
+      navigate('/admin/subjects');
+    }
+  }, [dispatch, isDeleteError, isDeleteSuccess, isDeleting]);
 
   if (isLoading) {
     return <CircularIndeterminate />;
@@ -37,6 +53,20 @@ function SubjectDetailPage() {
   if (error) {
     return <div>Error loading class periods: {error.message}</div>;
   }
+
+  const clickEdit = () => {
+    navigate(`/admin/subjects/update/${id}`);
+  };
+
+  const clickDetele = () => {
+    setSubjectToDelete(data.data);
+    dispatch(setModal({ open: true }));
+  };
+
+  const confirmDelete = async () => {
+    dispatch(setModal({ open: false }));
+    await deleteSubject(subjectToDelete.subject_id).unwrap();
+  };
 
   // Manually remove or nullify the `photo` property in the `data` object
   const dataWithoutPhoto = {
@@ -60,6 +90,12 @@ function SubjectDetailPage() {
       >
         <CardInformation data={details} />
       </CardComponent>
+      <DeleteConfirmationModal
+        open={modal.open}
+        onClose={() => dispatch(setModal({ open: false }))}
+        onConfirm={confirmDelete}
+        itemName="Subject"
+      />
     </FormComponent>
   );
 }
