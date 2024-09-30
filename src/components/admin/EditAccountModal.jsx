ef63398f1ebf2a5ef5d5fdfb8ce35b25c41f0644
NarakCODE <channarakluy@gmail.com>
2024-09-30 09:54:38 +0700

    Create : modal ui

--- src/components/admin/EditAccountModal.jsx
+++ src/components/admin/EditAccountModal.jsx
@@ -33,8 +33,79 @@ const EditAccountModal = () => {
       reader.readAsDataURL(file);
     }
   };
-  
-  return <div>EditAccountModal</div>;
+
+  return (
+    <Modal open={open} onClose={onClose}>
+      <Box
+        sx={{
+          position: 'absolute',
+          top: '50%',
+          left: '50%',
+          transform: 'translate(-50%, -50%)',
+          width: 400,
+          bgcolor: 'background.paper',
+          boxShadow: 24,
+          p: 4,
+          borderRadius: 2,
+        }}
+      >
+        <Typography variant="h6" component="h2" gutterBottom>
+          Edit Account
+        </Typography>
+        <form onSubmit={handleSubmit}>
+          <Box sx={{ mb: 2, display: 'flex', justifyContent: 'center' }}>
+            <Box sx={{ position: 'relative' }}>
+              <Avatar src={profileImage} sx={{ width: 100, height: 100 }} />
+              <input
+                accept="image/*"
+                type="file"
+                id="icon-button-file"
+                style={{ display: 'none' }}
+                onChange={handleImageUpload}
+              />
+              <label htmlFor="icon-button-file">
+                <IconButton
+                  aria-label="upload picture"
+                  component="span"
+                  sx={{
+                    position: 'absolute',
+                    bottom: 0,
+                    right: 0,
+                    bgcolor: 'background.paper',
+                  }}
+                >
+                  <EditIcon />
+                </IconButton>
+              </label>
+            </Box>
+          </Box>
+          <TextField
+            fullWidth
+            label="First Name"
+            value={firstName}
+            onChange={(e) => setFirstName(e.target.value)}
+            margin="normal"
+          />
+          <TextField
+            fullWidth
+            label="Last Name"
+            value={lastName}
+            onChange={(e) => setLastName(e.target.value)}
+            margin="normal"
+          />
+          <Button
+            type="submit"
+            variant="contained"
+            color="primary"
+            fullWidth
+            sx={{ mt: 2 }}
+          >
+            Save Changes
+          </Button>
+        </form>
+      </Box>
+    </Modal>
+  );
 };
 
 export default EditAccountModal;
