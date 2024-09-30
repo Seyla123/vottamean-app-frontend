51510740ee1972536483c7a713f3cc19e1a4042e
NarakCODE <channarakluy@gmail.com>
2024-09-30 11:02:15 +0700

    refactor : define function

--- src/components/admin/EditAccountModal.jsx
+++ src/components/admin/EditAccountModal.jsx
@@ -32,8 +32,34 @@ import { UserProfileValidator } from '../../validators/validationSchemas';
 // UI Slice for snackbar
 import { setSnackbar } from '../../store/slices/uiSlice';
 
+const EditAccountModal = ({ open, onClose }) => {
+  // - Initialize dispatch and navigate hooks
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
-const EditAccountModal = ({ open, onClose, onSave, dataToEdit }) => {
+  // - Redux hooks update user api
+  const [
+    updateUserProfile,
+    {
+      isLoading: isUpdateLoading,
+      isError: isUpdateError,
+      isSuccess: isUpdateSuccess,
+      error: updateError,
+    },
+  ] = useUpdateUserProfileMutation();
+
+  // - Redux hooks get user api
+  const { data: userProfile, isLoading, isSuccess } = useGetUserProfileQuery();
+
+  // - State to store original form values
+  const [originalData, setOriginalData] = useState(null);
+
+  // - State to store selected image file
+  const [selectedFile, setSelectedFile] = useState(null);
+
+  // - State to store the preview URL of the selected image
+  const [previewUrl, setPreviewUrl] = useState(null);
+
   return <div>EditAccountModal</div>;
 };
 
