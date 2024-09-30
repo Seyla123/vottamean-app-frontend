d8275e7498a175060c039cc007820d4e87b7e01a
NarakCODE <channarakluy@gmail.com>
2024-09-30 11:01:08 +0700

    import : important file

--- src/components/admin/EditAccountModal.jsx
+++ src/components/admin/EditAccountModal.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { useForm, Controller } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
 
-const EditAccountModal = () => {
-  return (
-    <div>EditAccountModal</div>
-  )
-}
+// MUI Components
+import {
+  Modal,
+  Box,
+  TextField,
+  Button,
+  Avatar,
+  Typography,
+  IconButton,
+  Fade,
+  Backdrop,
+} from '@mui/material';
 
-export default EditAccountModal
+// Redux Hooks and APIs
+import {
+  useUpdateUserProfileMutation,
+  useGetUserProfileQuery,
+} from '../../services/userApi';
+
+// Formatted Data
+import { getUserProfileUpdateData } from '../../utils/formatData';
+
+//  User Profile Validator
+import { UserProfileValidator } from '../../validators/validationSchemas';
+
+// UI Slice for snackbar
+import { setSnackbar } from '../../store/slices/uiSlice';
+
+
+const EditAccountModal = ({ open, onClose, onSave, dataToEdit }) => {
+  return <div>EditAccountModal</div>;
+};
+
+export default EditAccountModal;
