81dc0f30a38f8d10cc9930d91afcd685e0a3249c
Sok Chan Seiha <sokchanseiha@gmail.com>
2024-10-06 11:38:05 +0700

    feature: SubjectCreatePage,  add icon to input field

--- src/pages/admin/subject/SubjectCreatePage.jsx
+++ src/pages/admin/subject/SubjectCreatePage.jsx
@@ -3,7 +3,8 @@ import { useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import { useForm, Controller } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
-import { Typography, Stack, TextField } from '@mui/material';
+import { Typography, Stack, TextField, InputAdornment } from '@mui/material';
+import { Library, ScrollText } from 'lucide-react';
 // import components
 import CardComponent from '../../../components/common/CardComponent';
 import FormComponent from '../../../components/common/FormComponent';
@@ -99,12 +100,21 @@ function SubjectCreatePage() {
               control={control}
               render={({ field }) => (
                 <TextField
-                  placeholder="subject's name"
                   {...field}
+                  placeholder="subject's name"
                   error={!!errors.subject_name}
                   helperText={
                     errors.subject_name ? errors.subject_name.message : ''
                   }
+                  slotProps={{
+                    input: {
+                      startAdornment: (
+                        <InputAdornment position="start">
+                          <Library size={20} />
+                        </InputAdornment>
+                      ),
+                    },
+                  }}
                 />
               )}
             />
@@ -118,12 +128,24 @@ function SubjectCreatePage() {
                 <TextField
                   {...field}
                   error={!!errors.description}
-                  helperText={
-                    errors.description ? errors.description.message : ''
-                  }
                   multiline
                   minRows={5}
                   placeholder="description"
+                  helperText={ errors.description ? errors.description.message : ''}
+                  slotProps={{
+                    input: {
+                      startAdornment: (
+                        <InputAdornment position="start">
+                          <ScrollText size={20} />
+                        </InputAdornment>
+                      ),
+                    },
+                  }}
+                  sx={{
+                    '.MuiInputBase-root': {
+                      alignItems: 'flex-start',
+                    },
+                  }}
                 />
               )}
             />
