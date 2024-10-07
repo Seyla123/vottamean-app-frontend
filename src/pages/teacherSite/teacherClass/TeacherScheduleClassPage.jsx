e9aab83e8a3854f7b711f170b0117aba155562a3
NarakCODE <channarakluy@gmail.com>
2024-10-07 14:30:37 +0700

    feature : create class item random headaer img

--- src/pages/teacherSite/teacherClass/TeacherScheduleClassPage.jsx
+++ src/pages/teacherSite/teacherClass/TeacherScheduleClassPage.jsx
@@ -9,20 +9,42 @@ import {
   Skeleton,
   Divider,
   Tabs,
-  Tab
+  Tab,
 } from '@mui/material';
 import { styled } from '@mui/system';
 import { Clock, Calendar, Users } from 'lucide-react';
 import { useGetTeacherScheduleClassesQuery } from '../../../services/teacherApi';
 import EmptyList from '../../../components/common/EmptyList';
+import classHeaderImg1 from '../../../assets/images/study-bg.avif';
+import classHeaderImg2 from '../../../assets/images/class-header-img-2.avif';
+import classHeaderImg3 from '../../../assets/images/class-header-img-3.avif';
+import classHeaderImg4 from '../../../assets/images/class-header-img-4.avif';
+import classHeaderImg5 from '../../../assets/images/class-header-img-5.avif';
+import classHeaderImg6 from '../../../assets/images/class-header-img-6.avif';
+import { shadow } from '../../../styles/global';
+import FormComponent from '../../../components/common/FormComponent';
+const headerImages = [
+  classHeaderImg1,
+  classHeaderImg2,
+  classHeaderImg3,
+  classHeaderImg4,
+  classHeaderImg5,
+  classHeaderImg6,
+];
 
 const StyledPaper = styled(Paper)(({ theme }) => ({
-  padding: theme.spacing(3),
+  overflow: 'hidden',
+  cursor: 'pointer',
+  transition: 'all 0.3s ease-in-out',
   height: '100%',
   display: 'flex',
   flexDirection: 'column',
   borderRadius: theme.shape.borderRadius * 2,
-  boxShadow: '0 4px 6px rgba(0, 0, 0, 0.1)',
+  boxShadow: shadow.boxShadow,
+  '&:hover': {
+    transform: 'translateY(-4px)',
+    boxShadow: `0 12px 20px rgba(0, 0, 0, 0.1)`,
+  },
 }));
 
 const IconWrapper = styled(Box)(({ theme }) => ({
@@ -34,38 +56,75 @@ const IconWrapper = styled(Box)(({ theme }) => ({
   },
 }));
 
-const ClassListItem = ({ classData, onClick }) => (
-  <StyledPaper onClick={onClick}>
-    <Typography variant="h6" gutterBottom color="primary">
-      {classData.class_name}
-    </Typography>
-    <Typography variant="body1" color="text.secondary" gutterBottom>
-      {classData.subject}
-    </Typography>
-    <Divider sx={{ my: 2 }} />
-    <IconWrapper>
-      <Calendar size={18} />
-      <Typography variant="body2" textTransform="capitalize">
-        {classData.day}
-      </Typography>
-    </IconWrapper>
-    <IconWrapper sx={{ my: 1 }}>
-      <Clock size={18} />
-      <Typography variant="body2">
-        {classData.start_time.slice(0, 5)} - {classData.end_time.slice(0, 5)}
-      </Typography>
-    </IconWrapper>
-    <IconWrapper>
-      <Users size={18} />
-      <Typography variant="body2">{classData.students} students</Typography>
-    </IconWrapper>
-    <Box mt="auto" pt={2}>
-      <Typography variant="caption" color="text.secondary">
-        Session ID: {classData.session_id}
-      </Typography>
-    </Box>
-  </StyledPaper>
-);
+const ClassListItem = ({ classData, onClick }) => {
+  const dayIndex = [
+    'sunday',
+    'monday',
+    'tuesday',
+    'wednesday',
+    'thursday',
+    'friday',
+    'saturday',
+  ].indexOf(classData.day.toLowerCase());
+  const headerImage = headerImages[dayIndex % headerImages.length];
+
+  return (
+    <StyledPaper onClick={onClick}>
+      <Box
+        sx={{
+          position: 'relative',
+          backgroundImage: `url(${headerImage})`,
+          bgSize: 'top',
+          bgRepeat: 'no-repeat',
+          bgPosition: 'center',
+          height: '120px',
+          width: '100%',
+        }}
+      >
+        <Box
+          sx={{
+            width: '100%',
+            height: '100%',
+            position: 'absolute',
+            bottom: 0,
+            zIndex: 10,
+            backgroundColor: 'rgba(0, 0, 0, 0.5)',
+            padding: 2,
+            color: 'white',
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center',
+          }}
+        >
+          <Typography variant="h5" fontWeight={'bold'} gutterBottom>
+            {classData.class_name}
+          </Typography>
+          <Typography variant="body1">{classData.subject}</Typography>
+        </Box>
+      </Box>
+      <Box sx={{ padding: 3 }}>
+        <IconWrapper>
+          <Calendar size={18} />
+          <Typography variant="body2" textTransform="capitalize">
+            {classData.day}
+          </Typography>
+        </IconWrapper>
+        <IconWrapper sx={{ my: 1 }}>
+          <Clock size={18} />
+          <Typography variant="body2">
+            {classData.start_time.slice(0, 5)} -{' '}
+            {classData.end_time.slice(0, 5)}
+            {classData.start_time.slice(6, 7) === 'PM' ? 'PM' : 'AM'}
+          </Typography>
+        </IconWrapper>
+        <IconWrapper>
+          <Users size={18} />
+          <Typography variant="body2">{classData.students} students</Typography>
+        </IconWrapper>
+      </Box>
+    </StyledPaper>
+  );
+};
 
 function TeacherScheduleClassPage() {
   const navigate = useNavigate();
@@ -78,7 +137,7 @@ function TeacherScheduleClassPage() {
   } = useGetTeacherScheduleClassesQuery();
 
   const [classesData, setClassesData] = useState([]);
-  const [selectedCategory, setSelectedCategory] = useState('All');
+  const [selectedDay, setSelectedDay] = useState('All');
 
   useEffect(() => {
     if (getTeacherClasses && isSuccess) {
@@ -86,17 +145,25 @@ function TeacherScheduleClassPage() {
     }
   }, [getTeacherClasses, isSuccess]);
 
-  const categories = useMemo(() => {
-    const uniqueCategories = ['All', ...new Set(classesData.map(c => c.class_name))];
-    return uniqueCategories;
-  }, [classesData]);
+  const days = [
+    'All',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+    'Sunday',
+  ];
 
   const filteredClasses = useMemo(() => {
-    if (selectedCategory === 'All') {
+    if (selectedDay === 'All') {
       return classesData;
     }
-    return classesData.filter(c => c.class_name === selectedCategory);
-  }, [classesData, selectedCategory]);
+    return classesData.filter(
+      (c) => c.day.toLowerCase() === selectedDay.toLowerCase(),
+    );
+  }, [classesData, selectedDay]);
 
   if (isError) {
     return (
@@ -110,8 +177,8 @@ function TeacherScheduleClassPage() {
     navigate(`/teacher/mark-attendance/${sessionId}`);
   };
 
-  const handleCategoryChange = (event, newValue) => {
-    setSelectedCategory(newValue);
+  const handleDayChange = (event, newValue) => {
+    setSelectedDay(newValue);
   };
 
   const renderSkeleton = () => (
@@ -139,24 +206,25 @@ function TeacherScheduleClassPage() {
   }
 
   return (
-    <Container maxWidth="lg">
-      <Typography variant="h4" component="h1" gutterBottom>
-        Class Schedule
-      </Typography>
-      <Typography variant="subtitle1" gutterBottom>
-        You have {classesData.length} classes scheduled
-      </Typography>
-      
-      <Box sx={{ borderBottom: 1, borderColor: 'divider', mb: 3 }}>
-        <Tabs 
-          value={selectedCategory} 
-          onChange={handleCategoryChange} 
+    <FormComponent
+      title="Class Schedule"
+      subTitle={` You have ${classesData.length} classes scheduled`}
+    >
+      <Box sx={{ borderBottom: 1, borderColor: 'divider', mb: 2 }}>
+        <Tabs
+          value={selectedDay}
+          onChange={handleDayChange}
           variant="scrollable"
           scrollButtons="auto"
-          aria-label="class categories"
+          aria-label="class days"
         >
-          {categories.map((category) => (
-            <Tab label={category} value={category} key={category} />
+          {days.map((day) => (
+            <Tab
+              label={day}
+              value={day}
+              key={day}
+              sx={{ textTransform: 'capitalize' }}
+            />
           ))}
         </Tabs>
       </Box>
@@ -175,7 +243,7 @@ function TeacherScheduleClassPage() {
           ))}
         </Grid>
       )}
-    </Container>
+    </FormComponent>
   );
 }
 
