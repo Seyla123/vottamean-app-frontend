1b036d9245d2637bbb56e649f0c2b52750c2b00f
Nem Puthiraksa <raksasparkle@gmail.com>
2024-10-07 13:54:26 +0700

    feature : fix-create-teacher, fix sub header of create teacher page

--- src/pages/admin/teacher/TeacherCreatePage.jsx
+++ src/pages/admin/teacher/TeacherCreatePage.jsx
@@ -1,14 +1,13 @@
 import React from 'react';
 import FormInfo from '../../../components/teacher/FormInfo';
 import FormComponent from '../../../components/common/FormComponent';
-import { Box, Typography } from '@mui/material';
 function TeacherCreatePage() {
   return (
     <>
       {/* Header */}
       <FormComponent
         title="ADD TEACHER"
-        subTitle="Please Update Teacher Information"
+        subTitle="Please Add Teacher Information"
       >
         {/* Form input */}
         <FormInfo />
@@ -18,20 +17,3 @@ function TeacherCreatePage() {
 }
 
 export default TeacherCreatePage;
-const profileBox = {
-  border: '1px solid',
-  borderColor: '#E0E0E0',
-  borderRadius: '8px',
-  bgcolor: '#ffffff',
-
-  padding: {
-    xs: 2,
-    sm: 3,
-  },
-};
-const card = {
-  border: '1px solid',
-  borderColor: '#E0E0E0',
-  borderRadius: '8px',
-  bgcolor: '#ffffff',
-};
