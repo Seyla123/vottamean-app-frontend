1954a539db81830cc443df33d2951a895d28b129
NarakCODE <channarakluy@gmail.com>
2024-10-09 16:58:26 +0700

    feature : greeting and calendar

--- src/pages/teacherSite/dashboard/TeacherDashboardPage.jsx
+++ src/pages/teacherSite/dashboard/TeacherDashboardPage.jsx
@@ -1,7 +1,26 @@
 import React from 'react';
+import DateCalendarCard from '../../../components/common/DateCalendarCard';
 
 const TeacherDashboardPage = () => {
-  return <div>TeacherDashboardPage</div>;
+  // Get the current hour to determine the appropriate greeting
+  const currentHour = new Date().getHours();
+  let greeting = 'Good evening';
+
+  if (currentHour < 12) {
+    greeting = 'Good morning';
+  } else if (currentHour < 18) {
+    greeting = 'Good afternoon';
+  }
+
+  return (
+    <div className="teacher-dashboard">
+      <h1 className="dashboard-greeting">{greeting}, Teacher!</h1>
+      <div className="dashboard-content">
+        <DateCalendarCard />
+        {/* Add more dashboard components here */}
+      </div>
+    </div>
+  );
 };
 
 export default TeacherDashboardPage;
