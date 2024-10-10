19c63a69b1ceabd832184dfa9ccd762fdf565632
Nem Puthiraksa <raksasparkle@gmail.com>
2024-10-10 17:00:08 +0700

    feature :fix-teacher-form, add profile function in teacher info

--- src/components/teacher/TeacherInfo.jsx
+++ src/components/teacher/TeacherInfo.jsx
@@ -1,16 +1,18 @@
-// React and third-party libraries
-import { useEffect, useState } from 'react';
-import * as yup from 'yup';
-import { yupResolver } from '@hookform/resolvers/yup';
+import React, { useEffect, useRef, useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 import { useForm, Controller } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
+import dayjs from 'dayjs';
 
-// Material UI components
+// MUI Components
 import {
   TextField,
   MenuItem,
-  Button,
   Box,
   Avatar,
+  Button,
   Typography,
   Stack,
   FormControl,
@@ -18,152 +20,180 @@ import {
 } from '@mui/material';
 import { DatePicker, LocalizationProvider } from '@mui/x-date-pickers';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
-import dayjs from 'dayjs';
+
+// Redux Slice
+import { setSnackbar } from '../../store/slices/uiSlice';
+
+// Custom Components
+import RandomAvatar from '../common/RandomAvatar';
 import StyledButton from '../common/StyledMuiButton';
-
-// Redux hooks and actions
-import { useNavigate } from 'react-router-dom';
-
-// Custom components
 import SubHeader from './SubHeader';
 import PhoneInputField from '../common/PhoneInputField';
+import InputField from '../common/InputField';
+import GenderSelect from '../common/GenderSelect';
+import { ImagePlus, Trash2 } from 'lucide-react';
 
 const TeacherInfo = ({ handleNextClick, defaultValues }) => {
   const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const [photoFile, setPhotoFile] = useState(null);
+  const [photoPreview, setPhotoPreview] = useState(null);
+  const inputFileRef = useRef(null);
   const [dob, setDob] = useState(null);
-
-  // Define form methods validation
   const {
     control,
     handleSubmit,
+    setValue,
+    getValues,
     formState: { errors },
     reset,
   } = useForm({
     resolver: yupResolver(validationSchema),
     defaultValues: {
+      photo: '',
       firstName: '',
       lastName: '',
       phoneNumber: '',
       gender: '',
-      dob: '',
+      dob: null,
       address: '',
     },
   });
 
-  // Set default values and date of birth state when defaultValues change
+  // set the default values for the properties
   useEffect(() => {
-    // reset the form with the default values if it was provided
-    // and set the date of birth state
     if (defaultValues) {
       reset(defaultValues);
-      setDob(defaultValues.dob ? dayjs(defaultValues.dob) : null);
+      // set the photo preview if it exists in default values
+      if (defaultValues.photo) {
+        setPhotoPreview(defaultValues.photo);
+      }
     }
   }, [defaultValues, reset]);
 
   // Handle form submission
   const onSubmit = (data) => {
-    handleNextClick(true, { ...data, dob: dob ? dob.toISOString() : null }); // pass the date of birth to the next page
+    const updatedData = {
+      ...data,
+      photo: photoPreview,
+    };
+    handleNextClick(true, updatedData);
   };
 
-  // handle back to list page
+  const handlePhotoUpload = (event) => {
+    const file = event.target.files[0];
+    if (file) {
+      setPhotoFile(file);
+      const newPreviewUrl = URL.createObjectURL(file);
+      setPhotoPreview(newPreviewUrl);
+      setValue('photo', newPreviewUrl);
+    }
+  };
+
+  const handleRemovePhoto = () => {
+    if (photoPreview) {
+      URL.revokeObjectURL(photoPreview);
+    }
+    setPhotoFile(null);
+    setPhotoPreview(null);
+    setValue('photo', '');
+    if (inputFileRef.current) {
+      inputFileRef.current.value = '';
+    }
+  };
+
   const handleCancel = () => {
     navigate('/admin/teachers');
   };
+
   return (
     <LocalizationProvider dateAdapter={AdapterDayjs}>
       <form onSubmit={handleSubmit(onSubmit)}>
         <Box sx={profileBox}>
-          <Box sx={profilePic}>
-            <Avatar sx={imgStyle} alt="profile picture" src="r" />
+          {/* Profile Photo */}
+          <Box
+            sx={{
+              textAlign: 'start',
+              display: 'flex',
+              alignItems: 'center',
+              gap: 2,
+            }}
+          >
+            {photoPreview ? (
+              <Avatar
+                src={photoPreview}
+                alt="Profile"
+                sx={{ width: 140, height: 140, bgcolor: '#eee' }}
+              />
+            ) : (
+              <RandomAvatar
+                username={`${getValues('firstName')} ${getValues('lastName')}`}
+                gender={getValues('gender')}
+                size={140}
+              />
+            )}
+            <input
+              accept="image/*"
+              id="photo-upload"
+              type="file"
+              hidden
+              ref={inputFileRef}
+              onChange={handlePhotoUpload}
+            />
+            <label htmlFor="photo-upload">
+              <StyledButton
+                variant="contained"
+                size="small"
+                component="span"
+                startIcon={<ImagePlus size={20} />}
+              >
+                Upload
+              </StyledButton>
+            </label>
+            <StyledButton
+              variant="outlined"
+              size="small"
+              color="error"
+              startIcon={<Trash2 size={20} />}
+              onClick={handleRemovePhoto}
+            >
+              Remove
+            </StyledButton>
           </Box>
+
           <SubHeader title={'Teacher Information'} />
+
           <Box display={'flex'} flexDirection={'row'} sx={boxContainer}>
-            {/* First Name */}
             <Box sx={{ flex: 1, width: '100%' }}>
-              <Box sx={textFieldGap}>
-                <Typography variant="body2" fontWeight="bold">
-                  First Name {''}
-                  <span style={{ color: 'red', marginLeft: 1 }}>*</span>
-                </Typography>
-                <Controller
-                  name="firstName"
-                  control={control}
-                  render={({ field }) => (
-                    <TextField
-                      {...field}
-                      type="text"
-                      placeholder="First Name"
-                      error={!!errors.firstName}
-                      helperText={errors.firstName?.message}
-                      fullWidth
-                    />
-                  )}
-                />
-              </Box>
+              <InputField
+                name="firstName"
+                control={control}
+                label="First Name"
+                placeholder="First Name"
+                errors={errors}
+              />
             </Box>
-            {/* Last Name */}
             <Box sx={{ flex: 1, width: '100%' }}>
-              <Box sx={textFieldGap}>
-                <Typography variant="body2" fontWeight="bold">
-                  Last Name {''}
-                  <span style={{ color: 'red', marginLeft: 1 }}>*</span>
-                </Typography>
-                <Controller
-                  name="lastName"
-                  control={control}
-                  render={({ field }) => (
-                    <TextField
-                      {...field}
-                      type="text"
-                      placeholder="Last Name"
-                      error={!!errors.lastName}
-                      helperText={errors.lastName?.message}
-                      fullWidth
-                    />
-                  )}
-                />
-              </Box>
+              <InputField
+                name="lastName"
+                control={control}
+                label="Last Name"
+                placeholder="Last Name"
+                errors={errors}
+              />
             </Box>
           </Box>
-          {/* Gender */}
+
           <Box sx={{ ...textFieldGap, width: '100%' }}>
-            <Typography variant="body2" fontWeight="bold">
-              Gender <span style={{ color: 'red', marginLeft: 1 }}>*</span>
-            </Typography>
-            <Controller
+            <GenderSelect
               name="gender"
               control={control}
-              render={({ field }) => (
-                <FormControl fullWidth error={!!errors.gender}>
-                  <Select
-                    {...field}
-                    displayEmpty
-                    error={!!errors.gender}
-                    renderValue={(selected) => {
-                      if (!selected) {
-                        return <Box sx={{ color: '#B5B5B5' }}>Gender</Box>;
-                      }
-                      return selected;
-                    }}
-                  >
-                    <MenuItem value="Male">Male</MenuItem>
-                    <MenuItem value="Female">Female</MenuItem>
-                    <MenuItem value="Other">Other</MenuItem>
-                  </Select>
-                  <Typography
-                    variant="caption"
-                    color="error"
-                    sx={{
-                      marginLeft: errors.gender?.message ? '14px' : '0',
-                      marginTop: errors.gender?.message ? '3px' : '0',
-                    }}
-                  >
-                    {errors.gender?.message}
-                  </Typography>
-                </FormControl>
-              )}
+              errors={errors}
+              label="Gender"
+              defaultValue={defaultValues?.gender}
             />
           </Box>
+
           {/* Date of Birth */}
           <Box sx={{ ...textFieldGap, width: '100%' }}>
             <Typography variant="body2" fontWeight="bold">
@@ -205,26 +235,7 @@ const TeacherInfo = ({ handleNextClick, defaultValues }) => {
               )}
             />
           </Box>
-          {/* Phone Number */}
-          {/* <Box sx={{ ...textFieldGap, width: '100%' }}>
-            <Typography variant="body2" fontWeight="bold">
-              Contact Number {''}
-              <span style={{ color: 'red', marginLeft: 1 }}>*</span>
-            </Typography>
-            <Controller
-              name="phoneNumber"
-              control={control}
-              render={({ field }) => (
-                <TextField
-                  {...field}
-                  placeholder="Contact Number"
-                  error={!!errors.phoneNumber}
-                  helperText={errors.phoneNumber?.message}
-                  fullWidth
-                />
-              )}
-            />
-          </Box> */}
+
           <Box sx={{ ...textFieldGap, width: '100%' }}>
             <PhoneInputField
               name="phoneNumber"
@@ -233,35 +244,26 @@ const TeacherInfo = ({ handleNextClick, defaultValues }) => {
               errors={errors}
             />
           </Box>
-          {/* Address */}
+
           <Box sx={{ ...textFieldGap, width: '100%' }}>
-            <Typography variant="body2" fontWeight="bold">
-              Street Address{' '}
-            </Typography>
-            <Controller
+            <InputField
               name="address"
+              required={false}
               control={control}
-              render={({ field }) => (
-                <TextField
-                  {...field}
-                  placeholder="Phnom Penh, Street 210, ..."
-                  type="text"
-                  error={!!errors.address}
-                  helperText={errors.address?.message}
-                  fullWidth
-                  multiline
-                  minRows={5}
-                />
-              )}
+              label="Street Address"
+              placeholder="Phnom Penh, Street 210,..."
+              errors={errors}
+              multiline={true}
+              minRows={5}
             />
           </Box>
-          {/* Buttons */}
+
           <Stack
             direction={'row'}
             alignSelf={'flex-end'}
             justifyContent={'flex-end'}
-            width={{ xs: '100%', sm: '340px' }}
-            gap={{ xs: 0.5, sm: 1 }}
+            width={{ xs: '100%', sm: '300px', md: '280px' }}
+            gap={2}
             marginTop={{ xs: 2, sm: 0 }}
           >
             <StyledButton
@@ -289,53 +291,24 @@ const boxContainer = {
   width: '100%',
   marginTop: '16px',
   padding: '0px',
-  gap: {
-    xs: '12px',
-    sm: 3,
-  },
+  gap: { xs: '12px', sm: 3 },
 };
+
 const profileBox = {
   width: '100%',
   bgcolor: '#ffffff',
-  padding: {
-    xs: 2,
-    sm: 3,
-  },
-  gap: {
-    xs: '12px',
-    sm: 3,
-  },
+  padding: { xs: 2, sm: 3 },
+  gap: { xs: '12px', sm: 3 },
   display: 'flex',
   alignItems: 'center',
   justifyContent: 'center',
   flexDirection: 'column',
-  position: 'relative',
-};
-const profilePic = {
-  width: 100,
-  height: 100,
-  borderRadius: '50%',
-  overflow: 'hidden',
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'center',
-  mb: 2,
   position: 'relative',
 };
 const textFieldGap = {
   display: 'flex',
   gap: 0.5,
   flexDirection: 'column',
-};
-const imgStyle = {
-  width: {
-    xs: 120,
-    sm: 160,
-  },
-  height: {
-    xs: 120,
-    sm: 160,
-  },
 };
 
 // Define validation schema
@@ -358,34 +331,26 @@ export const validationSchema = yup.object({
     )
     .min(2, 'Name must be at least 2 characters long')
     .max(40, 'Name must be less than 40 characters'),
-  // phoneNumber: yup
-  //   .string()
-  //   .required('Phone number is required')
-  //   .matches(
-  //     /^\d{9,15}$/,
-  //     'Phone number must be between 9 and 15 digits and numeric.',
-  //   )
-  //   .test(
-  //     'length',
-  //     'Phone number must be between 9 and 15 digits',
-  //     (value) => value && value.length >= 9 && value.length <= 15,
-  //   ),
-  phoneNumber: yup.string()
-    .trim()
+  phoneNumber: yup
+    .string()
     .required('Phone number is required')
-    .matches(
-      /^\+\d{1,3}\s\d{1,3}.*$/,
-      'Phone number must start with a country code and area code (e.g., +855 23 ...)',
-    )
     .test(
       'length',
       'Phone number must be between 9 and 15 digits (excluding country code)',
       (value) => {
+        // Extract the number part (after the country code)
         const numberPart = value && value.replace(/[^0-9]/g, '');
         return numberPart && numberPart.length >= 9 && numberPart.length <= 15;
       },
+    )
+    .matches(
+      /^\+\d{1,3}\s\d{1,3}.*$/,
+      'Phone number must start with a country code and area code (e.g., +855 23 ...)',
     ),
-  gender: yup.string().required('Gender is required'),
+  gender: yup
+    .string()
+    .required('Gender is required')
+    .oneOf(['Male', 'Female', 'Other'], 'Please select a valid gender'),
   dob: yup
     .string()
     .required('Date of birth is required')
