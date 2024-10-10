65b914dfcd7dd3af5cfd9670ba821fc3d6f11968
Seav <Mrrseyla.758@gmail.com>
2024-10-10 19:35:10 +0700

    file : add static data and table pdf

--- src/components/attendance/AttendanceTable.jsx
+++ src/components/attendance/AttendanceTable.jsx
@@ -5,23 +5,53 @@ const attendanceData = [
   {
     id: '07',
     student_name: 'Seyla',
-    gender: 'M',
+    gender: 'Male',
     attendance: {
-      '2024-09-16': { 'Maths': 'P', 'WebDev': 'A', 'Khmer': 'P'},
-      '2024-09-17': { 'Maths': 'P', 'WebDev': 'P', 'Khmer': 'P' },
-      '2024-09-18': { 'Maths': 'P', 'WebDev': 'A', 'Khmer': 'P' },
-      '2024-09-19': { 'Maths': 'P', 'WebDev': 'A', 'Khmer': 'P' }
+      '2024-09-16': { 'Maths': 'P1', 'Web Dev': 'A1', 'Khmer': 'P1'},
+      '2024-09-17': { 'Maths': 'P2', 'Web Dev': 'P2', 'Khmer': 'P2' },
+      '2024-09-18': { 'Maths': 'P', 'Web Dev': 'A', 'Khmer': 'P' },
+      '2024-09-19': { 'Maths': 'P', 'Web Dev': 'A', 'Khmer': 'P' },
+      '2024-09-20': { 'Maths': 'P', 'Web Dev': 'A', 'Khmer': 'P' },
+      '2024-09-21': { 'Maths': 'P', 'Web Dev': 'A', 'Khmer': 'P' },
     }
   },
   {
     id: '08',
     student_name: 'Seyla2',
-    gender: 'M',
+    gender: 'Male',
     attendance: {
-      '2024-09-16': { 'Maths': 'P', 'WebDev': 'A', 'Khmer': 'P'},
-      '2024-09-17': { 'Maths': 'P', 'WebDev': 'P', 'Khmer': 'P' },
-      '2024-09-18': { 'Maths': 'P', 'WebDev': 'A', 'Khmer': 'P' },
-      '2024-09-19': { 'Maths': 'P', 'WebDev': 'A', 'Khmer': 'P' }
+      '2024-09-16': { 'Maths': 'P', 'Web Dev': 'A', 'Khmer': 'P'},
+      '2024-09-17': { 'Maths': 'P', 'Web Dev': 'P', 'Khmer': 'P' },
+      '2024-09-18': { 'Maths': 'P', 'Web Dev': 'A', 'Khmer': 'P' },
+      '2024-09-19': { 'Maths': 'P', 'Web Dev': 'A', 'Khmer': 'P' },
+      '2024-09-20': { 'Maths': 'P', 'Web Dev': 'A', 'Khmer': 'P' },
+      '2024-09-21': { 'Maths': 'P', 'Web Dev': 'A', 'Khmer': 'P' },
+    }
+  },
+  {
+    id: '09',
+    student_name: 'Seyla2',
+    gender: 'Male',
+    attendance: {
+      '2024-09-16': { 'Maths': 'P', 'Web Dev': 'A', 'Khmer': 'P'},
+      '2024-09-17': { 'Maths': 'P', 'Web Dev': 'P', 'Khmer': 'P' },
+      '2024-09-18': { 'Maths': 'P', 'Web Dev': 'A', 'Khmer': 'P' },
+      '2024-09-19': { 'Maths': 'P', 'Web Dev': 'A', 'Khmer': 'P' },
+      '2024-09-20': { 'Maths': 'P', 'Web Dev': 'A', 'Khmer': 'P' },
+      '2024-09-21': { 'Maths': 'P', 'Web Dev': 'A', 'Khmer': 'P' },
+    }
+  },
+  {
+    id: '10',
+    student_name: 'Seyla2',
+    gender: 'Male',
+    attendance: {
+      '2024-09-16': { 'Maths': 'P', 'Web Dev': 'A', 'Khmer': 'P'},
+      '2024-09-17': { 'Maths': 'P', 'Web Dev': 'P', 'Khmer': 'P' },
+      '2024-09-18': { 'Maths': 'P', 'Web Dev': 'A', 'Khmer': 'P' },
+      '2024-09-19': { 'Maths': 'P', 'Web Dev': 'A', 'Khmer': 'P' },
+      '2024-09-20': { 'Maths': 'P', 'Web Dev': 'A', 'Khmer': 'P' },
+      '2024-09-21': { 'Maths': 'P', 'Web Dev': 'A', 'Khmer': 'P' },
     }
   }
 ];
@@ -33,14 +63,24 @@ const StyledTableCell = styled(TableCell)(({ theme, fontSize }) => ({
     fontSize: fontSize || '10px', // Smaller font size
     height: '5px', // Reduced row height
   }));
-
-const AttendanceTable = () => {
-  const dates = [
-    { date: '2024-09-16', day: 'MONDAY' },
-    { date: '2024-09-17', day: 'TUESDAY' },
-    { date: '2024-09-18', day: 'WEDNESDAY' },
-    { date: '2024-09-19', day: 'THURSDAY' }
-  ]; // You can dynamically extract these from the data
+  const AttendanceTable = () => {
+      const dates = [
+          { date: '2024-09-16', day: 'MONDAY' },
+          { date: '2024-09-17', day: 'TUESDAY' },
+          { date: '2024-09-18', day: 'WEDNESDAY' },
+          { date: '2024-09-19', day: 'THURSDAY' },
+          { date: '2024-09-20', day: 'SATURDAY' },
+          { date: '2024-09-21', day: 'SUNDAY' },
+        ]; // You can dynamically extract these from the data
+        const renderedStatusAttendance = (attendance) => {
+            console.log(attendance);
+            return dates.map(date => (
+                subject.map(subject => (
+                  <StyledTableCell align="center">{attendance[date.date] ? attendance[date.date][subject] : '-'}</StyledTableCell>
+                ))
+              ))
+            
+        }
   const subject = ['Maths', 'Web Dev', 'Khmer'];
   return (
     <TableContainer component={Paper}>
@@ -81,13 +121,11 @@ const AttendanceTable = () => {
         <TableBody>
           {attendanceData.map((student, index) => (
             <TableRow key={student.id}>
-              <TableCell align="center">{index + 1}</TableCell>
-              <TableCell align="center">{student.id}</TableCell>
-              <TableCell align="center">{student.student_name}</TableCell>
-              <TableCell align="center">{student.gender}</TableCell>
-              {dates.map(date => (
-                <TableCell align="center">{student.attendance[date.date] ? student.attendance[date.date][subject[0]] : '-'}</TableCell>
-              ))}
+              <StyledTableCell align="center">{index + 1}</StyledTableCell>
+              <StyledTableCell align="center">{student.id}</StyledTableCell>
+              <StyledTableCell align="center">{student.student_name}</StyledTableCell>
+              <StyledTableCell align="center">{student.gender}</StyledTableCell>
+              {renderedStatusAttendance(student.attendance)}
             </TableRow>
           ))}
         </TableBody>
@@ -97,7 +135,7 @@ const AttendanceTable = () => {
 };
 const renderAttendanceStatus = (student, dates, subject) => (
   dates.map(date => (
-    <TableCell align="center">{student.attendance[date.date] ? student.attendance[date.date][subject[0]] : '-'}</TableCell>
+    <StyledTableCell align="center">{student.attendance[date.date] ? student.attendance[date.date][subject[0]] : '-'}</StyledTableCell>
   ))
 );
 export default AttendanceTable;
