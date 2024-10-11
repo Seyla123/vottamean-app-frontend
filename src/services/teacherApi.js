ecada3857ab17252a710478b5367c1a74543bc10
Nem Puthiraksa <raksasparkle@gmail.com>
2024-10-11 22:13:45 +0700

    feature: fix-teacher-form, add formData in teacherapi

--- src/services/teacherApi.js
+++ src/services/teacherApi.js
@@ -1,68 +1,69 @@
 import { baseApi } from './baseApi';
 
 export const teacherApi = baseApi.injectEndpoints({
-    endpoints: (builder) => ({
-        signUpTeacher: builder.mutation({
-            query: (teacher) => ({
-                url: 'teachers',
-                method: 'POST',
-                body: teacher
-            }),
-            invalidatesTags: ['Teachers']
-        }),
-        getAllTeachers: builder.query({
-            query: (params) => ({
-              url: 'teachers',
-              params: params,
-            }),
-            providesTags: ['Teachers'],
-        }),
-        deleteTeacher: builder.mutation({
-            query: (id) => ({
-                url: `teachers/${id}`,
-                method: 'DELETE'
-            }),
-            invalidatesTags: ['Teachers'],
-        }),
-        updateTeacher: builder.mutation({
-            query: ({ id, updates }) => ({
-                url: `teachers/${id}`,
-                method: 'PUT',
-                body: updates
-            }),
-            invalidatesTags: ['Teachers'],
-        }),
-        getTeacher: builder.query({
-            query: (id) => `teachers/${id}`,
-            providesTags: ['Teachers'],
-        }),
-        getTeacherScheduleClasses: builder.query({
-            query: (data) => ({
-                url: `teachers/sessions`,
-                method: 'GET',
-                params: {
-                    filter: data?.filter || ''
-                }
-            }),
-            providesTags: ['Teachers'],
-        }),
-        getAllStudentsByClassInSession: builder.query({
-            query: (id) => ({
-              url: `teachers/sessions/${id}`,
-              method: 'GET',
-              credentials: 'include',
-            }),
-            providesTags: ['Teachers'],
-        })
-    })
-})
+  endpoints: (builder) => ({
+    signUpTeacher: builder.mutation({
+      query: (formData) => ({
+        url: 'teachers',
+        method: 'POST',
+        body: formData,
+        formData: true,
+      }),
+      invalidatesTags: ['Teachers'],
+    }),
+    getAllTeachers: builder.query({
+      query: (params) => ({
+        url: 'teachers',
+        params: params,
+      }),
+      providesTags: ['Teachers'],
+    }),
+    deleteTeacher: builder.mutation({
+      query: (id) => ({
+        url: `teachers/${id}`,
+        method: 'DELETE',
+      }),
+      invalidatesTags: ['Teachers'],
+    }),
+    updateTeacher: builder.mutation({
+      query: ({ id, updates }) => ({
+        url: `teachers/${id}`,
+        method: 'PUT',
+        body: updates,
+      }),
+      invalidatesTags: ['Teachers'],
+    }),
+    getTeacher: builder.query({
+      query: (id) => `teachers/${id}`,
+      providesTags: ['Teachers'],
+    }),
+    getTeacherScheduleClasses: builder.query({
+      query: (data) => ({
+        url: `teachers/sessions`,
+        method: 'GET',
+        params: {
+          filter: data?.filter || '',
+        },
+      }),
+      providesTags: ['Teachers'],
+    }),
+    getAllStudentsByClassInSession: builder.query({
+      query: (id) => ({
+        url: `teachers/sessions/${id}`,
+        method: 'GET',
+        credentials: 'include',
+      }),
+      providesTags: ['Teachers'],
+    }),
+  }),
+});
 
 export const {
-    useSignUpTeacherMutation,
-    useGetAllTeachersQuery, 
-    useDeleteTeacherMutation,
-    useUpdateTeacherMutation,
-    useGetTeacherQuery,
-    useGetTeacherScheduleClassesQuery,
-    useGetAllStudentsByClassInSessionQuery
+  useSignUpTeacherMutation,
+  useGetAllTeachersQuery,
+  useDeleteTeacherMutation,
+  useUpdateTeacherMutation,
+  useGetTeacherQuery,
+  useGetTeacherScheduleClassesQuery,
+  useGetAllStudentsByClassInSessionQuery,
 } = teacherApi;
