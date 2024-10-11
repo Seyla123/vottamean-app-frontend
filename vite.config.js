62bfc8717b59e3557292e990dae901838a777b4b
NarakCODE <channarakluy@gmail.com>
2024-10-11 22:40:44 +0700

    feature  : auto redirect to signin page

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,11 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
-    server: {
-        host: '0.0.0.0',
-        port: 5173,
-        historyApiFallback: true,
-    },
+  plugins: [react()],
+  server: {
+    host: '0.0.0.0',
+    port: 5173,
+    historyApiFallback: true,
+    open: '/auth/signin',
+  },
 });
