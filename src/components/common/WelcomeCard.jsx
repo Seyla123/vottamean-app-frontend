79bf59ee20f3176afc9f09a5d264a0adfe55638c
Seav <Mrrseyla.758@gmail.com>
2024-10-13 19:16:56 +0700

    feature : aded WelcomeCard component

---
+++ src/components/common/WelcomeCard.jsx
@@ -0,0 +1,69 @@
+import React from 'react';
+import { Box, Typography, Paper, Chip } from '@mui/material';
+import { styled } from '@mui/system';
+import { shadow } from '../../styles/global';
+import greetingImage from '../../assets/images/startup-1.svg';
+import { School, Subtitles } from 'lucide-react';
+
+const StyledPaper = styled(Paper)(({ theme }) => ({
+    padding: theme.spacing(3),
+    marginBottom: theme.spacing(3),
+    background: 'linear-gradient(45deg, #4e54c8 30%, #EBD3F8  70%)',
+    color: 'white',
+    height: '100%',
+    width: '100%',
+    overflow: 'hidden',
+  }));
+function WelcomeCard({name, subTitle, schoolName,img=greetingImage}) {
+
+    const currentHour = new Date().getHours();
+    let greeting = 'Good evening';
+  
+    if (currentHour < 12) {
+      greeting = 'Good morning';
+    } else if (currentHour < 18) {
+      greeting = 'Good afternoon';
+    }
+    
+    return (
+        <StyledPaper elevation={3} sx={shadow}>
+            <Box
+                position={'relative'}
+                sx={{
+                    display: 'flex',
+                    alignItems: 'center',
+                    width: '100%',
+                    height: '100%',
+                }}
+            >
+                <Box sx={{ position: 'relative', zIndex: '100', width: '100%' }}>
+                    <Chip
+                        icon={<School size={16} />}
+                        label={schoolName || 'School Name'}
+                        color="primary"
+                        sx={{ mb: 3 }}
+                    />
+                    <Typography variant="h4" gutterBottom>
+                        {greeting}, {name}! 👋
+                    </Typography>
+                    <Typography variant="subtitle1">
+                       {subTitle}
+                    </Typography>
+                </Box>
+                <img
+                    src={img}
+                    alt="greeting"
+                    style={{
+                        width: '300px',
+                        objectFit: 'contain',
+                        position: 'absolute',
+                        right: 0,
+                        zIndex: 1,
+                    }}
+                />
+            </Box>
+        </StyledPaper>
+    )
+}
+
+export default WelcomeCard
