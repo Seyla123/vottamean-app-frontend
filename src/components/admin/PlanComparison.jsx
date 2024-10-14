ae0216bd57109140759664db7214187d037b6c23
NarakCODE <channarakluy@gmail.com>
2024-10-14 15:12:25 +0700

    feature : Plan comparison mobile view

--- src/components/admin/PlanComparison.jsx
+++ src/components/admin/PlanComparison.jsx
@@ -9,6 +9,9 @@ import {
   Paper,
   Typography,
   Box,
+  useTheme,
+  useMediaQuery,
+  Grid,
 } from '@mui/material';
 import { Check, X } from 'lucide-react';
 import { shadow } from '../../styles/global';
@@ -18,27 +21,97 @@ const PlanComparison = ({ plans }) => {
     new Set(plans.flatMap((plan) => plan.features)),
   );
 
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
+
   return (
-    <Paper sx={{ width: '100%', margin: 'auto', boxShadow: shadow }}>
-      <Typography variant="h4" sx={{ p: 2 }}>
-        Plan Comparison
-      </Typography>
-      <TableContainer>
-        <Table aria-label="plan comparison table">
-          <TableHead>
-            <TableRow>
-              <TableCell>Feature</TableCell>
-              {plans.map((plan) => (
-                <TableCell key={plan.type} align="center">
-                  <Box
-                    sx={{
-                      display: 'flex',
-                      flexDirection: 'column',
-                      alignItems: 'center',
-                    }}
-                  >
+    <>
+      {!isMobile ? (
+        <>
+          <Typography variant="h5" sx={{ p: 2 }} fontWeight={'bold'}>
+            Plan Comparison
+          </Typography>
+          <Paper sx={{ width: '100%', margin: 'auto', boxShadow: shadow }}>
+            <TableContainer>
+              <Table aria-label="plan comparison table">
+                <TableHead>
+                  <TableRow>
+                    <TableCell>
+                      <Typography variant="h6" fontWeight={'bold'}>
+                        Feature
+                      </Typography>
+                    </TableCell>
+                    {plans.map((plan) => (
+                      <TableCell key={plan.type} align="center">
+                        <Box
+                          sx={{
+                            display: 'flex',
+                            flexDirection: 'column',
+                            alignItems: 'center',
+                          }}
+                        >
+                          <Box color={'primary.main'}>
+                            {plan.icon}
+                            <Typography variant="subtitle1" fontWeight={'bold'}>
+                              {plan.type}
+                            </Typography>
+                          </Box>
+                          <Typography variant="body2">
+                            {plan.monthlyPrice}/month
+                          </Typography>
+                          <Typography variant="body2">
+                            {plan.yearlyPrice}/year
+                          </Typography>
+                        </Box>
+                      </TableCell>
+                    ))}
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {allFeatures.map((feature, index) => (
+                    <TableRow
+                      key={index}
+                      sx={{
+                        '&:nth-of-type(odd)': {
+                          backgroundColor: 'action.hover',
+                        },
+                      }}
+                    >
+                      <TableCell component="th" scope="row">
+                        {feature}
+                      </TableCell>
+                      {plans.map((plan) => (
+                        <TableCell
+                          key={`${plan.type}-${feature}`}
+                          align="center"
+                        >
+                          {plan.features.includes(feature) ? (
+                            <Check color="green" />
+                          ) : (
+                            <X color="red" />
+                          )}
+                        </TableCell>
+                      ))}
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            </TableContainer>
+          </Paper>
+        </>
+      ) : (
+        <Paper sx={{ width: '100%', margin: 'auto', boxShadow: shadow, p: 2 }}>
+          <Grid container spacing={3}>
+            {plans.map((plan) => (
+              <>
+                <Grid item xs={6} key={plan.type}>
+                  <Box color={'primary.main'}>
                     {plan.icon}
-                    <Typography variant="subtitle1">{plan.type}</Typography>
+                    <Typography variant="subtitle1" fontWeight={'bold'}>
+                      {plan.type}
+                    </Typography>
+                  </Box>{' '}
+                  <Box>
                     <Typography variant="body2">
                       {plan.monthlyPrice}/month
                     </Typography>
@@ -46,36 +119,19 @@ const PlanComparison = ({ plans }) => {
                       {plan.yearlyPrice}/year
                     </Typography>
                   </Box>
-                </TableCell>
-              ))}
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {allFeatures.map((feature, index) => (
-              <TableRow
-                key={feature}
-                sx={{
-                  '&:nth-of-type(odd)': { backgroundColor: 'action.hover' },
-                }}
-              >
-                <TableCell component="th" scope="row">
-                  {feature}
-                </TableCell>
-                {plans.map((plan) => (
-                  <TableCell key={`${plan.type}-${feature}`} align="center">
-                    {plan.features.includes(feature) ? (
-                      <Check color="green" />
-                    ) : (
-                      <X color="red" />
-                    )}
-                  </TableCell>
-                ))}
-              </TableRow>
+                </Grid>
+                <Grid item xs={6}>
+                  <Typography variant='body2'>{plan.description}</Typography>
+                  <Typography variant="h6" fontWeight={'bold'}>
+                    {plan.features.length} Features
+                  </Typography>
+                </Grid>
+              </>
             ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
-    </Paper>
+          </Grid>
+        </Paper>
+      )}
+    </>
   );
 };
 
