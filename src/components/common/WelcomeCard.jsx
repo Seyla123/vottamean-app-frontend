43e209c575fb99f734512b9f6252cde68067ce06
NarakCODE <channarakluy@gmail.com>
2024-10-14 17:01:42 +0700

    change proxy

--- src/components/common/WelcomeCard.jsx
+++ src/components/common/WelcomeCard.jsx
@@ -7,85 +7,73 @@ import { School } from 'lucide-react';
 import CircularProgress from '@mui/material/CircularProgress';
 
 const StyledPaper = styled(Paper)(({ theme }) => ({
-    padding: theme.spacing(3),
-    marginBottom: theme.spacing(3),
-    background: 'linear-gradient(45deg, #4e54c8 30%, #EBD3F8  70%)',
-    color: 'white',
-    height: '100%',
-    width: '100%',
-    overflow: 'hidden',
+  padding: theme.spacing(3),
+  marginBottom: theme.spacing(3),
+  background: 'linear-gradient(45deg, #4e54c8 30%, #EBD3F8  70%)',
+  color: 'white',
+  height: '100%',
+  width: '100%',
+  overflow: 'hidden',
 }));
-function WelcomeCard({ name, subTitle, schoolName, img = greetingImage, isLoading }) {
+function WelcomeCard({
+  name,
+  subTitle,
+  schoolName,
+  img = greetingImage,
+  isLoading,
+}) {
+  const currentHour = new Date().getHours();
+  let greeting = 'Good evening';
 
-    const currentHour = new Date().getHours();
-    let greeting = 'Good evening';
+  if (currentHour < 12) {
+    greeting = 'Good morning';
+  } else if (currentHour < 18) {
+    greeting = 'Good afternoon';
+  }
 
-    if (currentHour < 12) {
-        greeting = 'Good morning';
-    } else if (currentHour < 18) {
-        greeting = 'Good afternoon';
-    }
-
-    if (isLoading) {
-        return <Skeleton
-            variant="rectangular"
-            height={200}
-            sx={{ borderRadius: 4 }}
-        />
-    }
-
+  if (isLoading) {
     return (
-        <StyledPaper elevation={3} sx={shadow}>
+      <Skeleton variant="rectangular" height={200} sx={{ borderRadius: 4 }} />
+    );
+  }
 
-            <Box
-                position={'relative'}
-                sx={{
-                    display: 'flex',
-                    alignItems: 'center',
-                    width: '100%',
-                    height: '100%',
-                }}
-            >
-                <Box sx={{ position: 'relative', zIndex: '100', width: '100%' }}>
-<<<<<<< HEAD
-
-=======
->>>>>>> refs/remotes/origin/feature/teacher-site-class-session
-                    <Chip
-                        icon={<School size={16} />}
-                        label={schoolName || 'School Name'}
-                        color="primary"
-                        sx={{ mb: 3 }}
-                    />
-                    <Typography variant="h4" gutterBottom>
-                        {greeting}, {name}! 👋
-                    </Typography>
-                    <Typography variant="subtitle1">
-<<<<<<< HEAD
-                        {subTitle}
-=======
-                       {subTitle}
->>>>>>> refs/remotes/origin/feature/teacher-site-class-session
-                    </Typography>
-                </Box>
-                <img
-                    src={img}
-                    alt="greeting"
-                    style={{
-                        width: '300px',
-                        objectFit: 'contain',
-                        position: 'absolute',
-                        right: 0,
-                        zIndex: 1,
-                    }}
-                />
-<<<<<<< HEAD
-
-=======
->>>>>>> refs/remotes/origin/feature/teacher-site-class-session
-            </Box>
-        </StyledPaper>
-    )
+  return (
+    <StyledPaper elevation={3} sx={shadow}>
+      <Box
+        position={'relative'}
+        sx={{
+          display: 'flex',
+          alignItems: 'center',
+          width: '100%',
+          height: '100%',
+        }}
+      >
+        <Box sx={{ position: 'relative', zIndex: '100', width: '100%' }}>
+          <Chip
+            icon={<School size={16} />}
+            label={schoolName || 'School Name'}
+            color="primary"
+            sx={{ mb: 3 }}
+          />
+          <Typography variant="h4" gutterBottom>
+            {greeting}, {name}! 👋
+          </Typography>
+          <Typography variant="subtitle1">{subTitle}</Typography>
+        </Box>
+        <img
+          src={img}
+          alt="greeting"
+          style={{
+            width: '300px',
+            objectFit: 'contain',
+            position: 'absolute',
+            right: 0,
+            zIndex: 1,
+          }}
+        />
+      </Box>
+    </StyledPaper>
+  );
 }
 
-export default WelcomeCard
+export default WelcomeCard;
