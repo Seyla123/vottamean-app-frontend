cb0a44a8ac29467081787ffeac5cac27c079c4df
Seav <Mrrseyla.758@gmail.com>
2024-10-15 18:53:11 +0700

    feature : add pagination in attendance list , add selece check box and delete many api

--- src/components/attendance/AttendanceListTable.jsx
+++ src/components/attendance/AttendanceListTable.jsx
@@ -16,12 +16,19 @@ import {
   Box,
   TablePagination,
   Typography,
-  CircularProgress
+  Checkbox,
+  CircularProgress,
+  Button
 } from '@mui/material';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
-import NotFoundImage from '../../assets/images/not-found.jpg';
 import StatusChip from '../common/StatusChip';
 import { truncate } from '../../utils/truncate';
+import EmptyDataImage from '../../assets/images/empty-image.svg';
+import StyledButton from './../common/StyledMuiButton';
+import DeleteConfirmationModal from '../common/DeleteConfirmationModal';
+
+import { Trash2 } from 'lucide-react';
+
 
 const AttendanceListTable = ({
   rows,
@@ -29,15 +36,26 @@ const AttendanceListTable = ({
   hideColumns = [],
   handleView,
   handleDelete,
-  loading
+  onSelectedDelete,
+  loading,
+  page,
+  rowsPerPage,
+  setPage,
+  setRowsPerPage,
+  totalRows,
+  idField = 'attendance_id',
 }) => {
+  const [selected, setSelected] = useState([]);
   const [anchorEl, setAnchorEl] = useState(null);
   const [selectedRow, setSelectedRow] = useState(null);
-  const [page, setPage] = useState(0);
-  const [rowsPerPage, setRowsPerPage] = useState(10);
+
+  const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false);
 
   const theme = useTheme();
   const isMobile = useMediaQuery(theme.breakpoints.down('lg'));
+
+
+  console.log('this is rows', rows);
 
   const handleClick = (event, row) => {
     setAnchorEl(event.currentTarget);
@@ -56,7 +74,7 @@ const AttendanceListTable = ({
     }
   };
 
-  const visibleColumns = columns.filter((col) => 
+  const visibleColumns = columns.filter((col) =>
     isMobile ? !hideColumns.includes(col.id) : true
   );
 
@@ -66,75 +84,162 @@ const AttendanceListTable = ({
     setRowsPerPage(parseInt(event.target.value, 10));
     setPage(0);
   };
+  const height = rowsPerPage === 5 ? '300px' : '700px';
 
+
+  const handleSelectedDelete = () => {
+    if (selected.length > 0) {
+      setIsDeleteModalOpen(true);
+    } else {
+      console.log('No rows selected for deletion');
+    }
+  };
+
+  const handleConfirmDelete = () => {
+    if (onSelectedDelete) {
+      console.log('Deleting selected rows:', selected);
+      onSelectedDelete(selected);
+      setSelected([]);
+    }
+    setIsDeleteModalOpen(false);
+  };
+
+  const handleCancelDelete = () => {
+    setIsDeleteModalOpen(false);
+  };
+
+  const handleSelectAllClick = (event) => {
+    if (event.target.checked) {
+      const newSelecteds = rows.map((n) => n[idField]);
+      setSelected(newSelecteds);
+      console.log('All rows selected:', newSelecteds);
+    } else {
+      setSelected([]);
+      console.log('All rows deselected');
+    }
+  };
+  const isSelected = (id) => selected.indexOf(id) !== -1;
+  const handleCheckboxClick = (event, id) => {
+    event.stopPropagation();
+    const selectedIndex = selected.indexOf(id);
+    let newSelected = [];
+
+    if (selectedIndex === -1) {
+      newSelected = newSelected.concat(selected, id);
+    } else if (selectedIndex === 0) {
+      newSelected = newSelected.concat(selected.slice(1));
+    } else if (selectedIndex === selected.length - 1) {
+      newSelected = newSelected.concat(selected.slice(0, -1));
+    } else if (selectedIndex > 0) {
+      newSelected = newSelected.concat(
+        selected.slice(0, selectedIndex),
+        selected.slice(selectedIndex + 1),
+      );
+    }
+
+    setSelected(newSelected);
+    console.log('Selected row ID:', id);
+    console.log('Updated selected IDs:', newSelected);
+  };
   return (
-    <Paper sx={{ boxShadow:'none' }}>
+    <Paper sx={{ boxShadow: 'none' }}>
       <TableContainer>
         <Table aria-label="sticky table">
-          <TableHead sx={{ height: '60px'}}>
+          <TableHead sx={{ height: '60px' }}>
             <TableRow>
+              <TableCell padding="checkbox">
+                <Checkbox
+                  indeterminate={
+                    selected.length > 0 && selected.length < rows.length
+                  }
+                  checked={rows.length > 0 && selected.length === rows.length}
+                  onChange={handleSelectAllClick}
+                />
+              </TableCell>
               {visibleColumns.map((column) => (
                 <TableCell key={column.id} sx={{ width: column.id === 'id' ? '' : 'auto' }}>
                   {column.label}
                 </TableCell>
               ))}
+                {selected.length > 0 ? (
+              <TableCell align="right" sx={{ maxWidth: '50px', paddingY: '0px' }}>
+                <StyledButton
+                  onClick={handleSelectedDelete}
+                  color="error"
+                  startIcon={<Trash2 size={18} />}
+                
+                />
+              </TableCell>
+            ) : (
               <TableCell align="right">Status / Action</TableCell>
+            )}
             </TableRow>
           </TableHead>
           <TableBody>
-            {!loading && rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((row, index) => (
-              <TableRow key={index}>
-                {visibleColumns.map((column) => (
-                  <TableCell key={column.id}>
-                    {column.id === 'name' ? (
-                      <Box sx={{ display: 'flex', alignItems: 'center' }}>
-                        <Avatar src={row.img} alt={`Avatar ${row.name}`} sx={{ width: 32, height: 32, marginRight: '8px', objectFit: 'cover' }} />
-                        {row[column.id]}
-                      </Box>
-                    ) : column.id === 'id' ? (
-                      row[column.id]
-                    ) : (
-                      truncate(row[column.id], 30)
-                    )}
+            {loading ? (<LoadingTable columns={visibleColumns} height={height} />
+            ) : rows.length > 0 ? (
+              rows.map((row, index) => {
+                const isItemSelected = isSelected(row[idField]);
+                const labelId = `enhanced-table-checkbox-${index}`;
+
+                return <TableRow
+                  hover
+                  onClick={(event) => handleCheckboxClick(event, row[idField])}
+                  role="checkbox"
+                  aria-checked={isItemSelected}
+                  tabIndex={-1}
+                  key={row[idField]}>
+                  <TableCell padding="checkbox">
+                    <Checkbox
+                      checked={isItemSelected}
+                      inputProps={{ 'aria-labelledby': labelId }}
+                    />
                   </TableCell>
-                ))}
-                <TableCell sx={{ width: 'auto' }}>
-                  <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'flex-end' }}>
-                    <StatusChip statusId={row.status_id} />
-                    <IconButton size="small" onClick={(e) => handleClick(e, row)}>
-                      <MoreVertIcon fontSize="small" />
-                    </IconButton>
-                  </Box>
-                </TableCell>
-              </TableRow>
-            ))}
+                  {visibleColumns.map((column) => (
+                    <TableCell key={column.id}>
+                      {column.id === 'name' ? (
+                        <Box sx={{ display: 'flex', alignItems: 'center' }}>
+                          <Avatar src={row.img} alt={`Avatar ${row.name}`} sx={{ width: 32, height: 32, marginRight: '8px', objectFit: 'cover' }} />
+                          {row[column.id]}
+                        </Box>
+                      ) : column.id === 'id' ? (
+                        row[column.id]
+                      ) : (
+                        truncate(row[column.id], 30)
+                      )}
+                    </TableCell>
+                  ))}
+                  <TableCell sx={{ width: 'auto' }}>
+                    <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'flex-end' }}>
+                      <StatusChip statusId={row.status_id} />
+                      <IconButton size="small" onClick={(e) => handleClick(e, row)}>
+                        <MoreVertIcon fontSize="small" />
+                      </IconButton>
+                    </Box>
+                  </TableCell>
+                </TableRow>
+
+              })) : <EmptyTable columns={visibleColumns} emptyTitle={'No Attendance Record'} emptySubTitle={'No Attendance Records Available'}></EmptyTable>}
           </TableBody>
         </Table>
-        
-        {loading ? (
-          <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '500px', width: '100%' }}>
-            <CircularProgress />
-          </Box>
-        ) : rows.length === 0 && (
-          <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '500px', width: '100%' }}>
-            <Box>
-              <img src={NotFoundImage} alt="Not Found" width="400px" height="auto" style={{ objectFit: 'cover' }} />
-              <Typography variant="h6" color="text.primary" textAlign="center">No Attendance Record</Typography>
-            </Box>
-          </Box>
-        )}
+        <DeleteConfirmationModal
+        open={isDeleteModalOpen}
+        onClose={handleCancelDelete}
+        onConfirm={handleConfirmDelete}
+        itemName={`${selected.length} selected item${selected.length > 1 ? 's' : ''}`}
+      />
       </TableContainer>
 
       <TablePagination
         rowsPerPageOptions={[5, 10, 25]}
         component="div"
-        count={rows.length}
+        count={totalRows}
         rowsPerPage={rowsPerPage}
         page={page}
         onPageChange={handleChangePage}
         onRowsPerPageChange={handleChangeRowsPerPage}
       />
-      
+
       <Menu
         open={Boolean(anchorEl)}
         anchorEl={anchorEl}
@@ -144,6 +249,64 @@ const AttendanceListTable = ({
         <MenuItem onClick={() => handleMenuAction('delete')}>Delete</MenuItem>
       </Menu>
     </Paper>
+  );
+};
+
+const EmptyTable = ({ columns, emptyTitle, emptySubTitle }) => {
+  return (
+    <TableRow>
+      <TableCell
+        colSpan={columns.length + 2}
+        sx={{
+          height: '600px',
+          textAlign: 'center',
+          verticalAlign: 'middle',
+        }}
+      >
+        <div
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            justifyContent: 'center',
+            height: '100%',
+          }}
+        >
+          <Box sx={{ width: '100%', height: '300px' }}>
+            <img
+              src={EmptyDataImage}
+              alt="empty"
+              style={{
+                width: '100%',
+                height: '100%',
+                objectFit: 'contain',
+              }}
+            />
+          </Box>
+          <Typography variant="h6" gutterBottom>
+            {emptyTitle}
+          </Typography>
+          <Typography variant="body2">{emptySubTitle}</Typography>
+        </div>
+      </TableCell>
+    </TableRow>
+  );
+};
+const LoadingTable = ({ columns, height }) => {
+
+  return (
+    <TableRow>
+      <TableCell
+        colSpan={columns.length + 2}
+        sx={{
+          height: height,
+          textAlign: 'center',
+          verticalAlign: 'middle',
+        }}
+      >
+        <CircularProgress />
+      </TableCell>
+    </TableRow>
   );
 };
 
--- src/pages/admin/report/attendance/AttendanceListPage.jsx
+++ src/pages/admin/report/attendance/AttendanceListPage.jsx
@@ -1,24 +1,28 @@
-import React, { useEffect, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { useNavigate } from "react-router-dom";
-import { useGetAllAttendanceQuery, useDeleteAttendanceMutation } from "../../../../services/attendanceApi";
-import { setModal, setSnackbar } from "../../../../store/slices/uiSlice";
-import { transformAttendanceData } from "../../../../utils/formatData";
-import FormComponent from "../../../../components/common/FormComponent";
-import AttendanceTable from "../../../../components/attendance/AttendanceListTable";
-import LoadingCircle from "../../../../components/loading/LoadingCircle";
-import ReportHeader from "../../../../components/attendance/ReportHeader";
-import DeleteConfirmationModal from "../../../../components/common/DeleteConfirmationModal";
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import {
+  useGetAllAttendanceQuery,
+  useDeleteAttendanceMutation,
+  useDeleteManyAttendanceMutation,
+} from '../../../../services/attendanceApi';
+import { setModal, setSnackbar } from '../../../../store/slices/uiSlice';
+import { transformAttendanceData } from '../../../../utils/formatData';
+import FormComponent from '../../../../components/common/FormComponent';
+import AttendanceTable from '../../../../components/attendance/AttendanceListTable';
+import LoadingCircle from '../../../../components/loading/LoadingCircle';
+import ReportHeader from '../../../../components/attendance/ReportHeader';
+import DeleteConfirmationModal from '../../../../components/common/DeleteConfirmationModal';
 import AttendanceFilter from '../../../../components/attendance/AttendanceFilter';
-import { Box, Stack, Divider } from '@mui/material'
-import { tableShadow, shadow } from './../../../../styles/global'
+import { Box, Stack, Divider } from '@mui/material';
+import { tableShadow, shadow } from './../../../../styles/global';
 
 const columns = [
-  { id: "name", label: "Name" },
-  { id: "time", label: "Time" },
-  { id: "subject", label: "Subject" },
-  { id: "class", label: "Class" },
-  { id: "address", label: "Address" },
+  { id: 'name', label: 'Name' },
+  { id: 'time', label: 'Time' },
+  { id: 'subject', label: 'Subject' },
+  { id: 'class', label: 'Class' },
+  { id: 'address', label: 'Address' },
 ];
 
 const AttendanceListPage = () => {
@@ -37,33 +41,104 @@ const AttendanceListPage = () => {
   // - open: the state of the delete confirmation modal
   const { modal } = useSelector((state) => state.ui);
 
+  // - rowsPerPage: the number of rows per page
+  // - page: the current page number that is being displayed
+  // - totalRows: the total number of rows that are available
+  const [rowsPerPage, setRowsPerPage] = useState(5);
+  const [page, setPage] = useState(0);
+  const [totalRows, setTotalRows] = useState(0);
+
   // - useGetAllAttendanceQuery: a hook that returns a function to fetch all attendance records
-  const { data: allAttendanceData, isLoading, isSuccess, isFetching } = useGetAllAttendanceQuery(filter);
+  const {
+    data: allAttendanceData,
+    isLoading,
+    isSuccess,
+    isFetching,
+  } = useGetAllAttendanceQuery({
+    page: page + 1,
+    limit: rowsPerPage,
+    ...filter,
+  });
 
   // - useDeleteAttendanceMutation: a hook that returns a function to delete an attendance record
-  const [deleteAttendance, { isError, error, isLoading: isDeleting, isSuccess: isDeleted }] = useDeleteAttendanceMutation();
+  const [
+    deleteAttendance,
+    { isLoading: isDeleting,
+      isSuccess: isDeleteSuccess,
+      isError: isDeleteError,
+      error: deleteError, },
+  ] = useDeleteAttendanceMutation();
 
+  const [
+    deleteManyAttendance,
+    {
+      isLoading: isDeletingMany,
+      isSuccess: isDeleteManySuccess,
+      isError: isDeleteManyError,
+      error: deleteManyError,
+    },
+  ] = useDeleteManyAttendanceMutation();
   // - when the attendance records are fetched successfully, transform the data and set the rows state
   useEffect(() => {
     if (isSuccess && allAttendanceData) {
       const formattedData = transformAttendanceData(allAttendanceData.data);
       setRows(formattedData);
+      setTotalRows(allAttendanceData.results);
     }
-  }, [allAttendanceData, isDeleted]);
+  }, [allAttendanceData, isSuccess]);
 
   // when delete is in progress, show a snackbar with a message "Deleting..."
   // when delete is failed, show a snackbar with an error message
-  // when delete is successful, show a snackbar with a success message and navigate to the attendance list page
+  // when delete is successful, show a snackbar with a success message
   useEffect(() => {
-    if (isDeleting) {
-      dispatch(setSnackbar({ open: true, message: 'Deleting...', severity: 'info' }));
-    } else if (isError) {
-      dispatch(setSnackbar({ open: true, message: error.data.message, severity: 'error' }));
-    } else if (isDeleted) {
-      dispatch(setSnackbar({ open: true, message: 'Deleted successfully', severity: 'success' }));
-      navigate('/admin/reports/attendance');
+    if (isDeleting || isDeletingMany) {
+      dispatch(
+        setSnackbar({ open: true, message: 'Deleting...', severity: 'info' }),
+      );
+    } else if (isDeleteSuccess || isDeleteManySuccess) {
+      dispatch(
+        setSnackbar({
+          open: true,
+          message: 'Deleted successfully',
+          severity: 'success',
+        }),
+      );
+    } else if (isDeleteError) {
+      dispatch(
+        setSnackbar({
+          open: true,
+          message: deleteError.data?.message || 'Failed to delete subject',
+          severity: 'error',
+        }),
+      );
+    } else if (isDeleteManyError) {
+      dispatch(
+        setSnackbar({
+          open: true,
+          message: deleteManyError.data?.message || 'Failed to delete subjects',
+          severity: 'error',
+        }),
+      );
     }
-  }, [isDeleting, isError, isDeleted, navigate]);
+  }, [
+    isDeleting,
+    isDeleteError,
+    isDeleteSuccess,
+    deleteError,
+    isDeletingMany,
+    isDeleteManyError,
+    deleteManyError,
+    dispatch,
+  ]);
+
+  // Handle page change
+  const handleChangePage = (newPage) => {
+    setPage(newPage);
+  };
+  // Handle row per page change
+  const handleChangeRowsPerPage = (newRowsPerPage) => {
+    setRowsPerPage(newRowsPerPage);
+  };
 
   // handle delete action
   const onDelete = (id) => {
@@ -76,7 +151,11 @@ const AttendanceListPage = () => {
     dispatch(setModal({ open: false }));
     await deleteAttendance({ id: itemToDelete }).unwrap();
   };
-
+  // handle delete multiple subjects
+  const handleSelectedDelete = async (selectedIds) => {
+    console.log('this select mul : ', selectedIds);
+    await deleteManyAttendance(selectedIds).unwrap();
+  };
   // handle view action
   const onView = (id) => {
     navigate(`/admin/attendance/${id}`);
@@ -86,27 +165,32 @@ const AttendanceListPage = () => {
     return <LoadingCircle />;
   }
 
-
   return (
-    <FormComponent title={"Attendance List"}>
+    <FormComponent title={'Attendance List'}>
       <ReportHeader data={rows} title={filter.filterLabel} />
       <Stack sx={tableShadow}>
-      <AttendanceFilter />
-      <Divider />
-      <AttendanceTable
-        rows={rows}
-        columns={columns}
-        hideColumns={["id", "subject", 'class', "address"]}
-        handleDelete={onDelete}
-        handleView={onView}
-        loading={isFetching}
-      />
+        <AttendanceFilter />
+        <Divider />
+        <AttendanceTable
+          rows={rows}
+          columns={columns}
+          hideColumns={['id', 'subject', 'class', 'address']}
+          handleDelete={onDelete}
+          handleView={onView}
+          onSelectedDelete={handleSelectedDelete}
+          loading={isLoading}
+          page={page}
+          rowsPerPage={rowsPerPage}
+          setPage={handleChangePage}
+          setRowsPerPage={handleChangeRowsPerPage}
+          totalRows={totalRows}
+        />
       </Stack>
       <DeleteConfirmationModal
         open={modal.open}
         onClose={() => dispatch(setModal({ open: false }))}
         onConfirm={confirmDelete}
-        itemName={"attendance"}
+        itemName={'attendance'}
       />
     </FormComponent>
   );
