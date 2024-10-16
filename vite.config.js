8e2559313a1153ae50111a57ae3c0cb1e618e06b
Seang Makaramarady <seangdy085@gmail.com>
2024-10-16 09:46:18 +0700

    config : resolve vite configuration

--- vite.config.js
+++ vite.config.js
@@ -19,7 +19,7 @@ export default defineConfig(({ mode }) => {
       },
     },
     build: {
-      minify: 'terser',
+      minify: 'esbuild',
       rollupOptions: {
         output: {
           manualChunks(id) {
