4744c5bbf665a2281bff570d79392ee190751f33
Seang Makaramarady <seangdy085@gmail.com>
2024-10-17 10:40:37 +0700

    config : config the vite.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   return {
+    base: mode === 'production' ? '/app/' : '/', 
     plugins: [react()],
     server: {
       host: '0.0.0.0',
