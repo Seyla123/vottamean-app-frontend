dc891666c4d37bdb52a44d99a0a6c7f6b8502c18
Seang Makaramarady <seangdy085@gmail.com>
2024-10-17 12:14:43 +0700

    change vondig

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   return {
-    base: mode === 'production' ? '/app/' : '/', 
     plugins: [react()],
     server: {
       host: '0.0.0.0',
