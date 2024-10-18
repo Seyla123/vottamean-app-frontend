087f309f7976e2af1a69ba3cec9cb8b6320170c4
NarakCODE <channarakluy@gmail.com>
2024-10-18 11:34:16 +0700

    feature : change shadow

--- src/styles/global.js
+++ src/styles/global.js
@@ -1,25 +1,26 @@
 // shadow
 export const shadow = {
   borderRadius: '8px',
-  border: '0.2px solid rgba(0, 0, 0, 0.05)',
-  boxShadow: ' rgba(0, 0, 0, 0.16) 0px 1px 4px',
+  border: '1px solid #e0e0e0',
+  boxShadow: 'rgba(0, 0, 0, 0.05) 0px 1px 2px 0px',
 };
+
 // card card container
 export const cardContainer = {
-    width: '100%',
-    padding: { xs: 3, sm: 4 },
-    display: 'flex',
-    flexDirection: 'column',
-    gap: 2,
-    ...shadow,
+  width: '100%',
+  padding: { xs: 3, sm: 4 },
+  display: 'flex',
+  flexDirection: 'column',
+  gap: 2,
+  ...shadow,
 };
 export const tableShadow = {
-    borderRadius: '4px',
-    boxShadow: '0px 1px 4px rgba(0, 0, 0, 0.16)',
+  borderRadius: '4px',
+  boxShadow: '0px 1px 4px rgba(0, 0, 0, 0.16)',
 };
 
 export const longText = {
-    overflow: "hidden",
-    textOverflow: "ellipsis",
-    whiteSpace: "nowrap"
-}
+  overflow: 'hidden',
+  textOverflow: 'ellipsis',
+  whiteSpace: 'nowrap',
+};
