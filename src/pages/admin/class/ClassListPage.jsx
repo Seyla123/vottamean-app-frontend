5a59d9b6103af7fe2cd63cd0a8ced4fd72a478e8
Seav <Mrrseyla.758@gmail.com>
2024-10-20 19:04:06 +0700

    file : add active = 1 to student page list

--- src/pages/admin/class/ClassListPage.jsx
+++ src/pages/admin/class/ClassListPage.jsx
@@ -258,10 +258,12 @@ const ClassListPage = () => {
     },
   ];
 
+  console.log(selectedClass);
+  
   const dataToView = [
     { 'Class name': selectedClass?.class_name, icon: <BookMarked size={18} /> },
-    { Description: selectedClass?.description, icon: <LetterText size={18} /> },
-    { Status: selectedClass?.active, icon: <CircleDashed size={18} /> },
+    { Description: selectedClass?.description || 'N/A', icon: <LetterText size={18} /> },
+    // { Status: selectedClass?.active, icon: <CircleDashed size={18} /> },
     {
       'Created at': formatDate(selectedClass?.createdAt),
       icon: <Calendar size={18} />,
