5c0e230fb98440c324cbe03b876c5e1cb388a622
Nem Puthiraksa <raksasparkle@gmail.com>
2024-10-21 17:05:56 +0700

    fix: create student, fix formData in GuardianForm

--- src/components/student/GuardianForm.jsx
+++ src/components/student/GuardianForm.jsx
@@ -6,7 +6,7 @@ import { yupResolver } from '@hookform/resolvers/yup';
 import { useNavigate } from 'react-router-dom';
 
 // - Material UI components
-import { Box, Button, Grid, Stack } from '@mui/material';
+import { Box, Button, Grid, Stack, Typography } from '@mui/material';
 
 // - Material UI and Lucid Icons
 import { Mail, UserRoundPen } from 'lucide-react';
@@ -34,6 +34,9 @@ const GuardianForm = ({
   handleSubmitForm,
   studentData,
 }) => {
+  const { class_id } = studentData || {};
+  console.log('class_id in GuardianForm:', class_id);
+  console.log('student data recieve on guardian form:', studentData);
   // - Dispatch actions
   const dispatch = useDispatch();
   const navigate = useNavigate();
@@ -63,45 +66,46 @@ const GuardianForm = ({
 
   // Form submit function
   const onSubmit = async (data) => {
-    console.log('on submit on guardian:',data);
-    const formData = new FormData();
-
-    // Create an object with the form data
-    const formFields = {
-      guardianFirstName: data.guardianFirstName,  
-      guardianLastName: data.guardianLastName,    
-      guardianRelationship: data.guardianRelationship,
-      guardianPhoneNumber: data.guardianPhoneNumber,
-      guardianEmail: data.guardianEmail,
-      class_id: studentData.classId,
-      first_name: studentData.firstName,
-      last_name: studentData.lastName,
-      dob: dayjs(studentData.dob).format('YYYY-MM-DD'),
-      gender: studentData.gender,
-      phone_number: studentData.phoneNumber,
-      address: studentData.address || '',
-    };
-
-    // Append fields to FormData
-    Object.entries(formFields).forEach(([key, value]) =>
-      formData.append(key, value),
-    );
-
-    // Append the photo if it exists
-    if (studentData.photo) {
-      formData.append('photo', studentData.photo);
-    }
-    try {
-      await handleSubmitForm(formData);
-    } catch (error) {
-      console.error('Failed to sign up teacher:', error.message);
-    }
+  const formData = {
+    class_id: Number(studentData.class_id),
+    first_name: studentData.firstName,
+    last_name: studentData.lastName,
+    dob: dayjs(studentData.dob).format('YYYY-MM-DD'),
+    gender: studentData.gender,
+    phone_number: studentData.phoneNumber,
+    address: studentData.address || '',
+    guardian_first_name: data.guardianFirstName,
+    guardian_last_name: data.guardianLastName, 
+    guardian_relationship: data.guardianRelationship,
+    guardian_phone_number: data.guardianPhoneNumber,
+    guardian_email: data.guardianEmail
   };
+  console.log('formData:', formData);
+
+  if (studentData.photo) {
+    formData.photo = studentData.photo;
+  }
+
+  try {
+    await handleSubmitForm(formData);
+  } catch (error) {
+    console.error('Error creating student:', error);
+  }
+};
   return (
     <>
       <form onSubmit={handleSubmit(onSubmit)}>
         {/* Guardian Name Inputs */}
         <Stack direction={'column'} gap={2}>
+        <Typography
+              alignSelf={'start'}
+              variant="h6"
+              component="h2"
+              fontWeight={'bold'}
+              gutterBottom
+            >
+              Guardian Information
+            </Typography>
           <Grid container spacing={2}>
             <Grid item xs={12} sm={6}>
               <InputField
@@ -153,12 +157,31 @@ const GuardianForm = ({
           />
 
           {/* Action Buttons */}
-          <Stack direction="row" justifyContent="flex-end" gap={1}>
-            <StyledButton onClick={handleBack} variant="outlined" size="small">
+          <Stack
+            direction={'row'}
+            alignSelf={'flex-end'}
+            justifyContent={'flex-end'}
+            width={{ xs: '100%', sm: '300px', md: '280px' }}
+            gap={2}
+            marginTop={{ xs: 2, sm: 0 }}
+          >
+            <StyledButton
+              onClick={handleBack}
+              fullWidth
+              variant="outlined"
+              color="inherit"
+              size="small"
+            >
               Back
             </StyledButton>
-            <StyledButton variant="contained" type="submit" size="small">
-              Add Student
+            <StyledButton
+              fullWidth
+              variant="contained"
+              type="submit"
+              size="small"
+              disabled={isLoading}
+            >
+              {isLoading ? 'Adding...' : 'Add'}
             </StyledButton>
           </Stack>
         </Stack>
