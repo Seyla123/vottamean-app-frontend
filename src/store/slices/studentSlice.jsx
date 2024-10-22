81205c58a28cb06a85e9f32521f26bc008c047cc
Nem Puthiraksa <raksasparkle@gmail.com>
2024-10-22 10:15:33 +0700

    fix: create student, remove unused code

--- src/store/slices/studentSlice.jsx
+++ src/store/slices/studentSlice.jsx
@@ -1,38 +1,4 @@
-// import { createSlice } from '@reduxjs/toolkit';
-
-// const initialState = {
-//   photo: '',
-//   first_name: '',
-//   last_name: '',
-//   class_id: '',
-//   gender: '',
-//   dob: '',
-//   phone_number: '',
-//   address: '',
-//   guardian_first_name: '',
-//   guardian_last_name: '',
-//   guardian_relationship: '',
-//   guardian_email: '',
-//   guardian_phone_number: '',
-// };
-
-// export const studentFormSlice = createSlice({
-//   name: 'studentForm',
-//   initialState,
-//   reducers: {
-//     updateFormData: (state, action) => {
-//       return { ...state, ...action.payload };
-//     },
-//     resetFormData: () => initialState,
-//   },
-// });
-
-// export const { updateFormData, resetFormData } = studentFormSlice.actions;
-
-// export default studentFormSlice.reducer;
 import { createSlice } from "@reduxjs/toolkit";
-import { studentApi } from "../../services/studentApi"; // Assuming you have a studentApi service
-
 const studentSlice = createSlice({
   name: "students",
   initialState: {
