a5e8b7f21a0284a3bfca06b5a0ee1e1c9c63c007
Nem Puthiraksa <raksasparkle@gmail.com>
2024-10-24 10:45:20 +0700

    feature: update-student, add update modal to StudentUpdatePage

--- src/pages/admin/student/StudentUpdatePage.jsx
+++ src/pages/admin/student/StudentUpdatePage.jsx
@@ -16,6 +16,7 @@ import StyledButton from '../../../components/common/StyledMuiButton' ;
 
 import StudentUpdateForm from '../../../components/student/StudentUpdateForm';
 import GuardianUpdateForm from '../../../components/student/GuardianUpdateForm';
+import UpdateStudentForm from '../../../components/student/UpdateStudentForm';
 
 // Define TabPanel component
 const TabPanel = ({ children, value, index }) => {
@@ -49,77 +50,11 @@ const StudentUpdatePage = () => {
 
   return (
     <>
-      <Box sx={{ p: 2 }}>
-        <Stack
-          sx={{
-            direction: 'row',
-            bgcolor: '#fafafa',
-            boxShadow: '0px 3px 6px rgba(0,0,0,0.1)',
-            p: 1.5,
-            borderRadius: 4,
-            spacing: 2,
-          }}
-        >
-          <Tabs
-            value={value}
-            onChange={handleChange}
-            TabIndicatorProps={{ style: { display: 'none' } }}
-          >
-            <Tab
-              label="Student"
-              value={1}
-              sx={{
-                bgcolor: value === 1 ? '#eeedff' : 'transparent',
-                borderRadius: 4,
-                padding: isMobile ? '8px 24px' : '8px 16px',
-              }}
-            />
-            <Tab
-              label="Guardian"
-              value={2}
-              sx={{
-                bgcolor: value === 2 ? '#eeedff' : 'transparent',
-                borderRadius: 4,
-                padding: isMobile ? '8px 24px' : '8px 16px',
-              }}
-            />
-          </Tabs>
-        </Stack>
 
-        <Box sx={{ mt: 2 }}>
-          {/* Render StudentUpdateForm when value is 1 */}
-          <TabPanel value={value} index={1}>
-            <StudentUpdateForm
-              handleFormChange={handleFormChange}
-              onClose={handleCancel}
-            />
-          </TabPanel>
+       <UpdateStudentForm/>
 
-          {/* Render GuardianUpdateForm when value is 2 */}
-          <TabPanel value={value} index={2}>
-            <GuardianUpdateForm
-              handleFormChange={handleFormChange}
-              onClose={handleCancel}
-            />
-          </TabPanel>
-        </Box>
-      </Box>
     </>
   );
 };
 
 export default StudentUpdatePage;
-
-const imgStyle = {
-  width: { xs: 120, sm: 160 },
-  height: { xs: 120, sm: 160 },
-};
-
-const cardContainer = {
-  display: 'flex',
-  flexDirection: { xs: 'column', sm: 'row' },
-  width: '100%',
-  height: '100%',
-  borderRadius: 1,
-  overflow: 'hidden',
-};
